b31f8d75c42062b95bb9c79b72684a24776e2dec
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-10-21 15:27:58 -0400

    Adding TypeTags to capture type parameters for runtime.

--- loamstream.sbt
+++ loamstream.sbt
@@ -2,6 +2,9 @@ lazy val root = (project in file(".")).
   settings(
     name := "LoamStream",
     version := "0.1",
-    scalaVersion := "2.11.7"
+    scalaVersion := "2.11.7",
+    libraryDependencies ++= Seq(
+      "org.scala-lang" % "scala-library" % scalaVersion.value,
+      "org.scala-lang" % "scala-compiler" % scalaVersion.value,
+      "org.scala-lang" % "scala-reflect" % scalaVersion.value)
   )
-  
--- src/main/scala/loamstream/model/collections/LoamKeyTag.scala
+++ src/main/scala/loamstream/model/collections/LoamKeyTag.scala
@@ -1,21 +1,25 @@
 package loamstream.model.collections
 
+import scala.reflect.runtime.universe.{TypeTag, typeOf}
+
 /**
  * LoamStream
  * Created by oliverr on 10/21/2015.
  */
 object LoamKeyTag {
 
-  def createRoot[T]: LoamKeyTag[T, Nothing] = new LoamKeyTag[T, Nothing](None)
+  def createRoot[T: TypeTag]: LoamKeyTag[T, Nothing] = new LoamKeyTag[T, Nothing](None)
 
-  def node[T] = createRoot[T]
+  def node[T: TypeTag] = createRoot[T]
 
 }
 
-class LoamKeyTag[T, P <: LoamKeyTag[_, _]](val parentOpt: Option[P]) {
+class LoamKeyTag[T: TypeTag, P <: LoamKeyTag[_, _]](val parentOpt: Option[P]) {
 
-  def createChild[TC]: LoamKeyTag[TC, this.type] = new LoamKeyTag[TC, this.type](Some(this))
+  def createChild[TC: TypeTag]: LoamKeyTag[TC, this.type] = new LoamKeyTag[TC, this.type](Some(this))
 
-  def node[TC] = createChild[TC]
+  def node[TC: TypeTag] = createChild[TC]
+
+  override def toString: String = parentOpt.map(_.toString).getOrElse("") + "/" + typeOf[T]
 
 }

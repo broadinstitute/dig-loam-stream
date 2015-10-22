5bffb5058f7df90860154132b72e241b0261864a
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-10-22 13:49:09 -0400

    Turning LoamKeyTag into case class encapsulating type information.

--- src/main/scala/loamstream/model/collections/tags/LoamKeyTag.scala
+++ src/main/scala/loamstream/model/collections/tags/LoamKeyTag.scala
@@ -1,6 +1,6 @@
 package loamstream.model.collections.tags
 
-import scala.reflect.runtime.universe.{TypeTag, typeOf}
+import scala.reflect.runtime.universe.{TypeTag, typeOf, typeTag}
 
 /**
  * LoamStream
@@ -8,18 +8,18 @@ import scala.reflect.runtime.universe.{TypeTag, typeOf}
  */
 object LoamKeyTag {
 
-  def createRoot[T: TypeTag]: LoamKeyTag[T, Nothing] = new LoamKeyTag[T, Nothing](None)
+  def createRoot[T: TypeTag]: LoamKeyTag[T, Nothing] = new LoamKeyTag[T, Nothing](typeTag[T], None)
 
   def node[T: TypeTag] = createRoot[T]
 
 }
 
-class LoamKeyTag[T: TypeTag, P <: LoamKeyTag[_, _]](val parentOpt: Option[P]) {
+case class LoamKeyTag[T, P <: LoamKeyTag[_, _]](tpeTag: TypeTag[T], parentOpt: Option[P]) {
 
-  def createChild[TC: TypeTag]: LoamKeyTag[TC, this.type] = new LoamKeyTag[TC, this.type](Some(this))
+  def createChild[TC: TypeTag]: LoamKeyTag[TC, this.type] = new LoamKeyTag[TC, this.type](typeTag[TC], Some(this))
 
   def node[TC: TypeTag] = createChild[TC]
 
-  override def toString: String = parentOpt.map(_.toString).getOrElse("") + "/" + typeOf[T]
+  override def toString: String = parentOpt.map(_.toString).getOrElse("") + "/" + tpeTag.tpe
 
 }

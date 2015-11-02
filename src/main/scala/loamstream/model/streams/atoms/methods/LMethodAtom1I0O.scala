1797ad0b51bd93bde8aee9cdd54bc38e1a7c0d4f
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-11-02 15:32:02 -0500

    Adding two LMethod atoms stubs.

---
+++ src/main/scala/loamstream/model/streams/atoms/methods/LMethodAtom1I0O.scala
@@ -0,0 +1,17 @@
+package loamstream.model.streams.atoms.methods
+
+import loamstream.model.streams.methods.LMethod1I0O
+import loamstream.model.streams.sockets.LSocket
+import loamstream.model.tags.methods.LMethodTag1I0O
+import loamstream.model.tags.piles.LPileTag
+import scala.reflect.runtime.universe.{typeTag, TypeTag}
+
+/**
+ * RugLoom - A prototype for a pipeline building toolkit
+ * Created by oruebenacker on 11/2/15.
+ */
+case class LMethodAtom1I0O[I0 <: LPileTag : TypeTag](id: String, tag: LMethodTag1I0O[I0])
+  extends LMethod1I0O[I0, LMethodTag1I0O[I0], LMethodAtom1I0O[I0]] {
+  override def input0: LSocket[I0, LMethodTag1I0O[I0], LMethodAtom1I0O[I0]] = ??? // TODO
+
+}

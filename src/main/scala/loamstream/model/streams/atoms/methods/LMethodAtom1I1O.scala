6e5d38609c69a559d2d1d773e404c26f7fcf0562
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-11-03 16:26:05 -0500

    Adding 1,1 method atom.

---
+++ src/main/scala/loamstream/model/streams/atoms/methods/LMethodAtom1I1O.scala
@@ -0,0 +1,36 @@
+package loamstream.model.streams.atoms.methods
+
+import loamstream.model.streams.atoms.methods.LMethodAtom1I1O.{LSocketI0, LSocketO0}
+import loamstream.model.streams.methods.LMethod1I1O
+import loamstream.model.streams.sockets.LSocket
+import loamstream.model.tags.methods.LMethodTag1I1O
+import loamstream.model.tags.piles.LPileTag
+
+import scala.reflect.runtime.universe.TypeTag
+
+/**
+ * RugLoom - A prototype for a pipeline building toolkit
+ * Created by oruebenacker on 11/2/15.
+ */
+object LMethodAtom1I1O {
+
+  case class LSocketI0[I0 <: LPileTag, O0 <: LPileTag](method: LMethodAtom1I1O[I0, O0])
+    extends LSocket[I0, LMethodTag1I1O[I0, O0], LMethodAtom1I1O[I0, O0]] {
+    override def pileTag = method.tag.input0
+  }
+
+  case class LSocketO0[I0 <: LPileTag, O0 <: LPileTag](method: LMethodAtom1I1O[I0, O0])
+    extends LSocket[O0, LMethodTag1I1O[I0, O0], LMethodAtom1I1O[I0, O0]] {
+    override def pileTag = method.tag.output0
+  }
+
+}
+
+case class LMethodAtom1I1O[I0 <: LPileTag : TypeTag,
+O0 <: LPileTag : TypeTag](id: String, tag: LMethodTag1I1O[I0, O0])
+  extends LMethod1I1O[I0, O0, LMethodTag1I1O[I0, O0], LMethodAtom1I1O[I0, O0]] {
+  override def input0: LSocket[I0, LMethodTag1I1O[I0, O0], LMethodAtom1I1O[I0, O0]] = LSocketI0(this)
+
+  override def output0: LSocket[O0, LMethodTag1I1O[I0, O0], LMethodAtom1I1O[I0, O0]] = LSocketO0(this)
+
+}

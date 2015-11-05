0a97ac0c58b8513a572af0979d54b1482fb6a66d
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-11-05 17:48:12 -0500

    Example of creating a method.

--- src/main/scala/loamstream/model/streams/apps/LStreamApp1.scala
+++ src/main/scala/loamstream/model/streams/apps/LStreamApp1.scala
@@ -1,12 +1,16 @@
 package loamstream.model.streams.apps
 
 import loamstream.model.streams.atoms.maps.LMapAtom02
+import loamstream.model.streams.atoms.methods.LMethodAtom2I2O
 import loamstream.model.streams.atoms.sets.LSetAtom03
+import loamstream.model.tags.maps.LMapTag02
+import loamstream.model.tags.methods.LMethodTag2I2O
+import loamstream.model.tags.sets.LSetTag03
 
 /**
- * LoamStream
- * Created by oliverr on 10/28/2015.
- */
+  * LoamStream
+  * Created by oliverr on 10/28/2015.
+  */
 object LStreamApp1 extends App {
 
   val inputSet = LSetAtom03.create[Int, String, Double]("InputSet")
@@ -14,13 +18,16 @@ object LStreamApp1 extends App {
   val outputSet = LSetAtom03.create[Int, String, Double]("OutputSet")
   val outputMap = LMapAtom02.create[Byte, String, Int]("OutputMap")
 
-  //  val method = LMethodTag2I2O(inputSet, inputMap, outputSet, outputMap)
+  val method =
+    LMethodAtom2I2O("TheMethod", LMethodTag2I2O(LSetTag03.create[Int, String, Double],
+      LMapTag02.create[String, Char, String], LSetTag03.create[Int, String, Double],
+      LMapTag02.create[Byte, String, Int]))
 
   println(inputSet)
   println(inputMap)
   println(outputSet)
   println(outputMap)
-  //  println(method)
+  println(method)
 
 
 }

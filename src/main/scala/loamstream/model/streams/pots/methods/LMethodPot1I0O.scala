71b64df3174c5f5cf8d75e817a1b0d92205fa5d2
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-11-12 16:39:51 -0500

    Tinkering with methods in LMethodPot1I0O.

--- src/main/scala/loamstream/model/streams/pots/methods/LMethodPot1I0O.scala
+++ src/main/scala/loamstream/model/streams/pots/methods/LMethodPot1I0O.scala
@@ -1,14 +1,19 @@
 package loamstream.model.streams.pots.methods
 
 import loamstream.model.streams.methods.LMethod1I0O
+import loamstream.model.streams.sockets.LSocket
 import loamstream.model.tags.piles.LPileTag
+
+import scala.language.higherKinds
 
 /**
   * LoamStream
   * Created by oliverr on 11/10/2015.
   */
-case class LMethodPot1I0O[I0 <: LPileTag, +C <: LMethod1I0O[_]](id: String, child: C)
-  extends LMethodPot[C] with LMethod1I0O[I0] {
-  def input0 = ??? // TODO
+case class LMethodPot1I0O[I0C <: LPileTag, +C[_] <: LMethod1I0O[_], KN](id: String, child: C[I0C])
+  extends LMethodPot[C[I0C]] with LMethod1I0O[I0C#UpTag[KN]] {
+  def input0: LSocket[I0C#UpTag[KN], LMethod1I0O[I0C#UpTag[KN]]] = ???
+
+  // TODO
   def tag = ??? // TODO
 }

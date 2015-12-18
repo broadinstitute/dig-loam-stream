581a725a263367f0ec9269469a8ba9a4d788e48f
Oliver Ruebenacker <oliverr@broadinstitute.org>
2015-12-18 15:58:44 -0500

    Some minor changes.

--- src/main/scala/loamstream/apps/LapRunApp.scala
+++ src/main/scala/loamstream/apps/LapRunApp.scala
@@ -1,9 +1,17 @@
 package loamstream.apps
 
+import java.nio.file.Paths
+
+import scala.sys.process.stringToProcess
+
 /**
   * LoamStream
   * Created by oliverr on 11/25/2015.
   */
 object LapRunApp extends App {
-  println("This is LapRunApp saying 'Hello'!")
+  val cmdLine = """ls"""
+  println("Current directory: " + Paths.get(".").toAbsolutePath().normalize().toString())
+  println("Parent directory: " + Paths.get("..").toAbsolutePath().normalize().toString())
+  val exitCode = cmdLine.!
+  println("Exit code is " + exitCode)
 }

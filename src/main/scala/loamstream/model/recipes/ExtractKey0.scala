880411d9a3a333cb100485ee596ae20a29fd6c4f
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-01-12 13:52:07 -0500

    BROKEN: new attempt at static type-checking for key 0 extraction.

--- src/main/scala/loamstream/model/recipes/ExtractKey0.scala
+++ src/main/scala/loamstream/model/recipes/ExtractKey0.scala
@@ -1,11 +1,12 @@
 package loamstream.model.recipes
 
-import loamstream.model.calls.LPileCall
 import loamstream.model.calls.props.LProps
+import loamstream.model.calls.{LPileCall, LSetCall}
 import loamstream.model.recipes.ExtractKey0.CanExtractKey0
-import loamstream.model.tags.LPileTag
+import loamstream.model.tags.{LKeys, LPileTag, LSetTag}
 
 import scala.language.higherKinds
+import scala.reflect.runtime.universe.TypeTag
 
 /**
   * LoamStream
@@ -14,6 +15,12 @@ import scala.language.higherKinds
 object ExtractKey0 {
 
   trait CanExtractKey0 extends LProps
+
+  implicit class Extraction[K: TypeTag, Tag0 <: LPileTag[K, _], Call0 <: LPileCall[Tag0, _, _]](call0: Call0) {
+    type Tag = LSetTag.Set1[K]
+    type Call = LSetCall[Tag, LPileCalls.LCalls1[Tag0, Call0], _]
+    def key1(id: String): Call = LSetCall(LSetTag.forKeyTup1[K], ExtractKey0(LPileCalls.calls1(call0)))
+  }
 
   def fromPile[K, Tag0 <: LPileTag[K, _], Call0 <: LPileCall[Tag0, _, CanExtractKey0]](pile: Call0) = {
     ExtractKey0[K, Tag0, Call0](LPileCalls.calls1[Tag0, Call0](pile))
--- src/main/scala/loamstream/model/tags/LSetTag.scala
+++ src/main/scala/loamstream/model/tags/LSetTag.scala
@@ -8,11 +8,16 @@ import scala.reflect.runtime.universe.{TypeTag, typeTag}
   * Created by oliverr on 10/21/2015.
   */
 object LSetTag {
-  def forKeyTup1[K0: TypeTag] = LSetTag(LKeys.tup1[K0])
+  type Set0 = LSetTag[Nothing, Nothing]
+  type Set1[K0] = LSetTag[K0, LKeys.LKeys0]
+  type Set2[K0, K1] = LSetTag[K0, LKeys.LKeys1[K1]]
+  type Set3[K0, K1, K2] = LSetTag[K0, LKeys.LKeys2[K1, K2]]
 
-  def forKeyTup2[K0: TypeTag, K1: TypeTag] = LSetTag(LKeys.tup2[K0, K1])
+  def forKeyTup1[K0: TypeTag]: Set1[K0] = LSetTag(LKeys.tup1[K0])
 
-  def forKeyTup3[K0: TypeTag, K1: TypeTag, K2: TypeTag] = LSetTag(LKeys.tup3[K0, K1, K2])
+  def forKeyTup2[K0: TypeTag, K1: TypeTag]: Set2[K0, K1] = LSetTag(LKeys.tup2[K0, K1])
+
+  def forKeyTup3[K0: TypeTag, K1: TypeTag, K2: TypeTag]: Set3[K0, K1, K2] = LSetTag(LKeys.tup3[K0, K1, K2])
 }
 
 case class LSetTag[HeadKey, KeysTail <: LKeys[_, _]](keyTags: LKeys[HeadKey, KeysTail])

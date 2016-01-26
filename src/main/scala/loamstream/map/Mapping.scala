47464fcbe7f0cb823715947bb53aee0e13d67e1c
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-01-26 15:49:17 -0500

    Some more API changes for Mapping.

--- src/main/scala/loamstream/map/Mapping.scala
+++ src/main/scala/loamstream/map/Mapping.scala
@@ -1,6 +1,6 @@
 package loamstream.map
 
-import loamstream.map.Mapping.{Constraint, Slot, Target, TargetGenerator}
+import loamstream.map.Mapping.{ConstraintMaker, Constraint, Slot, Target, TargetChoices}
 
 /**
   * LoamStream
@@ -12,34 +12,43 @@ object Mapping {
 
   trait Slot
 
-  trait TargetGenerator {
+  trait TargetChoices {
     def numMoreTargets: Int
 
     def nextTarget: Target
 
-    def withConstraint(constraint: Constraint): TargetGenerator
+    def withConstraint(constraint: Constraint): TargetChoices
+  }
+
+  trait ConstraintMaker {
+    def newConstraintFor(mapping: Mapping): Constraint
   }
 
   trait Constraint {
+    def mapping: Mapping
+
     def slots: Set[Slot]
+
+    def slotFilter(slot: Slot): Target => Boolean
   }
 
   def empty = Mapping(Set.empty, Set.empty, Map.empty, Set.empty, Map.empty)
 
-  def fromSlots(slots: Map[Slot, TargetGenerator]) =
+  def fromSlots(slots: Map[Slot, TargetChoices]) =
     Mapping(slots.keySet, slots.keySet, slots, Set.empty, Map.empty)
 
 }
 
-case class Mapping(slots: Set[Slot], emptySlots: Set[Slot], generators: Map[Slot, TargetGenerator],
+case class Mapping(slots: Set[Slot], emptySlots: Set[Slot], choices: Map[Slot, TargetChoices],
                    constraints: Set[Constraint], bindings: Map[Slot, Target]) {
 
-  def plusSlot(slot: Slot, generator: TargetGenerator) =
-    copy(slots = slots + slot, emptySlots = emptySlots + slot, generators = generators + (slot -> generator))
+  def plusSlot(slot: Slot, slotChoices: TargetChoices) =
+    copy(slots = slots + slot, emptySlots = emptySlots + slot, choices = choices + (slot -> slotChoices))
 
-  def plusConstraint(constraint: Constraint) = {
-    val constrainedGenerators = constraint.slots.map(slot => (slot, generators(slot).withConstraint(constraint))).toMap
-    copy(constraints = constraints + constraint, generators = generators ++ constrainedGenerators)
+  def plusConstraint(constraintMaker: ConstraintMaker) = {
+    val constraint = constraintMaker.newConstraintFor(this)
+    val reducedChoices = constraint.slots.map(slot => (slot, choices(slot).withConstraint(constraint))).toMap
+    copy(constraints = constraints + constraint, choices = choices ++ reducedChoices)
   }
 
   def plusBinding(slot: Slot, target: Target) =

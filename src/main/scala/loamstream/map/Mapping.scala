9dd41f8603e42dc5592093c836283414af7f0f9c
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-01-26 16:58:51 -0500

    More changes to mappings API: distinguish rules (independent of mapping) and constraints (rules applied to particular mapping); distinguish between unconstrained and constrained choices.

--- src/main/scala/loamstream/map/Mapping.scala
+++ src/main/scala/loamstream/map/Mapping.scala
@@ -1,57 +1,71 @@
 package loamstream.map
 
-import loamstream.map.Mapping.{ConstraintMaker, Constraint, Slot, Target, TargetChoices}
+import loamstream.map.Mapping._
 
 /**
-  * LoamStream
-  * Created by oliverr on 1/22/2016.
-  */
+ * LoamStream
+ * Created by oliverr on 1/22/2016.
+ */
 object Mapping {
 
   trait Target
 
   trait Slot
 
-  trait TargetChoices {
-    def numMoreTargets: Int
+  trait Rule {
+    def slots(mapping: Mapping): Set[Slot]
 
-    def nextTarget: Target
+    def slotFilter(mapping: Mapping, slot: Slot): Target => Boolean
 
-    def withConstraint(constraint: Constraint): TargetChoices
-  }
-
-  trait ConstraintMaker {
-    def newConstraintFor(mapping: Mapping): Constraint
+    def constraintFor(mapping: Mapping): Constraint
   }
 
   trait Constraint {
+    def rule: Rule
+
     def mapping: Mapping
 
-    def slots: Set[Slot]
+    def slots: Set[Slot] = rule.slots(mapping)
 
-    def slotFilter(slot: Slot): Target => Boolean
+    def slotFilter(slot: Slot) = rule.slotFilter(mapping: Mapping, slot: Slot)
   }
 
-  def empty = Mapping(Set.empty, Set.empty, Map.empty, Set.empty, Map.empty)
+  trait RawChoices {
+    def constrainedBy(constraints: Set[Constraint]): ConstrainedChoices
+  }
 
-  def fromSlots(slots: Map[Slot, TargetChoices]) =
-    Mapping(slots.keySet, slots.keySet, slots, Set.empty, Map.empty)
+  trait ConstrainedChoices {
+    def countChoices: Int
+
+    def nextChoice: Target
+  }
+
+  def empty = Mapping(Set.empty, Set.empty, Map.empty, Set.empty, Set.empty, Map.empty)
+
+  def fromSlots(slots: Map[Slot, RawChoices]) =
+    Mapping(slots.keySet, slots.keySet, slots, Set.empty, Set.empty, Map.empty)
 
 }
 
-case class Mapping(slots: Set[Slot], emptySlots: Set[Slot], choices: Map[Slot, TargetChoices],
-                   constraints: Set[Constraint], bindings: Map[Slot, Target]) {
+case class Mapping(slots: Set[Slot], emptySlots: Set[Slot], choices: Map[Slot, RawChoices],
+                   rules: Set[Rule], constraints: Set[Constraint], bindings: Map[Slot, Target]) {
 
-  def plusSlot(slot: Slot, slotChoices: TargetChoices) =
-    copy(slots = slots + slot, emptySlots = emptySlots + slot, choices = choices + (slot -> slotChoices))
+  def plusSlot(slot: Slot, slotChoices: RawChoices) =
+    copy(slots = slots + slot, emptySlots = emptySlots + slot, constraints = rules.map(_.constraintFor(this)),
+      choices = choices + (slot -> slotChoices))
 
-  def plusConstraint(constraintMaker: ConstraintMaker) = {
-    val constraint = constraintMaker.newConstraintFor(this)
-    val reducedChoices = constraint.slots.map(slot => (slot, choices(slot).withConstraint(constraint))).toMap
-    copy(constraints = constraints + constraint, choices = choices ++ reducedChoices)
-  }
+  def plusSlots(slotsNew: Map[Slot, RawChoices]) =
+    copy(slots = slots ++ slotsNew.keys, emptySlots = emptySlots ++ slotsNew.keys,
+      constraints = rules.map(_.constraintFor(this)), choices = choices ++ slotsNew)
+
+  def plusRule(rule: Rule) =
+    copy(rules = rules + rule, constraints = constraints + rule.constraintFor(this))
+
+  def plusRules(rulesNew: Iterable[Rule]) =
+    copy(rules = rules ++ rulesNew, constraints = constraints ++ rulesNew.map(_.constraintFor(this)))
 
   def plusBinding(slot: Slot, target: Target) =
-    copy(emptySlots = emptySlots - slot, bindings = bindings + (slot -> target))
+    copy(emptySlots = emptySlots - slot, constraints = rules.map(_.constraintFor(this)),
+      bindings = bindings + (slot -> target))
 
 }

8fd5dc265bd6c6dbff21cff65de1b741327af665
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-03-09 16:29:59 -0500

    Adding variant ids to output of VcfParser.

--- src/main/scala/tools/VcfParser.scala
+++ src/main/scala/tools/VcfParser.scala
@@ -4,6 +4,7 @@ import java.nio.file.Path
 
 import htsjdk.variant.variantcontext.{Genotype, VariantContext}
 import htsjdk.variant.vcf.VCFFileReader
+import tools.VcfParser.{MapRow, RawRow, SeqRow}
 
 import scala.collection.JavaConverters.{asScalaBufferConverter, asScalaIteratorConverter}
 
@@ -13,6 +14,36 @@ import scala.collection.JavaConverters.{asScalaBufferConverter, asScalaIteratorC
   * @author Kaan Yuksel
   */
 object VcfParser {
+
+  trait Row[T] {
+    def id: String
+
+    def data: T
+  }
+
+  case class RawRow(context: VariantContext) extends Row[VariantContext] {
+    override def id: String = context.getID
+
+    override def data: VariantContext = context
+  }
+
+  object SeqRow {
+    def apply(context: VariantContext): SeqRow = new SeqRow(context.getID, context.getGenotypes.asScala.toSeq)
+  }
+
+  class SeqRow(val id: String, val genotypes: Seq[Genotype]) extends Row[Seq[Genotype]] {
+    override def data: Seq[Genotype] = genotypes
+  }
+
+  object MapRow {
+    def apply(context: VariantContext, samples: Seq[String]): MapRow =
+      new MapRow(context.getID, samples.zip(context.getGenotypes.asScala).toMap)
+  }
+
+  class MapRow(val id: String, val genotypesMap: Map[String, Genotype]) extends Row[Map[String, Genotype]] {
+    override def data: Map[String, Genotype] = genotypesMap
+  }
+
   def apply(path: Path, requireIndex: Boolean = false): VcfParser =
     new VcfParser(new VCFFileReader(path.toFile, requireIndex))
 }
@@ -20,12 +51,12 @@ object VcfParser {
 class VcfParser(val reader: VCFFileReader) {
   val samples: Seq[String] = reader.getFileHeader.getGenotypeSamples.asScala.toSeq
 
-  def rowIter: Iterator[VariantContext] = reader.iterator().asScala
+  def rowIter: Iterator[RawRow] = reader.iterator().asScala.map(RawRow)
 
-  def genotypesIter: Iterator[Seq[Genotype]] = rowIter.map({ row => row.getGenotypes.asScala.toSeq })
+  def genotypesIter: Iterator[SeqRow] = reader.iterator().asScala.map(SeqRow(_))
 
-  def genotypeMapIter: Iterator[Map[String, Genotype]] =
-    rowIter.map({row => samples.zip(row.getGenotypes.asScala).toMap})
+  def genotypeMapIter: Iterator[MapRow] =
+    reader.iterator().asScala.map(MapRow(_, samples))
 
 
 }
--- src/test/scala/tools/VcfParserTest.scala
+++ src/test/scala/tools/VcfParserTest.scala
@@ -15,15 +15,15 @@ class VcfParserTest extends FunSuite {
     val vcfParser = VcfParser(sampleVcfFile)
     val samples = vcfParser.samples
     assert(samples === Seq("Sample1", "Sample2", "Sample3"))
-    val genotypes = vcfParser.genotypesIter.toSeq
+    val seqRows = vcfParser.genotypesIter.toSeq
     // scalastyle:off magic.number
-    assert(VcfUtils.genotypeToAltCount(genotypes(2)(2)) === 2)
-    assert(VcfUtils.genotypeToAltCount(genotypes(1).head) === 0)
-    assert(VcfUtils.genotypeToAltCount(genotypes(4)(1)) === 1)
-    val genotypesMaps = vcfParser.genotypeMapIter.toSeq
-    assert(VcfUtils.genotypeToAltCount(genotypesMaps(2)("Sample3")) === 2)
-    assert(VcfUtils.genotypeToAltCount(genotypesMaps(1)("Sample1")) === 0)
-    assert(VcfUtils.genotypeToAltCount(genotypesMaps(4)("Sample2")) === 1)
+    assert(VcfUtils.genotypeToAltCount(seqRows(2).genotypes(2)) === 2)
+    assert(VcfUtils.genotypeToAltCount(seqRows(1).genotypes.head) === 0)
+    assert(VcfUtils.genotypeToAltCount(seqRows(4).genotypes(1)) === 1)
+    val mapRows = vcfParser.genotypeMapIter.toSeq
+    assert(VcfUtils.genotypeToAltCount(mapRows(2).genotypesMap("Sample3")) === 2)
+    assert(VcfUtils.genotypeToAltCount(mapRows(1).genotypesMap("Sample1")) === 0)
+    assert(VcfUtils.genotypeToAltCount(mapRows(4).genotypesMap("Sample2")) === 1)
     // scalastyle:on magic.number
   }
 

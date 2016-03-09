0b5ef84eb1fe9ca5d0ba1bf6fe54e542b74ecb0a
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-03-09 14:26:56 -0500

    Working on PCA projection.

---
+++ src/test/scala/tools/PcaProjectionApp.scala
@@ -0,0 +1,19 @@
+package tools
+
+import loamstream.conf.SampleFiles
+import loamstream.utils.TestUtils
+import utils.Loggable
+
+/**
+  * RugLoom - A prototype for a pipeline building toolkit
+  * Created by oruebenacker on 3/8/16.
+  */
+object PcaProjectionApp extends App with Loggable {
+  val pcaWeightsFile = TestUtils.assertSomeAndGet(PcaWeightsReader.weightsFilePath)
+  val weights = PcaWeightsReader.read(pcaWeightsFile)
+  val miniVcf = TestUtils.assertSomeAndGet(SampleFiles.miniVcfOpt)
+  val vcfParser = VcfParser(miniVcf)
+  for(genotypeMap <- vcfParser.genotypeMapIter) {
+    // TODO
+  }
+}

2366c39ebdde1fe8dbd93ff6de4c79ba8c7d55a6
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-04-05 12:14:35 -0400

    Adding core tools for ancestry inference.

--- src/main/scala/loamstream/model/kinds/instances/StoreKinds.scala
+++ src/main/scala/loamstream/model/kinds/instances/StoreKinds.scala
@@ -1,7 +1,7 @@
 package loamstream.model.kinds.instances
 
 import loamstream.model.kinds.LSpecificKind
-import loamstream.model.kinds.instances.PileKinds.{genotypeCallsByVariantAndSample, sampleIds, singletonCounts}
+import loamstream.model.kinds.instances.PileKinds._
 
 /**
   * LoamStream
@@ -10,6 +10,10 @@ import loamstream.model.kinds.instances.PileKinds.{genotypeCallsByVariantAndSamp
 object StoreKinds {
   val vcfFile = LSpecificKind("VCF file", genotypeCallsByVariantAndSample)
   val vdsFile = LSpecificKind("VDS file", genotypeCallsByVariantAndSample)
+  val pcaWeightsFile = LSpecificKind("PCA weights file", pcaWeights)
+  val pcaProjectedFile = LSpecificKind("PCA projected values file", pcaProjected)
+  val sampleClustersFile = LSpecificKind("sample clusters files", sampleClustersByAncestry)
+
   val singletonsFile = LSpecificKind("Singletons file", singletonCounts)
   val genotypesCassandraTable = LSpecificKind("Genotypes Cassandra table", genotypeCallsByVariantAndSample)
   val sampleIdsFile = LSpecificKind("Sample ids file", sampleIds)

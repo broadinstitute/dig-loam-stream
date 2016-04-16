948939039dabae2186c2cdbe7a5595b4f1318cd7
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-04-15 20:23:39 -0400

    Switching from Scala reflection types to LType.

--- src/main/scala/loamstream/apps/minimal/MiniMockStore.scala
+++ src/main/scala/loamstream/apps/minimal/MiniMockStore.scala
@@ -1,11 +1,11 @@
 package loamstream.apps.minimal
 
-import htsjdk.variant.variantcontext.Genotype
 import loamstream.model.id.LId
 import loamstream.model.kinds.instances.StoreKinds
 import loamstream.model.piles.{LPileSpec, LSig}
-import loamstream.model.signatures.Signatures.{SingletonCount, _}
 import loamstream.model.stores.LStore
+import loamstream.model.values.LType.LTuple.{LTuple1, LTuple2}
+import loamstream.model.values.LType.{LGenotype, LString, LVariantId}
 
 /**
   * LoamStream
@@ -13,10 +13,12 @@ import loamstream.model.stores.LStore
   */
 object MiniMockStore {
 
-  val genotypesCassandraTable = MiniMockStore("Cassandra genotype calls table",
-    LPileSpec(LSig.Map[(String, VariantId), Genotype], StoreKinds.genotypesCassandraTable))
-  val sampleIdsCassandraTable = MiniMockStore("Cassandra sample ids table.", LPileSpec(LSig.Set[Tuple1[String]],
-    StoreKinds.sampleIdsCassandraTable))
+  val genotypesCassandraTable =
+    MiniMockStore("Cassandra genotype calls table", LPileSpec(LSig.Map(LTuple2(LString, LVariantId), LGenotype),
+      StoreKinds.genotypesCassandraTable))
+  val sampleIdsCassandraTable =
+    MiniMockStore("Cassandra sample ids table.", LPileSpec(LSig.Set(LTuple1(LString)),
+      StoreKinds.sampleIdsCassandraTable))
 
   val stores = Set[LStore](genotypesCassandraTable, sampleIdsCassandraTable)
 
--- src/main/scala/loamstream/tools/core/CoreStore.scala
+++ src/main/scala/loamstream/tools/core/CoreStore.scala
@@ -1,12 +1,12 @@
 package loamstream.tools.core
 
-import htsjdk.variant.variantcontext.Genotype
 import loamstream.model.id.LId
 import loamstream.model.kinds.instances.StoreKinds
 import loamstream.model.piles.{LPileSpec, LSig}
-import loamstream.model.signatures.Signatures._
 import loamstream.model.stores.LStore
-import scala.reflect.runtime.universe
+import loamstream.model.values.LType.LTuple.{LTuple1, LTuple2}
+import loamstream.model.values.LType.{LClusterId, LDouble, LGenotype, LInt, LSampleId, LSingletonCount, LString,
+LVariantId}
 
 /**
   * LoamStream
@@ -14,17 +14,21 @@ import scala.reflect.runtime.universe
   */
 object CoreStore {
 
-  val vcfFile = CoreStore("VCF file", LPileSpec(LSig.Map[(String, VariantId), Genotype], StoreKinds.vcfFile))
-  val vdsFile = CoreStore("VDS file", LPileSpec(LSig.Map[(String, VariantId), Genotype], StoreKinds.vdsFile))
+  val vcfFile =
+    CoreStore("VCF file", LPileSpec(LSig.Map(LTuple2(LString, LVariantId), LGenotype), StoreKinds.vcfFile))
+  val vdsFile =
+    CoreStore("VDS file", LPileSpec(LSig.Map(LTuple2(LString, LVariantId), LGenotype), StoreKinds.vdsFile))
   val pcaWeightsFile =
-    CoreStore("PCA weights file", LPileSpec(LSig.Map[(SampleId, Int), Double], StoreKinds.pcaWeightsFile))
+    CoreStore("PCA weights file", LPileSpec(LSig.Map(LTuple2(LSampleId, LInt), LDouble), StoreKinds.pcaWeightsFile))
   val pcaProjectedFile =
-    CoreStore("PCA projected file", LPileSpec(LSig.Map[(SampleId, Int), Double], StoreKinds.pcaProjectedFile))
+    CoreStore("PCA projected file", LPileSpec(LSig.Map(LTuple2(LSampleId, LInt), LDouble),
+      StoreKinds.pcaProjectedFile))
   val sampleClusterFile =
-    CoreStore("Sample cluster file", LPileSpec(LSig.Map[SampleId, ClusterId], StoreKinds.sampleClustersFile))
-  val singletonsFile = CoreStore("Singletons file", LPileSpec(LSig.Map[Tuple1[String], SingletonCount],
+    CoreStore("Sample cluster file", LPileSpec(LSig.Map(LTuple1(LSampleId), LClusterId),
+      StoreKinds.sampleClustersFile))
+  val singletonsFile = CoreStore("Singletons file", LPileSpec(LSig.Map(LTuple1(LString), LSingletonCount),
     StoreKinds.singletonsFile))
-  val sampleIdsFile = CoreStore("Sample ids file", LPileSpec(LSig.Set[Tuple1[String]], StoreKinds.sampleIdsFile))
+  val sampleIdsFile = CoreStore("Sample ids file", LPileSpec(LSig.Set(LTuple1(LString)), StoreKinds.sampleIdsFile))
 
   val stores =
     Set[LStore](vcfFile, vdsFile, pcaWeightsFile, pcaProjectedFile, sampleClusterFile, singletonsFile, sampleIdsFile)

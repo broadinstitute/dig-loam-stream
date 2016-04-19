3d253abeb5ffc01d4dfbb09767295f8b6749c85b
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-04-19 15:36:42 -0400

    Switching specifics field in LKind from Any to LValue.

--- src/main/scala/loamstream/model/kinds/instances/RecipeKinds.scala
+++ src/main/scala/loamstream/model/kinds/instances/RecipeKinds.scala
@@ -1,16 +1,26 @@
 package loamstream.model.kinds.instances
 
 import loamstream.model.kinds.LSpecificKind
+import loamstream.model.values.LType.LTuple.LTuple2
+import loamstream.model.values.LType.{LInt, LString}
 
 /**
- * LoamStream
- * Created by oliverr on 2/16/2016.
- */
+  * LoamStream
+  * Created by oliverr on 2/16/2016.
+  */
 object RecipeKinds {
-  def usePreExisting(id: String): LSpecificKind[(String, String)] = LSpecificKind(("Use pre-existing", id))
-  def extractKey(index: Int): LSpecificKind[(String, Int)] = LSpecificKind(("Extract key", index))
-  def importVcf(index: Int): LSpecificKind[(String, Int)] = LSpecificKind(("Convert VCF to VDS", index))
-  def calculateSingletons(index: Int): LSpecificKind[(String, Int)] = LSpecificKind(("Calculate singletons", index))
+  def usePreExisting(id: String): LSpecificKind[(String, String)] =
+    LSpecificKind(LTuple2(LString, LString)("Use pre-existing", id))
+
+  def extractKey(index: Int): LSpecificKind[(String, Int)] =
+    LSpecificKind(LTuple2(LString, LInt)("Extract key", index))
+
+  def importVcf(index: Int): LSpecificKind[(String, Int)] =
+    LSpecificKind(LTuple2(LString, LInt)("Convert VCF to VDS", index))
+
+  def calculateSingletons(index: Int): LSpecificKind[(String, Int)] =
+    LSpecificKind(LTuple2(LString, LInt)("Calculate singletons", index))
+
   val extractSampleIdsFromGenotypeCalls = LSpecificKind("Extract sample ids from genotype calls.")
   val pcaProjection = LSpecificKind("PCA projection")
   val clusteringSamplesByFeatures = LSpecificKind("clustering samples by features")

289eceb31c571266ba45a043b28b1dbdaa8cd5ee
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-04-22 09:33:26 -0400

    Merge latest oar_values, fix post-merge conflicts.

--- src/test/scala/loamstream/model/values/LValuesTest.scala
+++ src/test/scala/loamstream/model/values/LValuesTest.scala
@@ -1,10 +1,7 @@
 package loamstream.model.values
 
-import loamstream.model.values.LType.LTuple.{LTuple1, LTuple10, LTuple11, LTuple12, LTuple13, LTuple14, LTuple15,
-LTuple16, LTuple17, LTuple18, LTuple19, LTuple2, LTuple20, LTuple21, LTuple22, LTuple3, LTuple4, LTuple5, LTuple6,
-LTuple7, LTuple8, LTuple9}
 import loamstream.model.values.LType.{LBoolean, LDouble, LFloat, LInt, LLong, LSampleId, LSeq, LSet, LShort, LString,
-LVariantId}
+LTuple, LVariantId}
 import org.scalatest.FunSuite
 
 /**
@@ -19,65 +16,65 @@ class LValuesTest extends FunSuite {
     assert(LValue("Hello World!", LString) === LString("Hello World!"))
     assert(LValue(Set("Hello", " World!"), LSet(LString)) === LSet(LString)(Set("Hello", " World!")))
     assert(LValue(Seq("Hello", " World!"), LSeq(LString)) === LSeq(LString)(Seq("Hello", " World!")))
-    assert(LTuple1(LString).asSeq === Seq(LString))
-    assert(LTuple2(LString, LInt).asSeq === Seq(LString, LInt))
-    assert(LTuple3(LString, LInt, LShort).asSeq === Seq(LString, LInt, LShort))
-    assert(LTuple4(LString, LInt, LShort, LLong).asSeq === Seq(LString, LInt, LShort, LLong))
-    assert(LTuple5(LString, LInt, LShort, LLong, LVariantId).asSeq === Seq(LString, LInt, LShort, LLong, LVariantId))
-    assert(LTuple6(LString, LInt, LShort, LLong, LVariantId, LBoolean).asSeq ===
+    assert(LTuple(LString).types === Seq(LString))
+    assert(LTuple(LString, LInt).types === Seq(LString, LInt))
+    assert(LTuple(LString, LInt, LShort).types === Seq(LString, LInt, LShort))
+    assert(LTuple(LString, LInt, LShort, LLong).types === Seq(LString, LInt, LShort, LLong))
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId).types === Seq(LString, LInt, LShort, LLong, LVariantId))
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean))
-    assert(LTuple7(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat))
-    assert(LTuple8(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble))
-    assert(LTuple9(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId))
-    assert(LTuple10(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString))
-    assert(LTuple11(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt))
-    assert(LTuple12(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort))
-    assert(LTuple13(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong))
-    assert(LTuple14(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId))
-    assert(LTuple15(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean))
-    assert(LTuple16(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat))
-    assert(LTuple17(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat, LDouble))
-    assert(LTuple18(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId))
-    assert(LTuple19(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString))
-    assert(LTuple20(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt))
-    assert(LTuple21(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort))
-    assert(LTuple22(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
-      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort, LLong).asSeq ===
+    assert(LTuple(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString,
+      LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort, LLong).types ===
       Seq(LString, LInt, LShort, LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort,
         LLong, LVariantId, LBoolean, LFloat, LDouble, LSampleId, LString, LInt, LShort, LLong))
     assertResult(false)(LInt(42) == LLong(42))

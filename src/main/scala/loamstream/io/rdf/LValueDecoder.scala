71882122277c345c1398a331a2f99fd6d02a6c47
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-04-26 10:46:33 -0400

    LValue encoder for all atomic LValues (primitives, String) that are represented as RDF literals.

---
+++ src/main/scala/loamstream/io/rdf/LValueDecoder.scala
@@ -0,0 +1,45 @@
+package loamstream.io.rdf
+
+import loamstream.io.LIO
+import loamstream.io.rdf.RedFern.Decoder
+import loamstream.model.values.LType._
+import loamstream.model.values.LValue
+import loamstream.util.shot.{Hit, Miss, Shot}
+import org.openrdf.model.vocabulary.{RDF, XMLSchema}
+import org.openrdf.model.{Literal, Resource, Value, ValueFactory}
+import org.openrdf.repository.RepositoryConnection
+
+/**
+  * LoamStream - Language for Omics Analysis Management
+  * Created by oruebenacker on 4/26/16.
+  */
+object LValueDecoder extends Decoder[LValue] {
+  // scalastyle:off cyclomatic.complexity
+  override def decode(io: LIO[RepositoryConnection, Value, ValueFactory], rdfNode: Value): Shot[LValue] = {
+    rdfNode match {
+      case literal: Literal =>
+        val dataType = literal.getDatatype
+        dataType match {
+          case XMLSchema.BOOLEAN => Hit(LBoolean(literal.booleanValue()))
+          case XMLSchema.DOUBLE => Hit(LDouble(literal.doubleValue()))
+          case XMLSchema.FLOAT => Hit(LFloat(literal.floatValue()))
+          case XMLSchema.LONG => Hit(LLong(literal.longValue()))
+          case XMLSchema.INT => Hit(LInt(literal.intValue()))
+          case XMLSchema.SHORT => Hit(LShort(literal.shortValue()))
+          case Loam.char => Hit(LChar(literal.intValue().toChar))
+          case XMLSchema.BYTE => Hit(LByte(literal.byteValue()))
+          case XMLSchema.STRING => Hit(LString(literal.stringValue()))
+          case Loam.variantId => Hit(LVariantId(literal.stringValue()))
+          case Loam.sampleId => Hit(LSampleId(literal.stringValue()))
+          case _ => Miss(s"Don't know how to decode data type '$dataType'.")
+        }
+      case resource: Resource =>
+        RdfQueries.findUniqueObject(resource, RDF.TYPE)(io.conn).flatMap({
+          case rdfType: Resource => ???
+          case literal: Literal => Miss(s"Need resource as RDF type, but got Literal '$literal'.")
+        })
+    }
+  }
+
+  // scalastyle:on cyclomatic.complexity
+}

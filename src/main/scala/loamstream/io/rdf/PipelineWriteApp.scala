eadd042c1a38682b7a9bf9435fc5527f32353219
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-04-27 12:55:14 -0400

    Partially successful attempts at pretty printing Turtle.

--- src/main/scala/loamstream/io/rdf/PipelineWriteApp.scala
+++ src/main/scala/loamstream/io/rdf/PipelineWriteApp.scala
@@ -3,9 +3,14 @@ package loamstream.io.rdf
 import java.io.FileOutputStream
 import java.nio.file.{Path, Paths}
 
+import loamstream.apps.hail.HailPipeline
 import loamstream.apps.minimal.MiniPipeline
 import loamstream.model.LPipeline
-import org.openrdf.rio.{RDFFormat, Rio}
+import loamstream.pipelines.qc.ancestry.AncestryInferencePipeline
+import loamstream.tools.core.LCoreDefaultPileIds
+import org.openrdf.rio.helpers.{BasicWriterSettings, BufferedGroupingRDFHandler}
+import org.openrdf.rio.turtle.TurtleWriter
+import org.openrdf.rio.{RDFFormat, Rio, RioSetting}
 
 /**
   * LoamStream - Language for Omics Analysis Management
@@ -18,11 +23,16 @@ object PipelineWriteApp extends App {
     val conn = repo.getConnection
     val redFern = RedFern(conn)
     redFern.write(pipeline)(LPipelineEncoder)
-    val writer = Rio.createWriter(RDFFormat.TURTLE, new FileOutputStream(path.toFile))
-    conn.export(writer)
+    var writer = Rio.createWriter(RDFFormat.TURTLE, new FileOutputStream(path.toFile))
+    writer = writer.set[java.lang.Boolean](BasicWriterSettings.PRETTY_PRINT, true)
+    val bufferedHandler = new BufferedGroupingRDFHandler(writer)
+    conn.export(bufferedHandler)
     conn.close()
   }
 
-  val file = Paths.get("mini.rdf")
-  writePipeline(MiniPipeline.apply("genotypes"), file)
+  writePipeline(MiniPipeline(LCoreDefaultPileIds.genotypes), Paths.get("mini.ttl"))
+  writePipeline(HailPipeline(LCoreDefaultPileIds.genotypes, LCoreDefaultPileIds.vds,
+    LCoreDefaultPileIds.singletons), Paths.get("hail.ttl"))
+  writePipeline(AncestryInferencePipeline(LCoreDefaultPileIds.genotypes, LCoreDefaultPileIds.pcaWeights),
+    Paths.get("ancestry.ttl"))
 }

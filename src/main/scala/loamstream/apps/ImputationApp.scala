deaa536707afe2df837651526d2f2e0a627b4189
Kaan Yuksel <kyuksel@broadinstitute.org>
2016-05-06 13:16:19 -0400

    Converted hardcoded ShapeIt related paths to those parsed from an external conf file

--- src/main/scala/loamstream/apps/ImputationApp.scala
+++ src/main/scala/loamstream/apps/ImputationApp.scala
@@ -15,28 +15,52 @@ import scala.concurrent.{Future, ExecutionContext}
   * Created by kyuksel on 05/02/2016.
   */
 object ImputationApp extends Loggable {
-  final case class ShapeItJob(inputs: Set[LJob]) extends LJob {
+  final case class ShapeItJob(inputs: Set[LJob], configFile: String) extends LJob {
     override def execute(implicit context: ExecutionContext): Future[Result] = {
       Future {
-        val shapeItExecutable = ImputationConfig.shapeItExecutable
-        val vcf = ImputationConfig.shapeItVcfFile
-        val map = ImputationConfig.shapeItMapFile
-        val haps = ImputationConfig.shapeItHapFile
-        val samples = ImputationConfig.shapeItSampleFile
-        val log = ImputationConfig.shapeItLogFile
-        val numThreads = ImputationConfig.shapeItNumThreads
+        val config = ImputationConfig(configFile)
+        val shapeItExecutable = config.shapeItExecutable
+        val vcf = config.shapeItVcfFile
+        val map = config.shapeItMapFile
+        val haps = config.shapeItHapFile
+        val samples = config.shapeItSampleFile
+        val log = config.shapeItLogFile
+        val numThreads = config.shapeItNumThreads.toInt
 
         val command = ImputationTools.getShapeItCmdLine(shapeItExecutable, vcf, map, haps, samples, log, numThreads)
         ImputationTools.execute(command)
-        SimpleSuccess(s"Phased VCF $vcf")
+        SimpleSuccess(s"Phased the VCF $vcf")
       }
     }
   }
 
+  // TODO: Replace with command line interface
+  def CheckIfArgsValid(args: Array[String]): Unit = {
+    val configHelpText = "Please provide path to config file in the form: --config <path> or -c <path>"
+
+    var isArgsValid = false
+    val longConfigOptionName = "--config"
+    val shortConfigOptionName = "-c"
+
+    if (args.size != 2) {
+      error(configHelpText)
+      System.exit(-1)
+    }
+
+    args(0) match {
+      case `longConfigOptionName` => isArgsValid = true
+      case `shortConfigOptionName` => isArgsValid = true
+      case _ => error(configHelpText) ; System.exit(-1)
+    }
+  }
+
   def main(args: Array[String]) {
+    CheckIfArgsValid(args)
+
     trace("Attempting to run ShapeIt...")
 
-    val shapeItJob = ShapeItJob(Set.empty)
+    val configFile = args(1)
+    val shapeItJob = ShapeItJob(Set.empty, args(1))
     val executable = LExecutable(Set(shapeItJob))
     val result = MiniExecuter.execute(executable)
   }
--- src/main/scala/loamstream/conf/ImputationConfig.scala
+++ src/main/scala/loamstream/conf/ImputationConfig.scala
@@ -1,18 +1,30 @@
 package loamstream.conf
 
+import java.io.File
+
+import com.typesafe.config.ConfigFactory
+
 /**
  * Created on: 5/4/16 
  * @author Kaan Yuksel 
  */
-object ImputationConfig {
-  val shapeItExecutable = "/humgen/diabetes/users/ryank/software/shapeit/bin/shapeit"
-  
-  val shapeItBaseDir = "/humgen/diabetes/users/kyuksel/imputation/shapeit_example/"
-  val shapeItVcfFile = shapeItBaseDir + "gwas.vcf.gz"
-  val shapeItMapFile = shapeItBaseDir + "genetic_map.txt.gz"
-  val shapeItHapFile = shapeItBaseDir + "gwas.phased.haps.gz"
-  val shapeItSampleFile = shapeItBaseDir + "gwas.phased.sample.gz"
-  val shapeItLogFile = shapeItBaseDir + "gwas.phased.log"
+case class ImputationConfig(configFile: String) {
+  val imputationConfig = ConfigFactory.parseFile(new File(configFile))
+  val props = TypesafeConfigLproperties(ConfigFactory.load(imputationConfig))
+  val shapeItBaseKey = "imputation.shapeit"
+  val shapeItExecutableKey = s"$shapeItBaseKey.executable"
+  val shapeItVcfFileKey = s"$shapeItBaseKey.vcfFile"
+  val shapeItMapFileKey = s"$shapeItBaseKey.mapFile"
+  val shapeItHapFileKey = s"$shapeItBaseKey.hapFile"
+  val shapeItSampleFileKey = s"$shapeItBaseKey.sampleFile"
+  val shapeItLogFileKey = s"$shapeItBaseKey.logFile"
+  val shapeItNumThreadsKey = s"$shapeItBaseKey.numThreads"
 
-  val shapeItNumThreads = 8
+  val shapeItExecutable = props.getString(shapeItExecutableKey).get
+  val shapeItVcfFile = props.getString(shapeItVcfFileKey).get
+  val shapeItMapFile = props.getString(shapeItMapFileKey).get
+  val shapeItHapFile = props.getString(shapeItHapFileKey).get
+  val shapeItSampleFile = props.getString(shapeItSampleFileKey).get
+  val shapeItLogFile = props.getString(shapeItLogFileKey).get
+  val shapeItNumThreads = props.getString(shapeItNumThreadsKey).get
 }

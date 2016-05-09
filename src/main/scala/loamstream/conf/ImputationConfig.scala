4c53d85831d4c6e6b3034d4d5f1a8b495a78f18d
Kaan Yuksel <kyuksel@broadinstitute.org>
2016-05-09 12:30:11 -0400

    Improvements for imputation configuration code

--- src/main/scala/loamstream/conf/ImputationConfig.scala
+++ src/main/scala/loamstream/conf/ImputationConfig.scala
@@ -1,6 +1,7 @@
 package loamstream.conf
 
 import java.io.File
+import java.nio.file.Paths
 
 import com.typesafe.config.ConfigFactory
 
@@ -9,7 +10,7 @@ import com.typesafe.config.ConfigFactory
  * @author Kaan Yuksel 
  */
 case class ImputationConfig(configFile: String) {
-  val imputationConfig = ConfigFactory.parseFile(new File(configFile))
+  val imputationConfig = ConfigFactory.parseFile(new File(configFile)).withFallback(ConfigFactory.load("loamstream"))
   val props = TypesafeConfigLproperties(ConfigFactory.load(imputationConfig))
   val shapeItBaseKey = "imputation.shapeit"
   val shapeItWorkDirKey = s"$shapeItBaseKey.workDir"
@@ -23,10 +24,10 @@ case class ImputationConfig(configFile: String) {
 
   val shapeItWorkDir = props.getString(shapeItWorkDirKey).get
   val shapeItExecutable = props.getString(shapeItExecutableKey).get
-  val shapeItVcfFile = shapeItWorkDir + props.getString(shapeItVcfFileKey).get
-  val shapeItMapFile = shapeItWorkDir + props.getString(shapeItMapFileKey).get
-  val shapeItHapFile = shapeItWorkDir + props.getString(shapeItHapFileKey).get
-  val shapeItSampleFile = shapeItWorkDir + props.getString(shapeItSampleFileKey).get
-  val shapeItLogFile = shapeItWorkDir + props.getString(shapeItLogFileKey).get
+  val shapeItVcfFile = Paths.get(shapeItWorkDir, props.getString(shapeItVcfFileKey).get).toString
+  val shapeItMapFile = Paths.get(shapeItWorkDir, props.getString(shapeItMapFileKey).get).toString
+  val shapeItHapFile = Paths.get(shapeItWorkDir, props.getString(shapeItHapFileKey).get).toString
+  val shapeItSampleFile = Paths.get(shapeItWorkDir, props.getString(shapeItSampleFileKey).get).toString
+  val shapeItLogFile = Paths.get(shapeItWorkDir, props.getString(shapeItLogFileKey).get).toString
   val shapeItNumThreads = props.getString(shapeItNumThreadsKey).get
 }

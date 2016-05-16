633b48a030eb5b3ab00360364dee04b28c67b2e4
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-05-16 10:36:09 -0400

    Replacing ProviderById with (String) => T and Provider with () => T.

--- src/main/scala/loamstream/tools/core/LCoreEnv.scala
+++ src/main/scala/loamstream/tools/core/LCoreEnv.scala
@@ -17,13 +17,13 @@ object LCoreEnv {
 
   object Keys {
     val nameOfThisObject = "loamstream.tools.core.LCoreEnv.Keys"
-    val vcfFilePath = Key[PathProviderById]("VCF file path")
-    val sampleFilePath = Key[PathProvider]("sample file path")
-    val singletonFilePath = Key[PathProvider]("singleton file path")
-    val pcaWeightsFilePath = Key[PathProvider]("PCA weights file path")
+    val vcfFilePath = Key[String => Path]("VCF file path")
+    val sampleFilePath = Key[() => Path]("sample file path")
+    val singletonFilePath = Key[() => Path]("singleton file path")
+    val pcaWeightsFilePath = Key[() => Path]("PCA weights file path")
     val klustaKwikKonfig = Key[KlustaKwikKonfig]("KlustaKwik konfig")
-    val pcaProjectionsFilePath = Key[PathProvider]("PCA projections file path")
-    val clusterFilePath = Key[PathProvider]("PCA projections file path")
+    val pcaProjectionsFilePath = Key[() => Path]("PCA projections file path")
+    val clusterFilePath = Key[() => Path]("PCA projections file path")
     val genotypesId = Key[String]("genotypes id")
     val vdsId = Key[String]("variant dataset")
     val singletonsId = Key[String]("singleton counts")
@@ -35,64 +35,41 @@ object LCoreEnv {
 
     def path(pathString: String): Path = Paths.get(pathString)
 
-    def tempFile(prefix: String, suffix: String): PathProvider = tempFileProvider(prefix, suffix)
+    def tempFile(prefix: String, suffix: String): () => Path = tempFileProvider(prefix, suffix)
 
-    def tempDir(prefix: String): PathProvider = tempDirProvider(prefix)
+    def tempDir(prefix: String): () => Path = tempDirProvider(prefix)
   }
 
   object Implicits {
     val nameOfThisObject = "loamstream.tools.core.LCoreEnv.Implicits"
 
-    implicit def pathToPathProvider(path: Path): PathProvider = PathProviderConst(path)
+    implicit def toConstantFunction[T](item: T): () => T = () => item
   }
 
-  trait PathProviderById extends (String => Path)
+  def tempFileProvider(prefix: String, suffix: String): () => Path = () => Files.createTempFile(prefix, suffix)
 
-  trait PathProvider {
-    def get: Path
-  }
-
-  case class PathProviderConst(get: Path) extends PathProvider
-
-  def tempFileProvider(prefix: String, suffix: String) = new PathProvider {
-    override def get: Path = Files.createTempFile(prefix, suffix)
-  }
-
-  def tempDirProvider(prefix: String) = new PathProvider {
-    override def get: Path = Files.createTempDirectory(prefix)
-  }
+  def tempDirProvider(prefix: String): () => Path = () => Files.createTempDirectory(prefix)
 
   object FilesInteractive {
-    def envVcf: LEnv = LEnv(Keys.vcfFilePath -> new PathProviderById {
-      override def apply(id: String): Path = FileAsker.ask("VCF file '" + id + "'")
-    })
+    def envVcf: LEnv = LEnv(Keys.vcfFilePath -> ((id: String) => FileAsker.ask("VCF file '" + id + "'")))
 
-    def envSamples: LEnv = LEnv(Keys.sampleFilePath -> new PathProvider {
-      override def get: Path = FileAsker.ask("samples file")
-    })
+    def envSamples: LEnv = LEnv(Keys.sampleFilePath -> (() => FileAsker.ask("samples file")))
 
-    def envSingletons: LEnv = LEnv(Keys.singletonFilePath -> new PathProvider {
-      override def get: Path = FileAsker.ask("singleton counts file")
-    })
+    def envSingletons: LEnv = LEnv(Keys.singletonFilePath -> (() => FileAsker.ask("singleton counts file")))
 
     def env: LEnv = envVcf ++ envSamples ++ envSingletons
   }
 
   object FileInteractiveFallback {
     def envVcf(vcfFiles: Seq[String => Path]): LEnv =
-      LEnv(Keys.vcfFilePath -> new PathProviderById {
-        override def apply(id: String): Path = FileAsker.askIfNotExist(vcfFiles.map(_ (id)))("VCF file '" + id + "'")
-      })
+      LEnv(Keys.vcfFilePath ->
+        ((id: String) => FileAsker.askIfNotExist(vcfFiles.map(_ (id)))("VCF file '" + id + "'")))
 
     def envSamples(sampleFiles: Seq[Path]): LEnv =
-      LEnv(Keys.sampleFilePath -> new PathProvider {
-        override def get: Path = FileAsker.askIfParentDoesNotExist(sampleFiles)("samples file")
-      })
+      LEnv(Keys.sampleFilePath -> (() => FileAsker.askIfParentDoesNotExist(sampleFiles)("samples file")))
 
     def envSingletons(singletonFiles: Seq[Path]): LEnv =
-      LEnv(Keys.singletonFilePath -> new PathProvider {
-        override def get: Path = FileAsker.askIfParentDoesNotExist(singletonFiles)("singleton file")
-      })
+      LEnv(Keys.singletonFilePath -> (() => FileAsker.askIfParentDoesNotExist(singletonFiles)("singleton file")))
 
     def env(vcfFiles: Seq[String => Path], sampleFiles: Seq[Path], singletonFiles: Seq[Path]): LEnv =
       envVcf(vcfFiles) ++ envSamples(sampleFiles) ++ envSingletons(singletonFiles)

1ea4521ff230793384d7b847803ec5029add15f2
ClintAtTheBroad <cgilbert@broadinstitute.org>
2016-05-18 19:42:31 -0400

    Removed no-longer-needed LId subtypes

--- src/main/scala/loamstream/model/LId.scala
+++ src/main/scala/loamstream/model/LId.scala
@@ -6,9 +6,7 @@ import scala.util.Random
   * LoamStream
   * Created by oliverr on 2/26/2016.
   */
-sealed trait LId { self =>
-  def /(name: LId): LId.CompositeId = LId.CompositeId(self, name)
-}
+sealed trait LId
 
 object LId {
 
@@ -22,12 +20,6 @@ object LId {
 
   final case class LAnonId(time: Long, random: Long) extends LId
   
-  final case class CompositeId(namespace: LId, name: LId) extends LId {
-    override def toString = s"$namespace/$name"
-  }
-  
-  case object Unknown extends LId
-
   private val random = new Random
 
   def newAnonId: LAnonId = LAnonId(System.currentTimeMillis, random.nextLong())

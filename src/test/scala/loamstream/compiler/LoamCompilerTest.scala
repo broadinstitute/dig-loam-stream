99d91416bd679d42dccba817eb79cd11f98dc2d1
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-05-24 11:31:56 -0400

    LoamCompilerTest: adding more tests.

--- src/test/scala/loamstream/compiler/LoamCompilerTest.scala
+++ src/test/scala/loamstream/compiler/LoamCompilerTest.scala
@@ -1,9 +1,11 @@
 package loamstream.compiler
 
+import java.io.File
 import java.nio.file.Paths
 
 import loamstream.compiler.ClientMessageHandler.OutMessageSink
 import loamstream.tools.core.LCoreEnv
+import loamstream.util.{ClassPathFinder, SourceUtils, StringUtils}
 import org.scalatest.FunSuite
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -12,8 +14,44 @@ import scala.concurrent.ExecutionContext.Implicits.global
   * LoamStream
   * Created by oliverr on 5/20/2016.
   */
+object LoamCompilerTest {
+
+  object SomeObject
+
+}
+
 class LoamCompilerTest extends FunSuite {
-  test("Testing compilation of various code fragments (only works after running captureSbtClasspath).") {
+  test("Testing resolution ot type name.") {
+    assert(SourceUtils.shortTypeName[LoamCompilerTest] === "LoamCompilerTest")
+    assert(SourceUtils.fullTypeName[LoamCompilerTest] === "loamstream.compiler.LoamCompilerTest")
+    assert(SourceUtils.shortTypeName[LoamCompilerTest.SomeObject.type] === "SomeObject")
+    assert(SourceUtils.fullTypeName[LoamCompilerTest.SomeObject.type] ===
+      "loamstream.compiler.LoamCompilerTest.SomeObject")
+  }
+  test("Testing sanity of classpath obtained from classloader.") {
+    val classpath = ClassPathFinder.getClassPath(this)
+    assert(classpath.split(File.pathSeparator).length > 5)
+    assert(classpath.contains("scala-library"))
+    assert(classpath.contains("scala-compiler"))
+    assert(classpath.contains("scala-reflect"))
+    assert(classpath.contains("htsjdk"))
+    assert(classpath.contains("logback"))
+  }
+  test("Testing compilation of legal code fragment with no settings.") {
+    val compiler = new LoamCompiler(OutMessageSink.NoOp)(global)
+    val code =
+      """
+     val hello = "Hello!"
+     println(hello.replace("!", "?").size)
+      """
+    val result = compiler.compile(code)
+    assert(result.errors.isEmpty)
+    assert(result.warnings.isEmpty)
+    assert(result.envOpt.nonEmpty)
+    val env = result.envOpt.get
+    assert(env.size === 0)
+  }
+  test("Testing compilation of legal code fragment with five settings.") {
     val compiler = new LoamCompiler(OutMessageSink.NoOp)(global)
     val code =
       """
@@ -36,5 +74,16 @@ class LoamCompilerTest extends FunSuite {
     assert(env.get(LCoreEnv.Keys.pcaProjectionsFilePath).nonEmpty)
     assert(env.get(LCoreEnv.Keys.klustaKwikKonfig).nonEmpty)
     assert(env.size === 5)
+  }
+  test("Testing that compilation of illegal code fragment causes compile errors.") {
+    val compiler = new LoamCompiler(OutMessageSink.NoOp)(global)
+    val code =
+      """
+    The enlightened soul is a person who is self-conscious of his "human condition" in his time and historical
+    and social setting, and whose awareness inevitably and necessarily gives him a sense of social responsibility.
+      """
+    val result = compiler.compile(code)
+    assert(result.errors.nonEmpty)
+    assert(result.envOpt.isEmpty)
   }
 }

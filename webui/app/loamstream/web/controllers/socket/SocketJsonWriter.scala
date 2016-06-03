c9022f1d3f5beb339f4ae820bdefbfc4267dcb61
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-06-03 18:02:27 -0400

    Sample file can now be loaded.

--- webui/app/loamstream/web/controllers/socket/SocketJsonWriter.scala
+++ webui/app/loamstream/web/controllers/socket/SocketJsonWriter.scala
@@ -1,6 +1,6 @@
 package loamstream.web.controllers.socket
 
-import loamstream.compiler.ClientOutMessage
+import loamstream.compiler.{ClientOutMessage, LoadResponseMessage}
 import play.api.libs.json.{JsValue, Json, Writes}
 
 /**
@@ -8,8 +8,17 @@ import play.api.libs.json.{JsValue, Json, Writes}
   * Created by oliverr on 5/9/2016.
   */
 object SocketJsonWriter extends Writes[ClientOutMessage] {
-  override def writes(message: ClientOutMessage): JsValue = Json.obj(
-    "type" -> message.typeName,
-    "message" -> message.message
-  )
+  override def writes(message: ClientOutMessage): JsValue = message match {
+    case LoadResponseMessage(name, content) =>
+      Json.obj(
+        "type" -> message.typeName,
+        "message" -> message.message,
+        "content" -> content
+      )
+    case _ =>
+      Json.obj(
+        "type" -> message.typeName,
+        "message" -> message.message
+      )
+  }
 }

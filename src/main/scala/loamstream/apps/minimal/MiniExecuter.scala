330c06a863f69f2ad96587983e439bb4a68b99ca
ClintAtTheBroad <cgilbert@broadinstitute.org>
2016-06-06 18:12:26 -0400

    New failure mode for MiniExecuter: Stop processing dependent jobs on first failure, report ensuing failures for all parent jobs

--- src/main/scala/loamstream/apps/minimal/MiniExecuter.scala
+++ src/main/scala/loamstream/apps/minimal/MiniExecuter.scala
@@ -1,18 +1,17 @@
 package loamstream.apps.minimal
 
+import scala.annotation.tailrec
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
-
-
 import loamstream.model.execute.{ LExecutable, LExecuter }
 import loamstream.model.jobs.LJob
 import loamstream.model.jobs.LJob.Result
-import loamstream.util.Maps
 import loamstream.util.{ Hit, Shot }
 import loamstream.util.Loggable
+import loamstream.util.Maps
 
 /**
  * RugLoom - A prototype for a pipeline building toolkit
@@ -21,11 +20,8 @@ import loamstream.util.Loggable
 object MiniExecuter extends LExecuter with Loggable {
 
   override def execute(executable: LExecutable)(implicit timeout: Duration = Duration.Inf): Map[LJob, Shot[Result]] = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-
     import Maps.Implicits._
-    
-    //debug(s"MiniExecuter got ${executable.jobs.size} jobs")
+    import scala.concurrent.ExecutionContext.Implicits.global
     
     def toShotMap(m: Map[LJob, Result]): Map[LJob, Shot[Result]] = m.strictMapValues(Hit(_))
     
@@ -42,34 +38,46 @@ object MiniExecuter extends LExecuter with Loggable {
 
     //NB: Convert the iterator to an IndexedSeq to force evaluation, and make sure 
     //input jobs are evaluated before jobs that depend on them.
-    val futureSubResults = Future.sequence(subResultFutures).map(_.takeWhile(noFailures).toIndexedSeq)
+    val futureSubResults = Future.sequence(subResultFutures).map(consumeUntilFirstFailure)
 
     futureSubResults.map(Maps.mergeMaps)
+  }
+  
+  private[minimal] def consumeUntilFirstFailure(iter: Iterator[Map[LJob, Result]]): IndexedSeq[Map[LJob, Result]] = {
+    @tailrec
+    def loop(acc: IndexedSeq[Map[LJob, Result]]): IndexedSeq[Map[LJob, Result]] = {
+      if(iter.isEmpty) { acc }
+      else {
+        val m = iter.next()
+      
+        val shouldKeepGoing = noFailures(m)
+        
+        val newAcc = acc :+ m
+        
+        if(shouldKeepGoing) { loop(newAcc) }
+        else { newAcc }
+      }
+    }
+    
+    loop(Vector.empty)
   }
   
   private[minimal] def exec(job: LJob)(implicit executor: ExecutionContext): Future[Map[LJob, Result]] = {
     for {
       inputResults <- execJobs(job.inputs)
-      resultMapping <- executeLeaf(job)
+      resultMapping <- execSingle(job)
     } yield {
-      inputResults ++ resultMapping 
+      inputResults ++ resultMapping
     }
   }
   
-  private[minimal] def noFailures[J <: LJob](m: Map[J, Result]): Boolean = {
-    m.values.forall(_.isSuccess)
-  }
+  private[minimal] def noFailures[J <: LJob](m: Map[J, Result]): Boolean = m.values.forall(_.isSuccess)
 
-  private[minimal] def executeLeaf(job: LJob)(implicit executor: ExecutionContext): Future[Map[LJob, Result]] = {
+  private[minimal] def execSingle(job: LJob)(implicit executor: ExecutionContext): Future[Map[LJob, Result]] = {
     for {
       result <- job.execute
     } yield {
-      if (result.isSuccess) { Map(job -> result) }
-      else { 
-        //warn(s"Discarding failure $result encountered when running $job")
-        
-        Map.empty[LJob, Result] 
-      }
+      Map(job -> result)
     }
   }
 }
--- src/test/scala/loamstream/apps/minimal/MiniExecuterTest.scala
+++ src/test/scala/loamstream/apps/minimal/MiniExecuterTest.scala
@@ -1,128 +1,33 @@
 package loamstream.apps.minimal
 
 import scala.concurrent.Await
-import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
-import org.scalatest.FunSuite
-
-import loamstream.model.execute.LExecutable
+import loamstream.model.execute.ExecuterTest
+import loamstream.model.execute.LExecuter
 import loamstream.model.jobs.LJob
 import loamstream.model.jobs.LJob.Result
 import loamstream.model.jobs.LJob.SimpleFailure
-import loamstream.model.jobs.LJob.SimpleSuccess
-import loamstream.util.Hit
 
 /**
  * @author clint
  * date: Apr 12, 2016
  */
-final class MiniExecuterTest extends FunSuite {
-  private final case class MockLJob(inputs: Set[LJob], toReturn: LJob.Result) extends LJob {
-    override def execute(implicit context: ExecutionContext): Future[Result] = Future.successful(toReturn)
+final class MiniExecuterTest extends ExecuterTest {
+  
+  override def makeExecuter: LExecuter = MiniExecuter
+  
+  test("execSingle()") {
+    import MiniExecuter.execSingle
     
-    override def withInputs(newInputs: Set[LJob]): LJob = copy(inputs = newInputs) 
-  }
-
-  private val two0Success = SimpleSuccess("2(0)")
-  private val two1Success = SimpleSuccess("2(1)")
-
-  private val twoPlusTwoSuccess = SimpleSuccess("2 + 2")
-
-  private val plusOneSuccess = SimpleSuccess("(2 + 2) + 1")
-  
-  private val two0Failure = SimpleFailure("2(0)")
-  private val two1Failure = SimpleFailure("2(1)")
-
-  private val twoPlusTwoFailure = SimpleFailure("2 + 2")
-
-  private val plusOneFailure = SimpleFailure("(2 + 2) + 1")
-
-  private val two0 = MockLJob(Set.empty, two0Success)
-  private val two1 = MockLJob(Set.empty, two1Success)
-
-  private val twoPlusTwo = MockLJob(Set(two0, two1), twoPlusTwoSuccess)
-
-  private val plusOne = MockLJob(Set(twoPlusTwo), plusOneSuccess)
-  
-  private val two0Failed = MockLJob(Set.empty, two0Failure)
-  private val two1Failed = MockLJob(Set.empty, two1Failure)
-
-  private val twoPlusTwoFailed = MockLJob(Set(two0Failed, two1Failed), twoPlusTwoFailure)
-
-  private val plusOneFailed = MockLJob(Set(twoPlusTwoFailed), plusOneFailure)
-
-  import scala.concurrent.ExecutionContext.Implicits.global
-  
-  test("execute() should work if all sub-jobs succeed") {
-
-    val executable = LExecutable(Set(plusOne))
-
-    val result = MiniExecuter.execute(executable)
-
-    val expected = Map(
-      two0 -> Hit(two0Success),
-      two1 -> Hit(two1Success),
-      twoPlusTwo -> Hit(twoPlusTwoSuccess),
-      plusOne -> Hit(plusOneSuccess))
-
-    assert(result === expected)
-  }
-  
-  test("execute() should work if no sub-jobs succeed") {
-
-    val executable = LExecutable(Set(plusOneFailed))
-
-    val result = MiniExecuter.execute(executable)
-
-    val expected = Map.empty
-
-    assert(result === expected)
-  }
-  
-  test("execute() should work if some (early) sub-jobs fail") {
-
-    val twoPlusTwo = MockLJob(Set(two0Failed, two1Failed), twoPlusTwoFailure)
-
-    val plusOne = MockLJob(Set(twoPlusTwo), plusOneFailure)
-    
-    val executable = LExecutable(Set(plusOneFailed))
-
-    val result = MiniExecuter.execute(executable)
-
-    val expected = Map.empty
-
-    assert(result === expected)
-  }
-  
-  test("execute() should work if some (late) sub-jobs fail") {
-
-    val twoPlusTwo = MockLJob(Set(two0, two1), twoPlusTwoFailure)
-
-    val plusOne = MockLJob(Set(twoPlusTwo), plusOneFailure)
-    
-    val executable = LExecutable(Set(plusOne))
-
-    val result = MiniExecuter.execute(executable)
-
-    val expected = Map(
-        two0 -> Hit(two0Success),
-        two1 -> Hit(two1Success))
-
-    assert(result === expected)
-  }
-  
-  test("executeLeaf()") {
-    import MiniExecuter.executeLeaf
-    
-    val success = Await.result(executeLeaf(two0), Duration.Inf)
+    val success = Await.result(execSingle(two0), Duration.Inf)
     
     assert(success === Map(two0 -> two0Success))
     
-    val failure = Await.result(executeLeaf(two0Failed), Duration.Inf)
+    val failure = Await.result(execSingle(two0Failed), Duration.Inf)
     
-    assert(failure === Map.empty)
+    assert(failure === Map(two0Failed -> two0Failure))
   }
   
   test("noFailures()") {
@@ -153,5 +58,33 @@ final class MiniExecuterTest extends FunSuite {
       plusOne -> SimpleFailure("blerg"))
       
     assert(noFailures(someFailures) === false)
+  }
+  
+  test("consumeUntilFirstFailure()") {
+    import MiniExecuter.consumeUntilFirstFailure
+    
+    assert(consumeUntilFirstFailure(Iterator.empty) == Vector.empty)
+    
+    val oneSuccess: Map[LJob, Result] = Map(two0 -> two0Success)
+    val anotherSuccess: Map[LJob, Result] = Map(two1 -> two1Success)
+    
+    val oneFailure: Map[LJob, Result] = Map(two0Failed -> two0Failure)
+    val anotherFailure: Map[LJob, Result] = Map(two1Failed -> two1Failure)
+    
+    assert(consumeUntilFirstFailure(Iterator(oneSuccess)) == Vector(oneSuccess))
+    
+    assert(consumeUntilFirstFailure(Iterator(oneSuccess, anotherSuccess)) == Vector(oneSuccess, anotherSuccess))
+    
+    assert(consumeUntilFirstFailure(Iterator(oneFailure)) == Vector(oneFailure))
+    
+    assert(consumeUntilFirstFailure(Iterator(oneFailure, anotherFailure)) == Vector(oneFailure))
+    
+    assert(consumeUntilFirstFailure(Iterator(oneSuccess, oneFailure)) == Vector(oneSuccess, oneFailure))
+    
+    assert(consumeUntilFirstFailure(Iterator(oneSuccess, anotherSuccess, oneFailure)) == 
+      Vector(oneSuccess, anotherSuccess, oneFailure))
+    
+    assert(consumeUntilFirstFailure(Iterator(oneSuccess, anotherSuccess, oneFailure, anotherFailure)) == 
+      Vector(oneSuccess, anotherSuccess, oneFailure))
   }
 }

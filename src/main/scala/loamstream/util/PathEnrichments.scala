3f98a443f2e9de0dc1aab0629eba0f666ff3d777
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-06-07 09:07:32 -0400

    Fixing path resolution.

--- src/main/scala/loamstream/util/PathEnrichments.scala
+++ src/main/scala/loamstream/util/PathEnrichments.scala
@@ -1,14 +1,15 @@
 package loamstream.util
 
 import java.nio.file.Path
-import java.nio.file.Paths
 
 /**
- * @author clint
- * date: Jun 1, 2016
- */
+  * @author clint
+  *         date: Jun 1, 2016
+  */
 object PathEnrichments {
+
   final implicit class PathHelpers(val path: Path) extends AnyVal {
-    def /(next: String): Path = Paths.get(path.toString, next)
+    def /(next: String): Path = path.resolve(next)
   }
+
 }

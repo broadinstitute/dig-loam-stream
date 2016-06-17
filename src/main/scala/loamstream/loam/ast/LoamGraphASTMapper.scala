017f24bfe27dae3a4de7286bcae341dabd88c11d
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-06-17 08:48:34 -0400

    Mapping LoamStore => Store

--- src/main/scala/loamstream/loam/ast/LoamGraphASTMapper.scala
+++ src/main/scala/loamstream/loam/ast/LoamGraphASTMapper.scala
@@ -1,13 +1,19 @@
 package loamstream.loam.ast
 
+import java.nio.file.Files
+
+import loamstream.loam.LoamGraph.StoreEdge
+import loamstream.loam.files.FileSuffixes
 import loamstream.loam.{LoamGraph, LoamStore, LoamTool}
-import loamstream.model.Tool
+import loamstream.model.{FileStore, StoreSpec, Tool}
 
 /**
   * LoamStream
   * Created by oliverr on 6/16/2016.
   */
 object LoamGraphASTMapper {
+
+  val tempFilePrefix = "loam"
 
   case class TaskResult(mapping: LoamGraphASTMapping, moreTasks: Set[Task])
 
@@ -16,15 +22,28 @@ object LoamGraphASTMapper {
   }
 
   case class MapStoreTask(loamStore: LoamStore) extends Task {
-    override def perform(loamGraphASTMapping: LoamGraphASTMapping): TaskResult = ??? // TODO
+    override def perform(mapping: LoamGraphASTMapping): TaskResult = {
+      val storeSpec = StoreSpec(loamStore.tpe)
+      val path = mapping.graph.storeSources.get(loamStore) match {
+        case Some(StoreEdge.PathEdge(inputPath)) => inputPath
+        case _ => mapping.graph.storeSinks.get(loamStore) match {
+          case Some(StoreEdge.PathEdge(outputPath)) => outputPath
+          case _ =>
+            val suffix = FileSuffixes(loamStore.tpe)
+            Files.createTempFile(tempFilePrefix, suffix)
+        }
+      }
+      val store = FileStore(path, storeSpec)
+      TaskResult(mapping.withStore(loamStore, store), Set.empty)
+    }
   }
 
   case class MapToolTask(loamTool: LoamTool) extends Task {
-    override def perform(loamGraphASTMapping: LoamGraphASTMapping): TaskResult = ??? // TODO
+    override def perform(mapping: LoamGraphASTMapping): TaskResult = ??? // TODO
   }
 
   case class MapToolASTTask(tool: Tool) extends Task {
-    override def perform(loamGraphASTMapping: LoamGraphASTMapping): TaskResult = ??? // TODO
+    override def perform(mapping: LoamGraphASTMapping): TaskResult = ??? // TODO
   }
 
   def newMapping(graph: LoamGraph): LoamGraphASTMapping = {
@@ -39,7 +58,7 @@ object LoamGraphASTMapper {
       val task = tasksCurrent.head
       val taskResult = task.perform(mapping)
       mapping = taskResult.mapping
-      tasksNext ++= taskResult.moreTasks
+      tasksNext ++= (taskResult.moreTasks -- (tasksCurrent - task))
     }
     mapping
   }

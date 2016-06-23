ff3773328a138b81bd448f79a52b32dc2a6ec1cd
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-06-23 10:47:53 -0400

    Some minor requested fixes.

--- src/main/scala/loamstream/model/jobs/commandline/CommandLineStringJob.scala
+++ src/main/scala/loamstream/model/jobs/commandline/CommandLineStringJob.scala
@@ -11,6 +11,8 @@ import scala.sys.process.{Process, ProcessBuilder, ProcessLogger}
   * LoamStream
   * Created by oliverr on 6/21/2016.
   */
+
+/** A job based on a command line provided as a String */
 case class CommandLineStringJob(commandLineString: String, workDir: Path, inputs: Set[LJob] = Set.empty,
                                 exitValueCheck: Int => Boolean = CommandLineJob.acceptAll,
                                 override val logger: ProcessLogger = noOpProcessLogger)
@@ -18,4 +20,6 @@ case class CommandLineStringJob(commandLineString: String, workDir: Path, inputs
   override def processBuilder: ProcessBuilder = Process(commandLineString, workDir.toFile)
 
   override def withInputs(newInputs: Set[LJob]): LJob = copy(inputs = newInputs)
+
+  override def exitValueIsOk(exitValue: Int): Boolean = exitValueCheck(exitValue)
 }

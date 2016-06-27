03513fa2e9aa7d1fb9ae2dfb5f71145046d1190a
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-06-27 11:12:45 -0400

    Added LoamComboRepository

---
+++ src/main/scala/loamstream/compiler/LoamComboRepository.scala
@@ -0,0 +1,13 @@
+package loamstream.compiler
+
+import loamstream.util.{Shot, Shots}
+
+/**
+  * LoamStream
+  * Created by oliverr on 6/27/2016.
+  */
+case class LoamComboRepository(repos: Seq[LoamRepository]) extends LoamRepository {
+  override def list: Seq[String] = repos.flatMap(_.list)
+
+  override def get(name: String): Shot[String] = Shots.findHit[LoamRepository, String](repos, _.get(name))
+}

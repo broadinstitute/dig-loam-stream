cce58968cdc2f168e539c6ce69bcc444393bf311
Kaan Yuksel <kyuksel@broadinstitute.org>
2016-07-01 17:55:22 -0400

    Script building to submit UGER task arrays

--- src/main/scala/loamstream/conf/ShapeItConfig.scala
+++ src/main/scala/loamstream/conf/ShapeItConfig.scala
@@ -15,12 +15,15 @@ import loamstream.util.ConfigEnrichments
  * date: Jun 13, 2016
  */
 final case class ShapeItConfig(
-    workDir: Path, 
-    executable: Path, 
-    script: Path,
-    mapFile: Path, 
-    logFile: Path, 
-    numThreads: Int)
+                                workDir: Path,
+                                executable: Path,
+                                script: Path,
+                                vcfFile: Path,
+                                mapFile: Path,
+                                sampleFile: Path,
+                                hapFile: Path,
+                                logFile: Path,
+                                numThreads: Int)
     
 object ShapeItConfig extends ConfigCompanion[ShapeItConfig] {
   
@@ -28,7 +31,10 @@ object ShapeItConfig extends ConfigCompanion[ShapeItConfig] {
     val workDirKey = "workDir"
     val executableKey = "executable"
     val scriptKey = "script"
+    val vcfFileKey = "vcfFile"
     val mapFileKey = "mapFile"
+    val sampleFileKey = "sampleFile"
+    val hapFileKey = "hapFile"
     val logFileKey = "logFile"
     val numThreadsKey = "numThreads"
   }
@@ -37,21 +43,27 @@ object ShapeItConfig extends ConfigCompanion[ShapeItConfig] {
     import Keys._
     import PathEnrichments._
     import ConfigEnrichments._
-    
+
     for {
       workDir <- config.tryGetPath(workDirKey)
       executable <- config.tryGetPath(executableKey)
       script <- config.tryGetPath(scriptKey)
+      vcfFile <- workDir / config.tryGetString(vcfFileKey)
       mapFile <- workDir / config.tryGetString(mapFileKey)
+      sampleFile <- workDir / config.tryGetString(sampleFileKey)
+      hapFile <- workDir / config.tryGetString(hapFileKey)
       logFile <- workDir / config.tryGetString(logFileKey)
       numThreads <- config.tryGetInt(numThreadsKey)
     } yield {
       ShapeItConfig(
         workDir, 
         executable, 
-        script, 
-        mapFile, 
-        logFile, 
+        script,
+        vcfFile,
+        mapFile,
+        sampleFile,
+        hapFile,
+        logFile,
         numThreads)
     }
   }
---
+++ src/main/scala/loamstream/uger/ScriptBuilder.scala
@@ -0,0 +1,61 @@
+package loamstream.uger
+
+import loamstream.model.jobs.commandline.CommandLineBuilderJob
+
+/**
+  * @author Kaan
+  *         Date: Jul 1, 2016
+  *
+  * Used to facilitate generation of bash scripts to submit task arrays to UGER
+  * For an example of such scripts, see src/test/resources/imputation/shapeItUgerSubmissionScript.sh
+  */
+object ScriptBuilder {
+  val tab: String = "\t"
+  val newLine: String = "\n"
+  val unixLineSep: String = " \\"
+
+  val scriptHeader: String =
+    s"""#!/bin/bash
+#$$ -cwd
+#$$ -j y
+
+source /broad/software/scripts/useuse
+reuse -q UGER
+
+i=$$SGE_TASK_ID
+      """
+  val endIf: String = s"""${newLine}fi$newLine"""
+
+  def buildFrom(commandLineBuilderJobs: Seq[CommandLineBuilderJob]): String = {
+    val taskIndexStartValue = 1
+    val firstIfBlock = getFirstIfBlock(commandLineBuilderJobs.head, taskIndexStartValue)
+
+    val elseIfBlocks = commandLineBuilderJobs.tail.zipWithIndex.map({case (job, index) =>
+      s"${getElseIfHeader(index + 2)}${getBody(job, s"$newLine$tab")}"}).mkString(newLine)
+
+    s"$scriptHeader$newLine$firstIfBlock$newLine$elseIfBlocks$endIf"
+  }
+
+  def getFirstIfBlock(commandLineBuilderJob: CommandLineBuilderJob, indexStartValue: Int): String = {
+    val ifHeader = getIfHeader(indexStartValue)
+    val ifBody = getBody(commandLineBuilderJob, s"$newLine$tab")
+
+    s"$ifHeader$ifBody"
+  }
+
+  def getBody(commandLineBuilderJob: CommandLineBuilderJob, sep: String): String = {
+    s"""$newLine$tab${commandLineBuilderJob.commandLine.tokens.mkString(s"$unixLineSep$sep")}$unixLineSep"""
+  }
+
+  def getIfHeader(index: Int): String = {
+    s"""if [ $$i -eq $index ]${newLine}then"""
+  }
+
+  def getElseIfHeader(index: Int): String = {
+    s"""elif [ $$i -eq $index ]${newLine}then"""
+  }
+
+  //TODO Should this be moved into Executor?
+  private def numberOfCpuCores: Int = Runtime.getRuntime.availableProcessors
+
+}

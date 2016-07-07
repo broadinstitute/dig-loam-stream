fd85020585add4942caa7479d32fbbd96f87980f
ClintAtTheBroad <cgilbert@broadinstitute.org>
2016-07-07 16:09:00 -0400

    Added comments to ObservableEnrichments

--- src/main/scala/loamstream/util/ObservableEnrichments.scala
+++ src/main/scala/loamstream/util/ObservableEnrichments.scala
@@ -8,21 +8,74 @@ import monix.execution.Cancelable
 /**
  * @author clint
  * date: Jul 1, 2016
+ * 
+ * Extension methods for Monix Observables.  Adds combinators that make some LoamStream use cases easier.
  */
 object ObservableEnrichments {
   final implicit class ObservableOps[A](val obs: Observable[A]) extends AnyVal {
+    /**
+     * Returns an Observable that emits elements that satisfy the passed-in predicate, up to and including the FIRST
+     * element that does not satisy the predicate.  The returned observable completes after emitting the first element 
+     * that satisfies the predicate.
+     * 
+     * For example, for a well-behaved Uger job, say we have
+     * 
+     * val statuses: Obervable[JobStatus] = ... // monitor some job
+     * 
+     * statuses.takeUntil(_.isFinished)
+     * 
+     * will produce an Observable that will emit elements like
+     * 
+     * JobStatus.Queued, JobStatus.Running, JobStatus.Running, ... , JobStatus.Done
+     * 
+     * Or suppose we had
+     * 
+     * val numbers = Observable(1,2,3,4,5,6,7,8,9,10) //an Observable that emits the elements passed to apply()
+     * 
+     * numbers.takeUntil(_ >= 6)
+     * 
+     * Would produce an Observable that yields 
+     * 
+     * 1,2,3,4,5,6
+     * 
+     * Note that this is different from the built-in .takeWhile, which returns elements that satisfy a predicate, BUT
+     * NOT any that don't.
+     * 
+     * numbers.takeWhile(_ < 6) 
+     * 
+     * would return an Observable that yields
+     * 
+     * 1,2,3,4,5
+     * 
+     * or for the job example, 
+     * 
+     * statuses.takeWhile(_.notFinished)
+     * 
+     * would return an Observable that might yield
+     * 
+     * JobStatus.Queued, JobStatus.Running, ..., JobStatus.Running
+     * 
+     * (Note the missing 'terminal' status.)
+     * 
+     * @param p the predicate to use to decide what events to emit
+     * @return an Observable that emits elements that satisfy the passed-in predicate, up to and including the FIRST
+     * element that does not satisy the predicate.
+     */
     def takeUntil(p: A => Boolean)(implicit scheduler: Scheduler): Observable[A] = {
       //TODO: Revisit Overflow Strategy
       Observable.create(OverflowStrategy.Unbounded) { downstream =>
+        def complete() = downstream.onComplete()
+        
         val cancelable = obs.foreach { a =>
           downstream.onNext(a)
           
           if(p(a)) {
-            downstream.onComplete()
+            complete()
           } 
         }
         
-        cancelable.foreach(_ => downstream.onComplete())
+        //TODO: There must be a better way.
+        cancelable.foreach(_ => complete())
         
         cancelable
       }

2faba78dc154b79af4bceec9cc5ae60b1fbfdf9f
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-08-05 05:33:39 -0400

    Addressed review comments, plus more tests.

--- src/main/scala/loamstream/util/ValueBox.scala
+++ src/main/scala/loamstream/util/ValueBox.scala
@@ -5,29 +5,31 @@ class ValueBox[V](valueNew: V) {
 
   @volatile private[this] var _value: V = valueNew
 
+  private[this] val valueLock = new AnyRef
+
   /** Returns the contained value */
-  def value: V = synchronized {
+  def value: V = valueLock.synchronized {
     _value
   }
 
   /** Sets a new value */
-  def value_=(newValue: V): Unit = synchronized {
+  def value_=(newValue: V): Unit = valueLock.synchronized {
     _value = newValue
   }
 
   /** Changes the value by applying a function to it */
-  def apply(f: V => V): ValueBox[V] = synchronized {
+  def apply(f: V => V): ValueBox[V] = valueLock.synchronized {
     _value = f(_value)
     this
   }
 
   /** Get a property of the contained value by applying a function to it */
-  def get[P](g: V => P): P = synchronized {
+  def get[P](g: V => P): P = valueLock.synchronized {
     g(_value)
   }
 
   /** Returns an item by applying a function that also changes the contained value */
-  def create[T](c: V => (V, T)): T = synchronized {
+  def getAndUpdate[T](c: V => (V, T)): T = valueLock.synchronized {
     val (valueNew, item) = c(_value)
     _value = valueNew
     item

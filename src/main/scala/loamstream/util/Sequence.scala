02540a304063f8233284b30a3703e3b7e16e3792
ClintAtTheBroad <cgilbert@broadinstitute.org>
2016-08-18 16:14:27 -0400

    Added comments to Sequence

--- src/main/scala/loamstream/util/Sequence.scala
+++ src/main/scala/loamstream/util/Sequence.scala
@@ -3,10 +3,23 @@ package loamstream.util
 /**
  * @author clint
  * date: Aug 12, 2016
+ * 
+ * A class representing an unbounded sequence of numbers, starting from 'start' and advancing by 'step'.
+ * 'start' and 'step' can be positive, negative, or a combination.
+ * 
+ * Use cases are primarily anonymous IDs. 
+ * 
+ * @param start the first element in this Sequence
+ * @param step the amount to advance the Sequence by
+ * @param N the type of values produced by the sequence.  An instance of scala.Numeric[N] needs to be available.
+ * By default, this means we can make Sequences of Bytes, Shorts, Ints, Longs, Doubles, Floats, and possibly more. 
  */
-final class Sequence[N](start: N, step: N)(implicit evidence: Numeric[N]) {
+final class Sequence[N](val start: N, val step: N)(implicit evidence: Numeric[N]) {
   private val current: ValueBox[N] = ValueBox(start)
   
+  /**
+   * @return the next value in this Sequence
+   */
   def next(): N = {
     def inc(n: N): N = evidence.plus(n, step)
     
@@ -15,5 +28,12 @@ final class Sequence[N](start: N, step: N)(implicit evidence: Numeric[N]) {
 }
 
 object Sequence {
+  /**
+   * Make a Sequence of Ns
+   * 
+   * @param start the first element in this Sequence - default is 0
+   * @param step the amount to advance the Sequence by - default is 1
+   * @param N the type of values produced by the sequence.  An instance of scala.Numeric[N] needs to be available.
+   */
   def apply[N: Numeric](start: N = 0, step: N = 1): Sequence[N] = new Sequence(start, step)
 }

9f49067b5e1e826b3be3935c94ef82fa9d65d82a
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-09-19 11:19:25 -0400

    More tests for ScalaId, part 1

--- src/test/scala/loamstream/util/code/ScalaIdTest.scala
+++ src/test/scala/loamstream/util/code/ScalaIdTest.scala
@@ -7,10 +7,35 @@ import org.scalatest.FunSuite
   * Created by oliverr on 9/16/2016.
   */
 final class ScalaIdTest extends FunSuite {
-  test("PackageId") {
-    assert(RootPackageId.parentOpt === None)
-    val a = PackageId("a")
-    assert(a.parent === RootPackageId)
+  def assertAllNames(id: ScalaId, inScala: String, inJvm: String, inScalaFull: String, inJvmFull: String): Unit = {
+    assert(id.inScala === inScala)
+    assert(id.inJvm === inJvm)
+    assert(id.inScalaFull === inScalaFull)
+    assert(id.inJvmFull === inJvmFull)
+  }
+
+  test("Root package") {
+    assertAllNames(RootPackageId, "_root_", "_root_", "_root_", "_root_")
+  }
+
+  test("Top level Java id") {
+    assertAllNames(PackageId("test"), "test", "test", "test", "test")
+    assertAllNames(TypeId("Test"), "Test", "Test", "Test", "Test")
+    assertAllNames(ObjectId("Test"), "Test", "Test$", "Test", "Test$")
+  }
+
+  test("Top level non-Java id.") {
+    val name = "Hello, World!"
+    val nameWithBackticks = "`Hello, World!`"
+    val nameEncoded = "Hello$u002C$u0020World$bang"
+    val objectNameEncoded = "Hello$u002C$u0020World$bang$"
+    assertAllNames(PackageId(name), nameWithBackticks, nameEncoded, nameWithBackticks, nameEncoded)
+    assertAllNames(TypeId(name), nameWithBackticks, nameEncoded, nameWithBackticks, nameEncoded)
+    assertAllNames(ObjectId(name), nameWithBackticks, objectNameEncoded, nameWithBackticks, objectNameEncoded)
+  }
+
+  test("Java id in package") {
+    val parent = PackageId("parent")
   }
 
 }

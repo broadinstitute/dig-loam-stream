787bb191cc515bb95f30f2cefa69b90bb389c185
Oliver Ruebenacker <oliverr@broadinstitute.org>
2016-09-28 11:51:36 -0400

    Refactoring multi-file compiler test.

--- src/test/scala/loamstream/compiler/LoamCompilerMultiFileTest.scala
+++ src/test/scala/loamstream/compiler/LoamCompilerMultiFileTest.scala
@@ -8,44 +8,43 @@ import org.scalatest.FunSuite
   * Created by oliverr on 9/19/2016.
   */
 final class LoamCompilerMultiFileTest extends FunSuite {
+  def assertCompiledFine(results: LoamCompiler.Result, nStores: Int, nTools: Int): Unit = {
+    assert(results.isSuccess)
+    assert(results.isClean)
+    assert(results.contextOpt.nonEmpty)
+    val graph = results.contextOpt.get.graph
+    assert(graph.stores.size === nStores)
+    assert(graph.tools.size === nTools)
+  }
+
+  val scriptValues = LoamScript("values",
+    """
+      |val greeting = "Hello"
+      |val answer = 42
+    """.stripMargin)
+
   test("Compile project with two scripts, individual import") {
-    val codeLib =
+    val scriptIndividualImport = LoamScript("individualImport",
       """
-        |val answer = 42
-      """.stripMargin
-    val codeMain =
-      """
-        |import lib.answer
-        |cmd"echo The answer is $answer"
-      """.stripMargin
-    val scriptMain = LoamScript("main", codeMain)
-    val scriptLib = LoamScript("lib", codeLib)
-    val project = LoamProject(scriptMain, scriptLib)
+        |import values.{answer, greeting}
+        |cmd"echo $greeting the answer is $answer"
+      """.stripMargin)
+    val project = LoamProject(scriptValues, scriptIndividualImport)
     val compiler = new LoamCompiler
     val compileResults = compiler.compile(project)
-    assert(compileResults.isSuccess)
-    assert(compileResults.isClean)
-    assert(compileResults.contextOpt.get.graph.tools.size === 1)
+    assertCompiledFine(compileResults, 0, 1)
   }
 
   test("Compile project with two scripts, wild-card import") {
-    val codeLib =
+    val scriptWildcardImport = LoamScript("wildcardImport",
       """
-        |val answer = 42
-      """.stripMargin
-    val codeMain =
-      """
-        |import lib._
-        |cmd"echo The answer is $answer"
-      """.stripMargin
-    val scriptMain = LoamScript("main", codeMain)
-    val scriptLib = LoamScript("lib", codeLib)
-    val project = LoamProject(scriptMain) + scriptLib
+        |import values._
+        |cmd"echo $greeting the answer is $answer"
+      """.stripMargin)
+    val project = LoamProject(scriptValues, scriptWildcardImport)
     val compiler = new LoamCompiler
     val compileResults = compiler.compile(project)
-    assert(compileResults.isSuccess)
-    assert(compileResults.isClean)
-    assert(compileResults.contextOpt.get.graph.tools.size === 1)
+    assertCompiledFine(compileResults, 0, 1)
   }
 
 }

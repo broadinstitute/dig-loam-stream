ce4e05ce521662bf0d103ccfaf96dfec7877cc07
clint <cgilbert@broadinstitute.org>
2017-01-13 11:14:50 -0500

    Remove reference to src/main/loam/examples

--- src/main/scala/loamstream/compiler/messages/ClientMessageHandler.scala
+++ src/main/scala/loamstream/compiler/messages/ClientMessageHandler.scala
@@ -50,7 +50,9 @@ object ClientMessageHandler {
 final case class ClientMessageHandler(outMessageSink: OutMessageSink)(implicit executionContext: ExecutionContext) {
   
   val repo = {
-    val exampleDir = Paths.get("src/main/loam/examples")
+    //NB: The will never do the expected thing, since src/examples/loam (and src/main/loam/examples before it) won't exist
+    //where the LS jar/executable is deployed.
+    val exampleDir = Paths.get("src/examples/loam")
     
     LoamRepository.inMemory ++ LoamRepository.ofFolder(exampleDir)
   }

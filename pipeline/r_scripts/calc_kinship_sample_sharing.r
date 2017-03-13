cd521683d5bea013534f747a81e96cc41d9d3af3
ryank <ryank@gsa5.broadinstitute.org>
2017-03-13 13:38:54 -0400

    fixed if statement error in calc_sample_sharing r script

--- pipeline/r_scripts/calc_kinship_sample_sharing.r
+++ pipeline/r_scripts/calc_kinship_sample_sharing.r
@@ -1,9 +1,10 @@
 args<-commandArgs(trailingOnly=T)
-x<-try(read.table(args[1],header=T,as.is=T,stringsAsFactors=F))
-if(inherits(x, "try-error"))
+x<-try(read.table(args[1],header=T,as.is=T,stringsAsFactors=F), silent=TRUE)
+if(inherits(x, "try-error")) {
 	file.create(args[2])
-else
+} else {
 	ids<-c(x$ID1,x$ID2)
 	out<-as.data.frame(sort(table(ids),decreasing=T))
 	names(out)[1]<-"ibd_pairs"
 	write.table(out,args[2],row.names=T,col.names=F,quote=F,append=F,sep="\t")
+}

0ada75bcc274421396e10cb844c3044a04385035
Kaan Yuksel <kyuksel@broadinstitute.org>
2017-03-29 18:48:56 -0400

    TestJobs compiling

--- src/test/scala/loamstream/model/jobs/TestJobs.scala
+++ src/test/scala/loamstream/model/jobs/TestJobs.scala
@@ -5,31 +5,31 @@ package loamstream.model.jobs
  * date: Jun 2, 2016
  */
 trait TestJobs {
-  protected val two0Success: JobResult = JobResult.Succeeded
-  protected val two1Success: JobResult = JobResult.Succeeded
+  protected val two0Success: JobStatus = JobStatus.Succeeded
+  protected val two1Success: JobStatus = JobStatus.Succeeded
 
-  protected val twoPlusTwoSuccess: JobResult = JobResult.Succeeded
+  protected val twoPlusTwoSuccess: JobStatus = JobStatus.Succeeded
 
-  protected val plusOneSuccess: JobResult = JobResult.Succeeded
+  protected val plusOneSuccess: JobStatus = JobStatus.Succeeded
   
-  protected val two0Failure: JobResult = JobResult.Failed()
-  protected val two1Failure: JobResult = JobResult.Failed()
+  protected val two0Failure: JobStatus = JobStatus.Failed
+  protected val two1Failure: JobStatus = JobStatus.Failed
 
-  protected val twoPlusTwoFailure: JobResult = JobResult.Failed()
+  protected val twoPlusTwoFailure: JobStatus = JobStatus.Failed
 
-  protected val plusOneFailure: JobResult = JobResult.Failed()
+  protected val plusOneFailure: JobStatus = JobStatus.Failed
 
   protected val two0 = MockJob(two0Success)
   protected val two1 = MockJob(two1Success)
 
-  protected val twoPlusTwo = MockJob(twoPlusTwoSuccess, inputs = Set(two0, two1))
+  protected val twoPlusTwo = MockJob(twoPlusTwoSuccess, inputs = Set[LJob](two0, two1))
 
-  protected val plusOne = MockJob(plusOneSuccess, inputs = Set(twoPlusTwo))
+  protected val plusOne = MockJob(plusOneSuccess, inputs = Set[LJob](twoPlusTwo))
   
   protected val two0Failed = MockJob(two0Failure)
   protected val two1Failed = MockJob(two1Failure)
 
-  protected val twoPlusTwoFailed = MockJob(twoPlusTwoFailure, inputs = Set(two0Failed, two1Failed))
+  protected val twoPlusTwoFailed = MockJob(twoPlusTwoFailure, inputs = Set[LJob](two0Failed, two1Failed))
 
-  protected val plusOneFailed = MockJob(plusOneFailure, inputs = Set(twoPlusTwoFailed))
+  protected val plusOneFailed = MockJob(plusOneFailure, inputs = Set[LJob](twoPlusTwoFailed))
 }

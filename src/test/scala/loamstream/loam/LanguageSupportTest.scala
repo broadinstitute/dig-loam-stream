dbf97c3d7a5d99584963a5166cda35e9474a9665
Kaan Yuksel <kyuksel@broadinstitute.org>
2017-04-12 14:12:27 -0400

    Added stores to and factored out duplicate code in LanguageSupportTest. Removed unused imports.

--- src/test/scala/loamstream/loam/LanguageSupportTest.scala
+++ src/test/scala/loamstream/loam/LanguageSupportTest.scala
@@ -1,6 +1,9 @@
 package loamstream.loam
 
+import loamstream.compiler.LoamPredef.store
 import loamstream.TestHelpers
+import loamstream.loam.ops.StoreType
+import loamstream.loam.ops.StoreType.TXT
 import loamstream.util.Files
 import org.scalatest.FunSuite
 
@@ -11,51 +14,51 @@ import org.scalatest.FunSuite
 final class LanguageSupportTest extends FunSuite {
   import TestHelpers.config
 
+  private implicit val scriptContext = new LoamScriptContext(emptyProjectContext)
+
   private def emptyProjectContext = LoamProjectContext.empty(config)
 
-  test("embedding of Python snippets") {
-    import LanguageSupport.Python._
+  private def doTest(loamLine: LoamCmdTool,
+                     expectedBinary: String,
+                     expectedScriptContent: String): Unit = {
 
-    implicit val scriptContext = new LoamScriptContext(emptyProjectContext)
-
-    val someVal1 = 123
-    val someVal2 = "ABC"
-
-    val tool = python"$someVal1 foo bar $someVal2 baz"
-
-    val commandLine = LoamCmdTool.toString(scriptContext.projectContext.fileManager, tool.tokens)
+    val commandLine = LoamCmdTool.toString(scriptContext.projectContext.fileManager, loamLine.tokens)
     val pieces = commandLine.split(" ")
     val binary = pieces.head
     val file = pieces.last
     val scriptContent = Files.readFrom(file)
-
-    val expectedBinary = "/path/to/python/binary"
-    val expectedScriptContent = "123 foo bar ABC baz"
 
     assert(binary === expectedBinary)
     assert(scriptContent === expectedScriptContent)
   }
 
+  test("embedding of Python snippets") {
+    import LanguageSupport.Python._
+
+    val someTool = "someToolPath"
+    val someVal = 123
+    val someStore = store[TXT].at("/someStorePath")
+
+    val loamLine = python"$someTool --foo $someVal --bar $someStore baz"
+
+    val expectedBinary = "/path/to/python/binary"
+    val expectedScriptContent = "someToolPath --foo 123 --bar /someStorePath baz"
+
+    doTest(loamLine, expectedBinary, expectedScriptContent)
+  }
+
   test("embedding of R snippets") {
     import LanguageSupport.R._
 
-    implicit val scriptContext = new LoamScriptContext(emptyProjectContext)
+    val someTool = "someToooolPath"
+    val someVal = 456
+    val someStore = store[TXT].at("/someStooorePath")
 
-    val someVal1 = 456
-    val someVal2 = "DEF"
-
-    val tool = r"$someVal1 foo bar $someVal2 baz"
-
-    val commandLine = LoamCmdTool.toString(scriptContext.projectContext.fileManager, tool.tokens)
-    val pieces = commandLine.split(" ")
-    val binary = pieces.head
-    val file = pieces.last
-    val scriptContent = Files.readFrom(file)
+    val loamLine = r"$someTool --foo $someVal --bar $someStore baz"
 
     val expectedBinary = "/path/to/R/binary"
-    val expectedScriptContent = "456 foo bar DEF baz"
+    val expectedScriptContent = "someToooolPath --foo 456 --bar /someStooorePath baz"
 
-    assert(binary === expectedBinary)
-    assert(scriptContent === expectedScriptContent)
+    doTest(loamLine, expectedBinary, expectedScriptContent)
   }
 }

7b20615ea9ff7ec316aa955e702ef2472422e1d0
clint <cgilbert@broadinstitute.org>
2017-04-20 17:47:42 -0400

    Order Executions by start time (if any) when printing summary of executions in apps.Main

--- src/main/scala/loamstream/apps/Main.scala
+++ src/main/scala/loamstream/apps/Main.scala
@@ -77,8 +77,24 @@ object Main extends Loggable {
   }
 
   private def listResults(jobsToExecutions: Map[LJob, Execution]): Unit = {
+    //NB: Order (LJob, Execution) tuples based on the Executions' start times (if any).
+    //If no start time is present (for jobs where Resources couldn't be - or weren't - 
+    //determined, like Skipped jobs, those jobs/Executions come first. 
+    def ordering(a: (LJob, Execution), b: (LJob, Execution)): Boolean = {
+      val (_, executionA) = a
+      val (_, executionB) = b
+      
+      (executionA.resources, executionB.resources) match {
+        case (Some(resourcesA), Some(resourcesB)) => {
+          resourcesA.startTime.toEpochMilli < resourcesB.startTime.toEpochMilli
+        }
+        case (_, None) => false
+        case _ => true
+      }
+    }
+    
     for {
-      (job, execution) <- jobsToExecutions
+      (job, execution) <- jobsToExecutions.toSeq.sortWith(ordering)
     } {
       info(s"${execution.status}\t(${execution.result}):\tRan $job got $execution")
     }

31347c9fe59b44c57e20e2020620cce793e22b8e
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-05-17 16:04:09 -0400

    Cleanup: removing unused code.

--- src/main/scala/loamstream/loam/LoamToolBox.scala
+++ src/main/scala/loamstream/loam/LoamToolBox.scala
@@ -6,24 +6,23 @@ import java.nio.file.{Path, Paths}
 import loamstream.googlecloud.CloudStorageClient
 import loamstream.loam.ops.filters.LoamStoreFilterTool
 import loamstream.loam.ops.mappers.LoamStoreMapperTool
-import loamstream.model.Tool
+import loamstream.model.execute.{Executable, ExecutionEnvironment}
 import loamstream.model.jobs.commandline.CommandLineStringJob
 import loamstream.model.jobs.ops.{StoreFilterJob, StoreMapperJob}
-import loamstream.model.jobs.{LJob, LToolBox, NativeJob, Output}
+import loamstream.model.jobs.{LJob, NativeJob, Output}
+import loamstream.model.{AST, Tool}
 import loamstream.util.{Hit, Miss, Shot, Snag}
-import loamstream.model.execute.ExecutionEnvironment
-import loamstream.util.ValueBox
 
 /**
   * LoamStream
   * Created by oliverr on 6/21/2016.
   */
-final class LoamToolBox(context: LoamProjectContext, client: Option[CloudStorageClient] = None) extends LToolBox {
+final class LoamToolBox(context: LoamProjectContext, client: Option[CloudStorageClient] = None) {
 
   @volatile private[this] var loamJobs: Map[LoamTool, LJob] = Map.empty
-  
+
   private[this] val lock = new AnyRef
-  
+
   private[loam] def newLoamJob(tool: LoamTool): Shot[LJob] = {
     val graph = tool.graphBox.value
 
@@ -41,7 +40,7 @@ final class LoamToolBox(context: LoamProjectContext, client: Option[CloudStorage
     }
 
     val workDir: Path = graph.workDirOpt(tool).getOrElse(Paths.get("."))
-    
+
     val environment: ExecutionEnvironment = graph.executionEnvironmentOpt(tool).getOrElse(ExecutionEnvironment.Local)
 
     val shotsForPrecedingTools: Shot[Set[LJob]] = Shot.sequence(graph.toolsPreceding(tool).map(getLoamJob))
@@ -78,7 +77,28 @@ final class LoamToolBox(context: LoamProjectContext, client: Option[CloudStorage
     }
   }
 
-  override def toolToJobShot(tool: Tool): Shot[LJob] = tool match {
+  def createExecutable(ast: AST): Executable = {
+    val noJobs: Set[LJob] = Set.empty
+
+    val jobs: Set[LJob] = ast match {
+      case AST.ToolNode(_, tool, deps) =>
+        val jobsOption = for {
+        //TODO: Don't convert to option, pass misses through and fail loudly
+          job <- toolToJobShot(tool).asOpt
+          newInputs = deps.map(_.producer).flatMap(createExecutable(_).jobs)
+          newJob = if (newInputs == job.inputs) job else job.withInputs(newInputs)
+        } yield {
+          Set[LJob](newJob)
+        }
+
+        jobsOption.getOrElse(noJobs)
+      case _ => noJobs //TODO: other AST nodes
+    }
+
+    Executable(jobs)
+  }
+
+  def toolToJobShot(tool: Tool): Shot[LJob] = tool match {
     case loamTool: LoamTool => getLoamJob(loamTool)
     case _ => Miss(Snag(s"LoamToolBox only knows Loam tools; it doesn't know about $tool."))
   }

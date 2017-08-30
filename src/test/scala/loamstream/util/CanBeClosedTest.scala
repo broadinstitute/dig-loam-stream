e16a8599eadaed949c19a97ca0cde9a5370fc9fd
Kaan Yuksel <kyuksel@broadinstitute.org>
2017-08-30 17:21:13 -0400

    Test to make sure CanBeClosed.enclosed() works when exceptions are thrown

--- src/test/scala/loamstream/util/CanBeClosedTest.scala
+++ src/test/scala/loamstream/util/CanBeClosedTest.scala
@@ -7,17 +7,10 @@ import org.scalatest.FunSuite
  *         date: 8/30/17
  */
 final class CanBeClosedTest extends FunSuite {
+  import CanBeClosedTest._
+  import CanBeClosedTest.FoosCanBeClosed._
+
   test("enclosed() closes things properly") {
-    final class Foo {
-      var isClosed = false
-
-      def close(): Unit = isClosed = true
-    }
-
-    implicit object FoosCanBeClosed extends CanBeClosed[Foo] {
-      override def close(f: Foo): Unit = f.close()
-    }
-
     val foo = new Foo
 
     assert(foo.isClosed === false)
@@ -28,5 +21,31 @@ final class CanBeClosedTest extends FunSuite {
 
     assert(result === 42)
     assert(foo.isClosed === true)
+  }
+
+  test("enclosed() can handle exceptions") {
+    val foo = new Foo
+
+    assert(foo.isClosed === false)
+
+    intercept[Exception] {
+      CanBeClosed.enclosed(foo) { foo =>
+        throw new Exception("something went wrong")
+      }
+    }
+
+    assert(foo.isClosed === true)
+  }
+}
+
+object CanBeClosedTest {
+  final class Foo {
+    var isClosed = false
+
+    def close(): Unit = isClosed = true
+  }
+
+  implicit object FoosCanBeClosed extends CanBeClosed[Foo] {
+    override def close(f: Foo): Unit = f.close()
   }
 }

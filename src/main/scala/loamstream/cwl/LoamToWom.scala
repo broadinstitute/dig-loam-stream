17e8d9808d5bcb2bbd1428cd420644afd3c8b00a
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-09-14 17:55:39 -0400

    Loam-to-WOM converter stub, part 2.

--- src/main/scala/loamstream/cwl/LoamToWom.scala
+++ src/main/scala/loamstream/cwl/LoamToWom.scala
@@ -4,6 +4,8 @@ import lenthall.validation.ErrorOr.ErrorOr
 import loamstream.loam.LoamGraph
 import loamstream.model.Tool
 import wdl4s.wom.callable.{TaskDefinition, WorkflowDefinition}
+import wdl4s.wom.expression.WomExpression
+import wdl4s.wom.graph.{Graph, GraphNode, RequiredGraphInputNode, TaskCallNode}
 
 /**
   * LoamStream
@@ -15,8 +17,17 @@ object LoamToWom {
     ???
   }
 
-  def toWom(loam: LoamGraph): ErrorOr[WorkflowDefinition] = {
-    ???
+  def toWom(name: String, loam: LoamGraph): ErrorOr[WorkflowDefinition] = {
+    val inputNodes: Set[RequiredGraphInputNode] = ???
+    val toolNodes: Set[TaskCallNode] = ???
+    val nodes: Set[GraphNode] = inputNodes ++ toolNodes
+    val errorOrGraph = Graph.validateAndConstruct(nodes)
+    errorOrGraph.map { graph =>
+      val meta: Map[String, String] = ???
+      val parameterMeta: Map[String, String] = ???
+      val declarations: List[(String, WomExpression)] = ???
+      WorkflowDefinition(name, graph, meta, parameterMeta, declarations)
+    }
   }
 
 }

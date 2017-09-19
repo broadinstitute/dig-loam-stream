b714434e6cdeae7ea691d8f5fc11b8fa0f464c42
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-09-19 15:21:53 -0400

    Topological graph sort, completed.

--- src/test/scala/loamstream/loam/LoamGraphTest.scala
+++ src/test/scala/loamstream/loam/LoamGraphTest.scala
@@ -12,7 +12,6 @@ import loamstream.compiler.LoamPredef
 import loamstream.loam.ops.StoreType
 import java.nio.file.Path
 
-import loamstream.TestHelpers.config
 import loamstream.loam.files.LoamFileManager
 import loamstream.util.{Hit, Maps, Shot}
 
@@ -143,12 +142,14 @@ final class LoamGraphTest extends FunSuite {
     }
   }
 
-  private def assertDagSorted(graph: LoamGraph, expected: Shot[Seq[Int]]): Unit = {
-    assert(graph.dagSortedTools.map(_.map(_.size)) === expected)
+  private def assertDagSorted(graph: LoamGraph, expected: Option[Seq[Int]]): Unit = {
+    assert(graph.dagSortedTools.map(_.map(_.size)).asOpt === expected)
   }
 
   test("LoamGraph.dagSortedTools") {
-    assertDagSorted(makeTestComponents.graph, Hit(Seq(1, 1)))
+    assertDagSorted(makeTestComponents.graph, Some(Seq(1, 1)))
+    assertDagSorted(makeComplexGraph, Some(Seq(3, 3, 1, 1, 1, 1, 3, 2)))
+    assertDagSorted(makeCircularGraph, None)
   }
   
   test("without") {
@@ -234,9 +235,64 @@ object LoamGraphTest {
     phaseTool: LoamCmdTool,
     imputeTool: LoamCmdTool)
 
+  // scalastyle:off magic.number
   private def makeComplexGraph: LoamGraph = {
+    import TestHelpers.config
+
     implicit val scriptContext: LoamScriptContext = new LoamScriptContext(LoamProjectContext.empty(config))
-    ???
+
+    import LoamPredef._
+    import LoamCmdTool._
+    val nIns = 6
+    val ins = Seq.fill(nIns)(store[TXT].asInput)
+    val nOuts = 15
+    val outs = Seq.fill(nOuts)(store[TXT])
+    cmd"yo0".in(ins(0)).out(outs(0))
+    cmd"yo1".in(ins(1)).out(outs(1))
+    cmd"yo2".in(ins(2)).out(outs(2))
+    cmd"yo3".in(outs(0), ins(3)).out(outs(3))
+    cmd"yo4".in(outs(1), ins(4)).out(outs(4))
+    cmd"yo5".in(outs(2), ins(5)).out(outs(5))
+    cmd"yo6".in(outs(3)).out(outs(6))
+    cmd"yo7".in(outs(4), outs(6)).out(outs(7))
+    cmd"yo8".in(outs(5), outs(7)).out(outs(8))
+    cmd"yo9".in(outs(6), outs(7), outs(8)).out(outs(9))
+    cmd"yo10".in(outs(9)).out(outs(10))
+    cmd"yo11".in(outs(9)).out(outs(11))
+    cmd"yo12".in(outs(9)).out(outs(12))
+    cmd"yo13".in(outs(10), outs(11), outs(12)).out(outs(13))
+    cmd"yo14".in(outs(10), outs(11), outs(12)).out(outs(14))
     scriptContext.projectContext.graph
   }
+
+  private def makeCircularGraph: LoamGraph = {
+    import TestHelpers.config
+
+    implicit val scriptContext: LoamScriptContext = new LoamScriptContext(LoamProjectContext.empty(config))
+
+    import LoamPredef._
+    import LoamCmdTool._
+    val nIns = 6
+    val ins = Seq.fill(nIns)(store[TXT].asInput)
+    val nOuts = 15
+    val outs = Seq.fill(nOuts)(store[TXT])
+    cmd"yo0".in(ins(0)).out(outs(0))
+    cmd"yo1".in(ins(1)).out(outs(1))
+    cmd"yo2".in(ins(2)).out(outs(2))
+    cmd"yo3".in(outs(0), ins(3)).out(outs(3))
+    cmd"yo4".in(outs(1), ins(4)).out(outs(4))
+    cmd"yo5".in(outs(2), ins(5)).out(outs(5))
+    cmd"yo6".in(outs(3), outs(4), outs(5), outs(7), outs(8)).out(outs(6))
+    cmd"yo7".in(outs(3), outs(4), outs(5), outs(6), outs(8)).out(outs(7))
+    cmd"yo8".in(outs(3), outs(4), outs(5), outs(6), outs(7)).out(outs(8))
+    cmd"yo9".in(outs(6), outs(7), outs(8)).out(outs(9))
+    cmd"yo10".in(outs(9)).out(outs(10))
+    cmd"yo11".in(outs(9)).out(outs(11))
+    cmd"yo12".in(outs(9)).out(outs(12))
+    cmd"yo13".in(outs(10), outs(11), outs(12)).out(outs(13))
+    cmd"yo14".in(outs(10), outs(11), outs(12)).out(outs(14))
+    scriptContext.projectContext.graph
+  }
+
+  // scalastyle:off magic.number
 }

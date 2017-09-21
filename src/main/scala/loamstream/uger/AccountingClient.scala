bd4aa09380ac2d78d546276138014b6e9a12d0dd
clint <cgilbert@broadinstitute.org>
2017-09-21 19:06:45 -0400

    Move annoying, common qacct-invocation failure stack traces to trace level

--- src/main/scala/loamstream/uger/AccountingClient.scala
+++ src/main/scala/loamstream/uger/AccountingClient.scala
@@ -77,7 +77,8 @@ object AccountingClient extends Loggable {
         tokens.lineStream.toIndexedSeq
       } catch {
         case NonFatal(e) => {
-          warn(s"Error invoking '$binaryName'; execution node and queue won't be available.", e)
+          warn(s"Error invoking '$binaryName'; execution node and queue won't be available: $e")
+          trace(s"'$binaryName' invocation failure stack trace:", e) 
           
           Seq.empty
         }

96fc62d365179307ef4333bd474ef245297f01ff
clint <cgilbert@broadinstitute.org>
2017-10-05 13:34:24 -0400

    Make ConfigUtilsTest reliable by not setting any JVM system props

--- src/test/scala/loamstream/util/ConfigUtilsTest.scala
+++ src/test/scala/loamstream/util/ConfigUtilsTest.scala
@@ -2,6 +2,7 @@ package loamstream.util
 
 import java.nio.file.Paths
 import org.scalatest.FunSuite
+import com.typesafe.config.Config
 
 /**
  * @author clint
@@ -9,48 +10,25 @@ import org.scalatest.FunSuite
  */
 final class ConfigUtilsTest extends FunSuite {
   test("Loading a config file works") {
-    //make sure we can access JVM system props
-    val sysPropKey = "some.system.property"
-    val sysPropValue = "skaldjalskdjklasdjlas"
+    val config = ConfigUtils.configFromFile(Paths.get("src/test/resources/foo.config"))
     
-    withSystemProperty(sysPropKey, sysPropValue) {
-      val config = ConfigUtils.configFromFile(Paths.get("src/test/resources/foo.config"))
+    //Config file should have been loaded, merged with system props, BUT NOT merged with defaults
       
-      //Config file should have been loaded BUT NOT merged with defaults
-      
-      //default from reference.conf, shouldn't have been loaded
-      intercept[Exception] {
-        config.getString("loamstream.uger.logFile")
-      }
-      
-      //new key
-      assert(config.getInt("loamstream.uger.maxNumJobs") === 42)
-      
-      //new key
-      assert(config.getString("loamstream.uger.foo") === "bar")
-      
-      //new key
-      assert(config.getString("loamstream.nuh") === "zuh")
-      
-      //system prop    
-      assert(config.getString(sysPropKey) === sysPropValue)
+    //default from reference.conf, shouldn't have been loaded
+    intercept[Exception] {
+      config.getString("loamstream.uger.logFile")
     }
-  }
-  
-  private def withSystemProperty[A](sysPropKey: String, sysPropValue: String)(f: => A): A = {
-    def isUndefined(k: String): Boolean = Option(System.getProperty(k)).isEmpty
-    
-    assert(isUndefined(sysPropKey))
-    
-    System.setProperty(sysPropKey, sysPropValue)
-    
-    assert(System.getProperty(sysPropKey) === sysPropValue)
-    
-    try { f }
-    finally {
-      System.getProperties.remove(sysPropKey)
       
-      assert(isUndefined(sysPropKey))
-    }
+    //new key
+    assert(config.getInt("loamstream.uger.maxNumJobs") === 42)
+    
+    //new key
+    assert(config.getString("loamstream.uger.foo") === "bar")
+    
+    //new key
+    assert(config.getString("loamstream.nuh") === "zuh")
+    
+    //system prop    
+    assert(config.getString("user.dir") === System.getProperty("user.dir"))
   }
 }

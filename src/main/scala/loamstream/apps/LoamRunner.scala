1811977e258f79312d852b3abde1dfa98a524656
clint <cgilbert@broadinstitute.org>
2017-10-26 18:23:06 -0400

    Make sure exceptions thrown by evaluating graph thunks in LoamRunner are at least logged; added tests for the current behavior when exceptions are thrown by code in andThen blocks; added getLines and getNonEmptyLines to loamstream.util.Files; made Files.readFrom as .readFromAsUtf8 work the same - they previously both used the UTF-8 encoding, but had different behavior regarding line endings.

--- src/main/scala/loamstream/apps/LoamRunner.scala
+++ src/main/scala/loamstream/apps/LoamRunner.scala
@@ -1,13 +1,17 @@
 package loamstream.apps
 
+import scala.util.control.NonFatal
+
+import loamstream.compiler.GraphSource
+import loamstream.compiler.GraphThunk
 import loamstream.compiler.LoamCompiler
 import loamstream.compiler.LoamEngine
 import loamstream.compiler.LoamProject
+import loamstream.loam.LoamGraph
+import loamstream.model.Tool
 import loamstream.model.jobs.Execution
 import loamstream.model.jobs.LJob
 import loamstream.util.Loggable
-import loamstream.compiler.GraphSource
-import loamstream.model.Tool
 
 /**
  * @author clint
@@ -53,13 +57,21 @@ object LoamRunner {
       //Keep track of job-execution results and the tools we've run "so far"
       val z: (Map[LJob, Execution], Set[Tool]) = (emptyJobResults, Set.empty)
       
+      debug("Processing graph chunks")
+      
       //Fold over the "stream" of graph chunks, producing job-execution results
       val (jobResults, _) = graphSource.iterator.foldLeft(z) { (state, chunk) =>
         val (jobResultsSoFar, toolsRunSoFar) = state
         
+        val rawChunkGraph = getGraphFrom(chunk)
+        
+        debug(s"Made raw chunk graph with ${rawChunkGraph.tools.size} tools and ${rawChunkGraph.stores.size} stores.")
+        
         //Filter out tools from previous chunks, so we don't run jobs more than necessary, saving
         //the expense of calculating if a job can be skipped.
-        val chunkGraph = chunk().without(toolsRunSoFar)
+        val chunkGraph = rawChunkGraph.without(toolsRunSoFar)
+
+        debug(s"Made filtered chunk graph with ${chunkGraph.tools.size} tools and ${chunkGraph.stores.size} stores.")
         
         //Skip running if there are no new tools
         val jobResults = {
@@ -71,6 +83,17 @@ object LoamRunner {
       }
       
       jobResults
+    }
+    
+    private def getGraphFrom(thunk: GraphThunk): LoamGraph = {
+      try { thunk() }
+      catch {
+        case NonFatal(e) => {
+          error(s"Evaluating loam code chunk threw exception: $e", e)
+          
+          throw e
+        }
+      }
     }
   }
 }

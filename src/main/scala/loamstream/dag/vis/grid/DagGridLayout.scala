0c375da1823d1468865de6ce3c359744b6aaccc5
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-10-30 11:26:53 -0400

    Minor.

--- src/main/scala/loamstream/dag/vis/grid/DagGridLayout.scala
+++ src/main/scala/loamstream/dag/vis/grid/DagGridLayout.scala
@@ -15,7 +15,7 @@ case class DagGridLayout(dag: Dag, rows: Seq[NodeRow]) extends DagLayout {
 object DagGridLayout {
 
   case class NodeRow(nodes: Seq[Option[Dag.NodeBase]]) {
-    def add(node: Dag.NodeBase, iCol: Int): NodeRow = NodeRow(nodes.)
+    def add(node: Dag.NodeBase, iCol: Int): NodeRow = NodeRow(nodes.updated(iCol, Option(node)))
   }
 
   def empty(nCols: Int): NodeRow = NodeRow(Seq.fill(nCols)(None))

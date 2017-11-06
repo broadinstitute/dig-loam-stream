54da09ee78c5d9a2305c4cf028593c0ff12c41c8
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-11-06 18:08:07 -0500

    More on node placement on a grid.

--- src/main/scala/loamstream/dag/vis/grid/DagGridLayoutBuilder.scala
+++ src/main/scala/loamstream/dag/vis/grid/DagGridLayoutBuilder.scala
@@ -9,9 +9,38 @@ import loamstream.dag.vis.grid.DagGridLayout.NodeRow
   * Created by oliverr on 10/17/2017.
   */
 case class DagGridLayoutBuilder(nCols: Int) extends DagLayout.Builder[DagGridLayout] {
-  override def build(dag: Dag) = {
-    val nodeRows : Seq[NodeRow] = Seq.empty
 
+  override def build(dag: Dag) = {
+    val nodeRows: Seq[NodeRow] = Seq.empty
+    for (levelNodes <- dag.levelsFromBottom.reverse) {
+
+    }
     DagGridLayout(dag, nodeRows)
   }
+}
+
+object DagGridLayoutBuilder {
+
+  case class NodePlacement[D <: Dag](dag: D, nodeRows: Seq[NodeRow], unplacedLevelNodes: Seq[Set[D#Node]]) {
+    def currentRow: NodeRow = nodeRows.last
+
+    def previousRowOpt: Option[NodeRow] = {
+      val size = nodeRows.size
+      if(size < 2) None else Option(nodeRows(size - 2))
+    }
+
+    def evaluatePlacement(node: D#Node, iCol: Int): Double = {
+      ???
+    }
+  }
+
+  object NodePlacement {
+    def apply[D <: Dag, N <: D#Node](dag:D, nCols: Int): NodePlacement[D] =
+      NodePlacement(
+        dag = dag,
+        nodeRows = Seq(NodeRow.empty(nCols)),
+        unplacedLevelNodes = dag.levelsFromBottom.map(_.map(_.asInstanceOf[D#Node])).reverse
+      )
+  }
+
 }

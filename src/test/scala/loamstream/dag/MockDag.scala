36ed8339fd1e3667d043193d74a5a829142c4c6c
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-11-07 18:12:59 -0500

    Implementing missing MockGraph methods.

--- src/test/scala/loamstream/dag/MockDag.scala
+++ src/test/scala/loamstream/dag/MockDag.scala
@@ -1,22 +1,25 @@
 package loamstream.dag
 
+import loamstream.dag.MockDag.Node
+
 /**
   * LoamStream
   * Created by oliverr on 11/7/2017.
   */
-case class MockDag(n: Int) extends Dag {
+case class MockDag(idToNode: Map[String, Node], nodeToNextUp: Map[Node, Set[Node]],
+                   nodeToNextDown: Map[Node, Set[Node]]) extends Dag {
   override type Node = MockDag.Node
 
-  override def nodes: Set[Node] = ???
+  override def nodes: Set[Node] = idToNode.values.toSet
 
-  override def nextUp(node: Node): Set[Node] = ???
+  override def nextUp(node: Node): Set[Node] = nodeToNextUp.getOrElse(node, Set.empty)
 
-  override def nextDown(node: Node): Set[Node] = ???
+  override def nextDown(node: Node): Set[Node] = nodeToNextDown.getOrElse(node, Set.empty)
 }
 
 object MockDag {
 
-  def empty: MockDag = MockDag(0)
+  def empty: MockDag = MockDag(Map.empty, Map.empty, Map.empty)
 
   case class Node(id: String) extends Dag.NodeBase {
     override def label: String = id

038a278dee1dd1e463b9b9833a178d152bee3a4c
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-11-08 16:11:05 -0500

    Next node placement stubs.

--- src/main/scala/loamstream/dag/vis/grid/DagGridLayoutBuilder.scala
+++ src/main/scala/loamstream/dag/vis/grid/DagGridLayoutBuilder.scala
@@ -3,12 +3,18 @@ package loamstream.dag.vis.grid
 import loamstream.dag.Dag
 import loamstream.dag.vis.DagLayout
 import loamstream.dag.vis.grid.DagGridLayout.NodeRow
+import loamstream.dag.vis.grid.DagGridLayoutBuilder.NodesPlacement
 
 /**
   * LoamStream
   * Created by oliverr on 10/17/2017.
   */
 case class DagGridLayoutBuilder[D <: Dag](nCols: Int) extends DagLayout.Builder[D, DagGridLayout] {
+
+  private def evaluatePlacement(placement: NodesPlacement[D])(node: placement.dag.Node, iCol: Int): Double = {
+    ???
+  }
+
 
   override def build(dag: D): DagGridLayout[D] = {
     val nodeRows: Seq[NodeRow[D]] = Seq.empty
@@ -21,7 +27,7 @@ case class DagGridLayoutBuilder[D <: Dag](nCols: Int) extends DagLayout.Builder[
 
 object DagGridLayoutBuilder {
 
-  case class NodePlacement[D <: Dag](dag: D, nodeRows: Seq[NodeRow[D]], unplacedLevelNodes0: Seq[Set[D#Node]]) {
+  case class NodesPlacement[D <: Dag](dag: D, nodeRows: Seq[NodeRow[D]], unplacedLevelNodes0: Seq[Set[D#Node]]) {
 
     def unplacedLevelNodes: Seq[Set[dag.Node]] = unplacedLevelNodes0.map(_.map(_.asInstanceOf[dag.Node]))
 
@@ -32,14 +38,17 @@ object DagGridLayoutBuilder {
       if (size < 2) None else Option(nodeRows(size - 2))
     }
 
-    def evaluatePlacement(node: dag.Node, iCol: Int): Double = {
-      ???
-    }
+    case class NextNodePlacement(node: dag.Node, iCol: Int)
+
+    def findAllNextNodePlacements: Set[NextNodePlacement] = ???
+
+    def +(nextNodePlacement: NextNodePlacement): NodesPlacement[D] = ???
+
   }
 
-  object NodePlacement {
-    def apply[D <: Dag, N <: D#Node](dag: D, nCols: Int): NodePlacement[D] =
-      NodePlacement(
+  object NodesPlacement {
+    def apply[D <: Dag, N <: D#Node](dag: D, nCols: Int): NodesPlacement[D] =
+      NodesPlacement(
         dag = dag,
         nodeRows = Seq(NodeRow.empty[D](dag, nCols)),
         unplacedLevelNodes0 = dag.levelsFromBottom.map(_.map(_.asInstanceOf[D#Node])).reverse

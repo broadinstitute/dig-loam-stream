55a4e473ccfdda895edc8ee0567d2b19085253f4
ryank <ryank@dig-ae-dev-01.broadinstitute.org>
2017-12-04 23:36:44 -0500

    added per command uger resources; added number of cpu option to king.sh and pcair.r

--- pipeline/scripts/king.sh
+++ pipeline/scripts/king.sh
@@ -6,8 +6,9 @@ prefix=$3
 log=$4
 kin0=$5
 kin0Related=$6
+cpus=$7
 
-$king -b $bed --kinship --prefix $prefix > $log
+$king -b $bed --kinship --prefix $prefix --cpus $cpus > $log
 if [ -f $kin0 ]; then
 	(head -1 $kin0; sed '1d' $kin0 | awk '{if($8 >= 0.0884) print $0}' | sort -rn -k8,8) > $kin0Related
 else

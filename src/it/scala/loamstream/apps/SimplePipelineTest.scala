17cf17f63f2324687554e94a5e72f49a783bab3c
clint <cgilbert@broadinstitute.org>
2018-01-30 18:25:57 -0500

    SimpleLocalPipelineTest => SimplePipelineTest; added Uger cases

---
+++ src/it/scala/loamstream/apps/SimplePipelineTest.scala
@@ -0,0 +1,90 @@
+package loamstream.apps
+
+import org.scalatest.FunSuite
+import loamstream.util.Files
+import java.nio.file.Files.exists
+import loamstream.IntegrationTestHelpers
+import loamstream.cli.Intent
+import loamstream.util.PathEnrichments
+import loamstream.model.execute.HashingStrategy
+import java.nio.file.Path
+import loamstream.db.slick.DbDescriptor
+import loamstream.db.LoamDao
+
+/**
+ * @author clint
+ * Jan 29, 2018
+ */
+final class SimplePipelineTest extends FunSuite with IntegrationTestHelpers {
+    
+  test(s"A simple linear pipeline running locally should work") {
+    doTest("local", shouldRunEverything = true, hashingStrategy = HashingStrategy.HashOutputs)
+    doTest("local", shouldRunEverything = false, hashingStrategy = HashingStrategy.HashOutputs)
+    doTest("local", shouldRunEverything = true, hashingStrategy = HashingStrategy.DontHashOutputs)
+    doTest("local", shouldRunEverything = false, hashingStrategy = HashingStrategy.DontHashOutputs)
+  }
+   
+  test(s"A simple linear pipeline running on Uger should work") {
+    doTest("uger", shouldRunEverything = true, hashingStrategy = HashingStrategy.HashOutputs)
+    doTest("uger", shouldRunEverything = false, hashingStrategy = HashingStrategy.HashOutputs)
+    doTest("uger", shouldRunEverything = true, hashingStrategy = HashingStrategy.DontHashOutputs)
+    doTest("uger", shouldRunEverything = false, hashingStrategy = HashingStrategy.DontHashOutputs)
+  }
+  
+  private val dbDescriptor: DbDescriptor = IntegrationTestHelpers.inMemoryH2(this.getClass.getSimpleName)
+      
+  private val dao: LoamDao = AppWiring.makeDaoFrom(dbDescriptor)
+  
+  private def doTest(environment: String, shouldRunEverything: Boolean, hashingStrategy: HashingStrategy): Unit = {
+    val tempDir = getWorkDir
+    
+    import PathEnrichments._
+    
+    val pathA = tempDir / "a.txt"
+    val pathB = tempDir / "b.txt"
+    val pathC = tempDir / "c.txt"
+    
+    Files.writeTo(pathA)("AAA")
+    
+    assert(Files.readFrom(pathA) === "AAA")
+    
+    assert(exists(pathA))
+    assert(exists(pathB) === false)
+    assert(exists(pathC) === false)
+    
+    val loamScriptContents = {
+      s"""|
+          |val a = store.at("${pathA}").asInput
+          |val b = store.at("${pathB}")
+          |val c = store.at("${pathC}")
+          |
+          |$environment { //should be 'local' or 'uger'
+          |  cmd"cp $$b $$c".in(b).out(c) //NB: declare commands "out of order"
+          |
+          |  cmd"cp $$a $$b".in(a).out(b)
+          |}
+          |""".stripMargin.trim
+    }
+    
+    val loamScriptPath = tempDir / s"copy-a-b-c-$environment-$shouldRunEverything-$hashingStrategy.loam"
+    
+    Files.writeTo(loamScriptPath)(loamScriptContents)
+    
+    assert(Files.readFrom(loamScriptPath) === loamScriptContents)
+
+    val intent = Intent.RealRun(
+      confFile = None,
+      hashingStrategy = hashingStrategy,
+      shouldRunEverything = shouldRunEverything,
+      loams = Seq(loamScriptPath))
+    
+    Main.doRealRun(intent, dao)
+    
+    assert(exists(pathA))
+    assert(exists(pathB))
+    assert(exists(pathC))
+    
+    assert(Files.readFrom(pathA) === Files.readFrom(pathB))
+    assert(Files.readFrom(pathB) === Files.readFrom(pathC))
+  }
+}

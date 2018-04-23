42b5c248826d420770303de569be6aa79f2e4d18
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-04-23 17:58:23 -0400

    More stubbing for Loam to WDL.

--- src/main/scala/loamstream/wdl/LoamToWdl.scala
+++ src/main/scala/loamstream/wdl/LoamToWdl.scala
@@ -1,10 +1,19 @@
 package loamstream.wdl
 
 import loamstream.model.execute.Executable
-import wdl.model.draft3.elements.WorkflowDefinitionElement
+import wdl.model.draft3.elements.{InputsSectionElement, MetaSectionElement, OutputsSectionElement}
+import wdl.model.draft3.elements.{ParameterMetaSectionElement, WorkflowDefinitionElement, WorkflowGraphElement}
 
 object LoamToWdl {
 
-  def loamToWdl(executable: Executable): WorkflowDefinitionElement = ???
+  def loamToWdl(executable: Executable): WorkflowDefinitionElement = {
+    val name: String = ???
+    val inputsSection: Option[InputsSectionElement] = ???
+    val graphElements: Set[WorkflowGraphElement] = ???
+    val outputsSection: Option[OutputsSectionElement] = ???
+    val metaSection: Option[MetaSectionElement] = ???
+    val parameterMetaSection: Option[ParameterMetaSectionElement] = ???
+    WorkflowDefinitionElement(name, inputsSection, graphElements, outputsSection, metaSection, parameterMetaSection)
+  }
 
 }

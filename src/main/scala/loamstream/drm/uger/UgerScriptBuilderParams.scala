756b832c253a9f178e453606f5fc78c0beaa3c0c
Jeffrey Massung <massung@gmail.com>
2018-10-22 12:43:29 -0500

    Fixing tests on Windows.

--- src/main/scala/loamstream/drm/uger/UgerScriptBuilderParams.scala
+++ src/main/scala/loamstream/drm/uger/UgerScriptBuilderParams.scala
@@ -10,14 +10,15 @@ import loamstream.conf.UgerConfig
  * May 11, 2018
  */
 final case class UgerScriptBuilderParams(extraPathDir: Path, condaEnvName: String) extends ScriptBuilderParams {
-  
+  import loamstream.util.BashScript.Implicits._
+
   /*
    * We need to 'use' Java-1.8 to make some steps of the QC pipeline work.
-   * 
-   * Set SINGULARITY_CACHEDIR to something other than the default, under 
-   * ~/.singularity, which will cause quota problems.  Follow recommendations 
-   * from BITS at 
-   * https://broad.service-now.com/nav_to.do?uri=%2Fkb_view.do%3Fsysparm_article%3DKB0010821 
+   *
+   * Set SINGULARITY_CACHEDIR to something other than the default, under
+   * ~/.singularity, which will cause quota problems.  Follow recommendations
+   * from BITS at
+   * https://broad.service-now.com/nav_to.do?uri=%2Fkb_view.do%3Fsysparm_article%3DKB0010821
    */
   private def ugerPreamble = s"""|#$$ -cwd
                                  |
@@ -25,14 +26,14 @@ final case class UgerScriptBuilderParams(extraPathDir: Path, condaEnvName: Strin
                                  |reuse -q UGER
                                  |reuse -q Java-1.8
                                  |
-                                 |export PATH=${extraPathDir}:$$PATH
+                                 |export PATH=${extraPathDir.render}:$$PATH
                                  |source activate ${condaEnvName}
                                  |
                                  |mkdir -p /broad/hptmp/$${USER}
                                  |export SINGULARITY_CACHEDIR=/broad/hptmp/$${USER}""".stripMargin
-  
-  override val preamble: Option[String] = Option(ugerPreamble) 
-  override val indexEnvVarName: String = "SGE_TASK_ID" 
+
+  override val preamble: Option[String] = Option(ugerPreamble)
+  override val indexEnvVarName: String = "SGE_TASK_ID"
   override val jobIdEnvVarName: String = "JOB_ID"
   override val drmIndexVarExpr: String = JobTemplate.PARAMETRIC_INDEX
 }
--- src/test/scala/loamstream/drm/uger/UgerScriptBuilderParamsTest.scala
+++ src/test/scala/loamstream/drm/uger/UgerScriptBuilderParamsTest.scala
@@ -3,51 +3,51 @@ package loamstream.drm.uger
 import org.scalatest.FunSuite
 import loamstream.TestHelpers
 
-
 /**
  * @author clint
  * May 11, 2018
  */
 final class UgerScriptBuilderParamsTest extends FunSuite {
   import TestHelpers.path
-  
+  import loamstream.util.BashScript.Implicits._
+
   private val someDir = path("/some/dir")
-  
+
   private val someEnv = "someEnv"
-  
+
   test("drmIndexVarExpr") {
     val params = new UgerScriptBuilderParams(someDir, someEnv)
-    
+
     assert(params.drmIndexVarExpr === "$drmaa_incr_ph$")
   }
-  
+
   test("preamble") {
-    val expected = """|#$ -cwd
-                      |
-                      |source /broad/software/scripts/useuse
-                      |reuse -q UGER
-                      |reuse -q Java-1.8
-                      |
-                      |export PATH=/some/dir:$PATH
-                      |source activate someEnv
-                      |
-                      |mkdir -p /broad/hptmp/${USER}
-                      |export SINGULARITY_CACHEDIR=/broad/hptmp/${USER}""".stripMargin
-    
+    val expected = s"""|#$$ -cwd
+                       |
+                       |source /broad/software/scripts/useuse
+                       |reuse -q UGER
+                       |reuse -q Java-1.8
+                       |
+                       |export PATH=${someDir.render}:$$PATH
+                       |source activate someEnv
+                       |
+                       |mkdir -p /broad/hptmp/$${USER}
+                       |export SINGULARITY_CACHEDIR=/broad/hptmp/$${USER}""".stripMargin
+
     val params = new UgerScriptBuilderParams(someDir, someEnv)
-                      
+
     assert(params.preamble === Some(expected))
   }
-  
+
   test("indexEnvVarName") {
     val params = new UgerScriptBuilderParams(someDir, someEnv)
-    
+
     assert(params.indexEnvVarName === "SGE_TASK_ID")
   }
-  
+
   test("jobIdEnvVarName") {
     val params = new UgerScriptBuilderParams(someDir, someEnv)
-    
+
     assert(params.jobIdEnvVarName === "JOB_ID")
   }
 }

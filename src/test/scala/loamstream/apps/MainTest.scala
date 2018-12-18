ceba931d711c7e21529ecfe3e0d014926839be0d
clint <cgilbert@broadinstitute.org>
2018-12-18 18:13:37 -0500

    Added test for Main.Run.doClean()

--- src/test/scala/loamstream/apps/MainTest.scala
+++ src/test/scala/loamstream/apps/MainTest.scala
@@ -7,6 +7,10 @@ import loamstream.model.execute.Executer
 import loamstream.googlecloud.CloudStorageClient
 import loamstream.model.execute.JobFilter
 import loamstream.model.execute.ExecutionRecorder
+import loamstream.cli.Intent
+import loamstream.db.slick.DbDescriptor
+import loamstream.conf.Locations
+import java.nio.file.Path
 
 /**
  * @author clint
@@ -31,6 +35,47 @@ final class MainTest extends FunSuite {
     run.shutdown(mockWiring)
     
     assert(mockWiring.shutdownInvocations === 1)
+  }
+  
+  test("doClean - clean db") {
+    import java.nio.file.Files.exists
+    
+    def makeIfNecessary(p: Path): Unit = {
+      p.toFile.mkdirs()
+      
+      assert(exists(p))
+    }
+    
+    def doTest(intent: Intent.Clean): Unit = {
+      val run = new Main.Run
+      
+      val dbDirShouldExistAfterClean = !intent.db
+      val logDirsShouldExistAfterClean = !intent.logs
+      val scriptDirsShouldExistAfterClean = !intent.scripts
+      
+      makeIfNecessary(DbDescriptor.defaultDbDir)
+      
+      makeIfNecessary(Locations.logDir)
+      makeIfNecessary(Locations.jobOutputDir)
+      
+      makeIfNecessary(Locations.ugerDir)
+      makeIfNecessary(Locations.lsfDir)
+      
+      run.doClean(intent)
+      
+      assert(exists(DbDescriptor.defaultDbDir) === dbDirShouldExistAfterClean)
+      
+      assert(exists(Locations.logDir) === logDirsShouldExistAfterClean)
+      assert(exists(Locations.jobOutputDir) === logDirsShouldExistAfterClean)
+      
+      assert(exists(Locations.ugerDir) === scriptDirsShouldExistAfterClean)
+      assert(exists(Locations.lsfDir) === scriptDirsShouldExistAfterClean)
+    }
+    
+    doTest(Intent.Clean(None, db = true, logs = false, scripts = false))
+    doTest(Intent.Clean(None, db = false, logs = true, scripts = false))
+    doTest(Intent.Clean(None, db = false, logs = false, scripts = true))
+    doTest(Intent.Clean(None, db = true, logs = true, scripts = true))
   }
 }
 

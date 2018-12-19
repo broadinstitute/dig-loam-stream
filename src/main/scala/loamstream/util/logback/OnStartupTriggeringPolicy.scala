fbd9a11ffa00cb2d6989527a6c73e5ecca7ab3ed
clint <cgilbert@broadinstitute.org>
2018-12-19 18:46:08 -0500

    Move OnStartupTriggeringPolicy to src/main/scala, and make sure its start() method is called

--- src/main/scala/loamstream/util/logback/OnStartupTriggeringPolicy.scala
+++ src/main/scala/loamstream/util/logback/OnStartupTriggeringPolicy.scala
@@ -4,9 +4,16 @@ import ch.qos.logback.core.rolling.TriggeringPolicyBase
 import ch.qos.logback.core.joran.spi.NoAutoStart
 import java.io.File
 
+/**
+ * A class to make Slf4j/Logback create a new log file on app startup, while keeping
+ * some number of previous logs.  See src/main/resources/logback.xml
+ */
 @NoAutoStart
 final class OnStartupTriggeringPolicy[A] extends TriggeringPolicyBase[A] {
   @volatile private[this] var triggerRollover: Boolean = true
+  
+  //This call must happen or else no logging will occur via appenders that use this policy.
+  start()
   
   override def isTriggeringEvent(activeFile: File, event: A): Boolean = {
     if (!triggerRollover) { false }

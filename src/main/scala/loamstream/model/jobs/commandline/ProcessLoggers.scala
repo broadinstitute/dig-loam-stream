98ded3e289c1a310464cec8fce7c3900427e2a84
clint <cgilbert@broadinstitute.org>
2018-12-19 19:22:11 -0500

    organized imports in ProcessLoggers

--- src/main/scala/loamstream/model/jobs/commandline/ProcessLoggers.scala
+++ src/main/scala/loamstream/model/jobs/commandline/ProcessLoggers.scala
@@ -1,21 +1,8 @@
 package loamstream.model.jobs.commandline
 
-import scala.sys.process.ProcessLogger
-import java.nio.file.Path
-import java.io.FileWriter
-import java.io.BufferedWriter
-import java.io.Writer
-import loamstream.model.jobs.LJob
-import java.nio.file.Paths
-import loamstream.util.Loggable
-import loamstream.util.Throwables
-import loamstream.model.jobs.LogFileNames
-import loamstream.util.Files
-import loamstream.util.Functions
 import loamstream.conf.ExecutionConfig
-import java.util.concurrent.atomic.AtomicBoolean
-import loamstream.model.jobs.OutputStreams
-import loamstream.conf.Locations
+import loamstream.model.jobs.LJob
+import loamstream.model.jobs.LogFileNames
 
 /**
  * @author clint

5f207aafdd1dbbf0b25fbfb720e9f811d59585c4
clint <cgilbert@broadinstitute.org>
2019-05-23 17:16:39 -0400

    Added test for Terminable.StopsComponents

--- src/test/scala/loamstream/util/TerminableTest.scala
+++ src/test/scala/loamstream/util/TerminableTest.scala
@@ -22,4 +22,29 @@ final class TerminableTest extends FunSuite {
     
     assert(flag === 2)
   }
+  
+  test("StopsComponents") {
+    var flag0 = 0
+    var flag1 = 0
+    
+    val t0 = Terminable { flag0 += 1 }
+    val t1 = Terminable { flag1 += 1 }
+    
+    val sc: Terminable = new Terminable.StopsComponents with Loggable {
+      override val terminableComponents = Seq(t0, t1)
+    }
+    
+    assert(flag0 === 0)
+    assert(flag1 === 0)
+    
+    sc.stop()
+    
+    assert(flag0 === 1)
+    assert(flag1 === 1)
+    
+    sc.stop()
+    
+    assert(flag0 === 2)
+    assert(flag1 === 2)
+  }
 }

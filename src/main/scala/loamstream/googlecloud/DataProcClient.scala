8e040bb7a258ebfceb11da68f6a062eaa74a2c17
clint <cgilbert@broadinstitute.org>
2019-07-22 18:23:59 -0400

    Removed unused method from DataProcClient

--- src/main/scala/loamstream/googlecloud/DataProcClient.scala
+++ src/main/scala/loamstream/googlecloud/DataProcClient.scala
@@ -20,20 +20,4 @@ object DataProcClient {
   }
   
   type CanStopAndTellIfRunning = CanStop with CanTellIfRunning
-  
-  private final class FromComponents(
-      canStart: CanStart, 
-      canStop: CanStop, 
-      canTellIfRunning: CanTellIfRunning) extends DataProcClient {
-    
-    override def startCluster(): Unit = canStart.startCluster() 
-  
-    override def deleteCluster(): Unit = canStop.deleteCluster()
-  
-    override def isClusterRunning: Boolean = canTellIfRunning.isClusterRunning
-  }
-  
-  def from(canStart: CanStart, canStop: CanStop, canTellIfRunning: CanTellIfRunning): DataProcClient = {
-    new FromComponents(canStart, canStop, canTellIfRunning)
-  }
 }

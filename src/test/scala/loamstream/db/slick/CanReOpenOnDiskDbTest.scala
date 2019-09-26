0da8aac39f7f23cc5442b14a6ee77a767e1d9e35
clint <cgilbert@broadinstitute.org>
2019-09-26 15:43:54 -0400

    Re-organized CanReOpenOnDiskDbTest

--- src/test/scala/loamstream/db/slick/CanReOpenOnDiskDbTest.scala
+++ src/test/scala/loamstream/db/slick/CanReOpenOnDiskDbTest.scala
@@ -1,43 +1,47 @@
 package loamstream.db.slick
 
-import org.scalatest.FunSuite
 import java.nio.file.Path
+
+import org.scalatest.FunSuite
+
 import loamstream.TestHelpers
 
+/**
+ * @author clint
+ * Sep 26, 2019
+ */
 final class CanReOpenOnDiskDbTest extends FunSuite {
+  private val testName = getClass.getSimpleName
+  
   test("Can re-open the same on-disk DB") {
-    final class HasDao(dbDir: Path, dbName: String) extends ProvidesSlickLoamDao {
-      override protected val descriptor: DbDescriptor = DbDescriptor.onDiskHsqldbAt(dbDir, dbName)
-      
-      def startUp(): Unit = dao.createTables()
-      
-      def shutDown(): Unit = dao.shutdown()
-    }
-    
-    val testName = getClass.getSimpleName
-    
     TestHelpers.withWorkDir(testName) { workDir1 =>
-      val hasDao0 = new HasDao(workDir1, testName)
       
-      hasDao0.startUp()
+      def startAndStopIn(workDir: Path): Unit = {
+        val hasDao0 = new CanReOpenOnDiskDbTest.HasDao(workDir, testName)
       
-      hasDao0.shutDown()
+        hasDao0.startUp()
       
-      val hasDao1 = new HasDao(workDir1, testName)
+        hasDao0.shutDown()
+      }
       
-      hasDao1.startUp()
-      
-      hasDao1.shutDown()
+      startAndStopIn(workDir1)
+      startAndStopIn(workDir1)
       
       TestHelpers.withWorkDir(testName) { workDir2 =>
         assert(workDir1 !== workDir2) 
-        
-        val hasDao0 = new HasDao(workDir2, testName)
-        
-        hasDao0.startUp()
-        
-        hasDao0.shutDown()
+
+        startAndStopIn(workDir2)
       }
     }
+  }
+}
+
+object CanReOpenOnDiskDbTest {
+  final class HasDao(dbDir: Path, dbName: String) extends ProvidesSlickLoamDao {
+    override protected val descriptor: DbDescriptor = DbDescriptor.onDiskHsqldbAt(dbDir, dbName)
+    
+    def startUp(): Unit = dao.createTables()
+    
+    def shutDown(): Unit = dao.shutdown()
   }
 }

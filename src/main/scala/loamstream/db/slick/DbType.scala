2b076668622a2232aa6570dbae98bce45f69c460
clint <cgilbert@broadinstitute.org>
2019-09-26 13:40:55 -0400

    Removed DbType.H2

--- src/main/scala/loamstream/db/slick/DbType.scala
+++ src/main/scala/loamstream/db/slick/DbType.scala
@@ -6,19 +6,13 @@ import slick.jdbc.JdbcProfile
  * @author clint
  * date: Aug 18, 2016
  */
-trait DbType {
+sealed trait DbType {
   def driver: JdbcProfile
   
   def jdbcDriverClass: String
 }
 
 object DbType {
-  object H2 extends DbType {
-    override val driver: JdbcProfile = slick.jdbc.H2Profile
-  
-    override val jdbcDriverClass: String = "org.h2.Driver"
-  }
-  
   object Hsqldb extends DbType {
     override val driver: JdbcProfile = slick.jdbc.HsqldbProfile
   

039d43ddbd284e9534eb23201365e7d3e6f88d50
clint <cgilbert@broadinstitute.org>
2019-10-28 19:34:00 -0400

    Instead of using an AWS parameterized on strict/lazy evaluation, use an IO-oriented AWS, and call unsafeRunSync() in AwsApi.Default

--- src/main/scala/loamstream/loam/aws/AwsApi.scala
+++ src/main/scala/loamstream/loam/aws/AwsApi.scala
@@ -2,11 +2,11 @@ package loamstream.loam.aws
 
 import java.net.URI
 import java.nio.file.Path
+
 import org.broadinstitute.dig.aws.AWS
-import cats.Id
-import rx.lang.scala.Observable
 import org.broadinstitute.dig.aws.JobStep
 import org.broadinstitute.dig.aws.emr.Cluster
+
 import loamstream.aws.AwsJobDesc
 
 /**
@@ -28,24 +28,24 @@ trait AwsApi {
 }
 
 object AwsApi {
-  def apply(aws: AWS[Id]): AwsApi = Default(aws)
+  def apply(aws: AWS): AwsApi = Default(aws)
   
-  final case class Default(aws: AWS[Id]) extends AwsApi {
+  final case class Default(aws: AWS) extends AwsApi {
     override def copy(src: Path, dest: URI): Unit = {
-      require(aws.bucketOf(dest) == aws.bucket)
+      require(AWS.bucketOf(dest) == aws.bucket)
 
       println(s"%%%%%%%%% copying '$src' ==> '$dest'")
       
-      aws.put(aws.keyOf(dest), src)
+      aws.put(AWS.keyOf(dest), src).unsafeRunSync()
     }
   
     override def copy(src: URI, dest: Path): Unit = {
       println(s"%%%%%%%%% copying '$src' ==> '$dest'")
       
-      aws.download(aws.keyOf(src), dest)
+      aws.download(AWS.keyOf(src), dest).unsafeRunSync()
     }
     
-    override def exists(uri: URI): Boolean = aws.exists(aws.keyOf(uri))
+    override def exists(uri: URI): Boolean = aws.exists(AWS.keyOf(uri)).unsafeRunSync()
     
     override def runPySparkJob(cluster: Cluster, pySparkScriptUri: URI, scriptArgs: Seq[String]): Unit = {
       runSingleStep(cluster, JobStep.PySpark(pySparkScriptUri, scriptArgs: _*))
@@ -56,15 +56,20 @@ object AwsApi {
     }
     
     override def runOnClusterPool(cluster: Cluster, maxClusters: Int, awsJobs: Seq[AwsJobDesc]): Unit = {
-      val jobs = aws.clusterJobs(cluster, awsJobs, maxClusters)
+      val jobIOs = aws.clusterJobs(cluster, awsJobs, maxClusters).toList
       
-      aws.waitForJobs(jobs)
+      val io = aws.waitForJobs(jobIOs)
+      
+      io.unsafeRunSync()
     }
     
     private def runSingleStep(cluster: Cluster, step: JobStep): Unit = {
-      val job = aws.runJob(cluster, step) 
+      val io = for {
+        job <- aws.runJob(cluster, step)
+        _ <- aws.waitForJob(job)
+      } yield ()
         
-      aws.waitForJob(job)
+      io.unsafeRunSync()
     }
   }
 }

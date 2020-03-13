efb05b8b05837097fbdefa6a46353ee14bd15c46
clint <cgilbert@broadinstitute.org>
2020-03-13 12:23:48 -0400

    Add hopefully-clarifying comment to DrmTaskId

--- src/main/scala/loamstream/drm/DrmTaskId.scala
+++ src/main/scala/loamstream/drm/DrmTaskId.scala
@@ -3,6 +3,10 @@ package loamstream.drm
 /**
  * @author clint
  * Jan 29, 2020
+ * 
+ * An identifier for a task within a DRM task array.  Contains:
+ * jobId: The id of the job representing the whole task array this task is a part of.
+ * taskIndex: The index of this task within the task array.
  */
 final case class DrmTaskId(jobId: String, taskIndex: Int)
 

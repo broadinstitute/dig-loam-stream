459a2eeaa68e182e021bdc1c4cc3f22535a41579
clint <cgilbert@broadinstitute.org>
2020-03-27 21:06:52 -0400

    Added Iterators.sample

--- src/main/scala/loamstream/util/Iterators.scala
+++ src/main/scala/loamstream/util/Iterators.scala
@@ -8,7 +8,27 @@ object Iterators {
   object Implicits {
     implicit final class IteratorOps[A](val itr: Iterator[A]) extends AnyVal {
       //NB: This will consume the iterator, if it's not empty
-      def nextOption: Option[A] = if(itr.hasNext) Some(itr.next()) else None
+      def nextOption(): Option[A] = if(itr.hasNext) Some(itr.next()) else None
+    }
+  }
+  
+  private[util] def deltasBetween(is: Seq[Int]): Iterator[Int] = {
+    is. 
+      //step through the list pairwise
+      sliding(2, 1).
+      //compute the difference between the numbers in each pair  
+      map { case Seq(start, end) => end - start }
+  }
+  
+  def sample[A](as: Iterator[A], indices: Seq[Int]): Iterator[A] = {
+    def deltas = deltasBetween(indices.sorted)
+    
+    import Implicits._
+    
+    deltas.flatMap { d => 
+      as.drop(d)
+      
+      as.nextOption()
     }
   }
 }
--- src/test/scala/loamstream/util/IteratorsTest.scala
+++ src/test/scala/loamstream/util/IteratorsTest.scala
@@ -24,4 +24,23 @@ final class IteratorsTest extends FunSuite {
     assert(initiallyNonEmpty.nextOption === Some(100))
     assert(initiallyNonEmpty.nextOption === None)
   }
+  
+  test("deltasBetween") {
+    import Iterators.deltasBetween
+    
+    assert(deltasBetween(Nil).toIndexedSeq === Nil)
+    
+    assert(deltasBetween(1 to 5).toIndexedSeq === Seq(1,1,1,1))
+    
+    assert(deltasBetween(Seq(1, 2, 4, 7, 11)).toIndexedSeq === Seq(1,2,3,4))
+    assert(deltasBetween(Seq(11, 7, 4, 2, 1)).toIndexedSeq === Seq(-4, -3, -2, -1))
+  }
+  
+  test("sample") {
+    import Iterators.sample
+    
+    assert(sample(Iterator.empty, Seq(1,2,3,4)).toIndexedSeq === Iterator.empty.toIndexedSeq)
+    
+    assert(sample(Iterator('a','b','c','d','e','f','g','h'), Seq(1, 2, 4, 6)).toIndexedSeq === Seq('b','c','e','g'))
+  }
 }

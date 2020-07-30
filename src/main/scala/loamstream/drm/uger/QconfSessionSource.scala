5be634d95deac0f61e5dc9108025dd8542ab8ef5
clint <cgilbert@broadinstitute.org>
2020-07-30 16:48:53 -0400

    Don't kill the main thread when creating or deleting Uger sessions fails

--- src/main/scala/loamstream/drm/uger/QconfSessionSource.scala
+++ src/main/scala/loamstream/drm/uger/QconfSessionSource.scala
@@ -9,6 +9,11 @@ import scala.concurrent.duration.Duration
 import loamstream.conf.UgerConfig
 import rx.lang.scala.schedulers.IOScheduler
 import rx.lang.scala.Scheduler
+import loamstream.util.ValueBox
+import loamstream.util.OneTimeLatch
+import scala.util.control.NonFatal
+import loamstream.util.Loggable
+import scala.util.Try
 
 /**
  * @author clint
@@ -16,22 +21,51 @@ import rx.lang.scala.Scheduler
  */
 final class QconfSessionSource(
     createInvoker: CommandInvoker[Unit],
-    deleteInvoker: CommandInvoker[String])(implicit ec: ExecutionContext) extends SessionSource {
+    deleteInvoker: CommandInvoker[String])(implicit ec: ExecutionContext) extends SessionSource with Loggable {
   
-  override lazy val getSession: String = {
+  private[this] val sessionBox: ValueBox[Option[String]] = ValueBox(None)
+  
+  private def isInitialized: Boolean = sessionBox.value.isDefined
+  
+  private[this] lazy val init: Unit = {
     val resultsFuture = createInvoker.apply(())
     
     val sessionIdFuture = resultsFuture.map(_.stdout).flatMap(lines => Future.fromTry(Qconf.parseOutput(lines)))
     
     //TODO
-    Await.result(sessionIdFuture, Duration.Inf)
+    try { 
+      val sessionId = Await.result(sessionIdFuture, Duration.Inf)
+      
+      sessionBox := Some(sessionId)
+    } catch {
+      case NonFatal(e) => error(s"Couldn't get Uger session: ", e)
+    }
   }
   
-  override def stop(): Unit = {
-    val resultsFuture = deleteInvoker.apply(getSession)
+  private def doInit(): Unit = init
+  
+  override lazy val getSession: String = {
+    doInit()
     
-    //TODO
-    Await.result(resultsFuture, Duration.Inf)
+    sessionBox.value.getOrElse {
+      sys.error(s"Uger session initialization failed previously; see earlier log messages")
+    }
+  }
+  
+  override def stop(): Unit = sessionBox.foreach { sessionIdOpt =>
+    if(sessionIdOpt.isDefined) {
+      val resultsFuture = deleteInvoker.apply(getSession)
+    
+      try {
+        //TODO
+        
+        Await.result(resultsFuture, Duration.Inf)
+      } catch {
+        case NonFatal(e) => error(s"Couldn't delete Uger session") 
+      }
+    } else {
+      debug("No Uger session initialized, not attempting to delete it")
+    }
   }
 }
 

2f5b2b1e632cdb5d39832c12d5574132d9709a13
clint <cgilbert@broadinstitute.org>
2020-09-23 16:55:55 -0400

    Factor out id-based equality in some tools

--- src/main/scala/loamstream/loam/InvokesLsTool.scala
+++ src/main/scala/loamstream/loam/InvokesLsTool.scala
@@ -7,21 +7,19 @@ import loamstream.model.LId
 /**
  * @author clint
  * Aug 11, 2020
+ * 
+ * A tool representing the invocation of LS as a job, managed by another LS instance.
+ * This class exists to provide a graph node for this case, that tracks any setup commands needed by the child job,
+ * (the preambles), such as `using` commands in the Uger context.
  */
 final case class InvokesLsTool private (
-    preambles: Seq[String] = Nil)(implicit override val scriptContext: LoamScriptContext) extends Tool {
+    preambles: Seq[String] = Nil)
+   (implicit override val scriptContext: LoamScriptContext) extends Tool with LId.IdBasedEquality {
   
   /** Input and output stores before any are specified using in or out */
   override def defaultStores: DefaultStores = DefaultStores.empty
     
   override val id: LId = LId.newAnonId
-  
-  override def equals(other: Any): Boolean = other match {
-    case that: InvokesLsTool => this.id == that.id
-    case _ => false
-  }
-  
-  override def hashCode: Int = id.hashCode
   
   def addPreamble(preamble: String): InvokesLsTool = copy(preambles = preambles :+ preamble)
 }

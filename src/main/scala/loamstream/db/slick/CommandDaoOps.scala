89f170783bd85d5da439b7fd186d87ae816564eb
clint <cgilbert@broadinstitute.org>
2020-11-02 22:25:00 -0500

    Remove verbose logging

--- src/main/scala/loamstream/db/slick/CommandDaoOps.scala
+++ src/main/scala/loamstream/db/slick/CommandDaoOps.scala
@@ -1,7 +1,5 @@
 package loamstream.db.slick
 
-import loamstream.util.Loggable
-
 
 /**
  * @author clint
@@ -10,16 +8,11 @@ import loamstream.util.Loggable
 trait CommandDaoOps { self: CommonDaoOps with OutputDaoOps with ExecutionDaoOps =>
   import driver.api._
   
-  private object CommandDaoOpsLogger extends Loggable
-  
   override def findCommand(loc: String): Option[String] = {
     for {
       outputRow <- findOutputRow(loc)
-      _ = CommandDaoOpsLogger.trace(s"Looking up '$loc', found output row $outputRow")
       executionId <- outputRow.executionId
-      _ = CommandDaoOpsLogger.trace(s"Looking up '$loc', found execution id $executionId")
       executionRow <- findExecutionRow(executionId)
-      _ = CommandDaoOpsLogger.trace(s"Looking up '$loc', found execution row $executionRow")
       commandLine <- executionRow.cmd
     } yield commandLine
   }

206f59df66e7e651aecc02441b93a0045ef820b5
clint <cgilbert@broadinstitute.org>
2020-11-09 14:54:34 -0500

    More cases in RowFiltersTest

--- src/test/scala/loamstream/loam/intake/RowFiltersTest.scala
+++ src/test/scala/loamstream/loam/intake/RowFiltersTest.scala
@@ -25,134 +25,227 @@ final class RowFiltersTest extends FunSuite {
   import RowFiltersTest.LogFileOps
   
   test("noDsNorIs") {
-    TestHelpers.withWorkDir(getClass.getSimpleName) { workDir =>
-      TestHelpers.withScriptContext { implicit context =>
-        import LoamSyntax._
-        import Filters.CsvRowFilters
-        
-        val REF = ColumnName("REF")
-        val ALT = ColumnName("ALT")
-        
-        val rows = Helpers.csvRows(
-            Seq(REF.name, ALT.name, "FOO"),
-            Seq("D", "T", "42"),
-            Seq("G", "C", "42"),
-            Seq("D", "I", "42"),
-            Seq("A", "I", "42"),
-            Seq("A", "T", "42"))
-        
-        val logStore = logStoreIn(workDir, "blah.log")
-            
-        val predicate = CsvRowFilters.noDsNorIs(refColumn = REF, altColumn = ALT, logStore = logStore, append = true)
-        
-        val filtered = rows.filter(predicate)
-        
-        val actual = filtered.map(_.values.toIndexedSeq)
-        
-        val expected = Seq(
-            Seq("G", "C", "42"),
-            Seq("A", "T", "42"))
-       
-        assert(actual === expected)
-        
-        predicate.close()
-        
-        val logLines = linesFrom(logStore.path)
-        
-        assert(logLines.size === 3)
-        assert(logLines.containsOnce("D,T,42"))
-        assert(logLines.containsOnce("D,I,42"))
-        assert(logLines.containsOnce("A,I,42"))
-      }
+    withLogStore { logStore =>
+      import Filters.CsvRowFilters
+      
+      val REF = ColumnName("REF")
+      val ALT = ColumnName("ALT")
+      
+      val rows = Helpers.csvRows(
+          Seq(REF.name, ALT.name, "FOO"),
+          Seq("D", "T", "42"),
+          Seq("G", "C", "42"),
+          Seq("D", "I", "42"),
+          Seq("A", "I", "42"),
+          Seq("A", "T", "42"))
+          
+      val predicate = CsvRowFilters.noDsNorIs(refColumn = REF, altColumn = ALT, logStore = logStore, append = true)
+      
+      val filtered = rows.filter(predicate)
+      
+      val actual = filtered.map(_.values.toIndexedSeq)
+      
+      val expected = Seq(
+          Seq("G", "C", "42"),
+          Seq("A", "T", "42"))
+     
+      assert(actual === expected)
+      
+      predicate.close()
+      
+      val logLines = linesFrom(logStore.path)
+      
+      assert(logLines.size === 3)
+      assert(logLines.containsOnce("D,T,42"))
+      assert(logLines.containsOnce("D,I,42"))
+      assert(logLines.containsOnce("A,I,42"))
     }
   }
   
   test("filterRefAndAlt") {
-    TestHelpers.withWorkDir(getClass.getSimpleName) { workDir =>
-      TestHelpers.withScriptContext { implicit context =>
-        import LoamSyntax._
-        
-        val REF = ColumnName("X")
-        val ALT = ColumnName("Y")
-        
-        val rows = Helpers.csvRows(
-            Seq(REF.name, ALT.name, "FOO"),
-            Seq("U", "T", "42"),
-            Seq("G", "C", "42"),
-            Seq("U", "V", "42"),
-            Seq("A", "V", "42"),
-            Seq("A", "T", "42"))
-        
-        val logStore = logStoreIn(workDir)
-            
-        val predicate = CsvRowFilters.filterRefAndAlt(
-                                    refColumn = REF, 
-                                    altColumn = ALT,
-                                    disallowed = Set("U", "V"),
-                                    logStore = logStore, 
-                                    append = true)
-        
-        val filtered = rows.filter(predicate)
-        
-        val actual = filtered.map(_.values.toIndexedSeq)
-        
-        val expected = Seq(
-            Seq("G", "C", "42"),
-            Seq("A", "T", "42"))
-       
-        assert(actual === expected)
-        
-        predicate.close()
-        
-        val logLines = linesFrom(logStore.path)
-        
-        assert(logLines.size === 3)
-        assert(logLines.containsOnce("U,T,42"))
-        assert(logLines.containsOnce("U,V,42"))
-        assert(logLines.containsOnce("A,V,42"))
-      }
+    withLogStore { logStore =>
+      val REF = ColumnName("X")
+      val ALT = ColumnName("Y")
+      
+      val rows = Helpers.csvRows(
+          Seq(REF.name, ALT.name, "FOO"),
+          Seq("U", "T", "42"),
+          Seq("G", "C", "42"),
+          Seq("U", "V", "42"),
+          Seq("A", "V", "42"),
+          Seq("A", "T", "42"))
+          
+      val predicate = CsvRowFilters.filterRefAndAlt(
+                                  refColumn = REF, 
+                                  altColumn = ALT,
+                                  disallowed = Set("U", "V"),
+                                  logStore = logStore, 
+                                  append = true)
+      
+      val filtered = rows.filter(predicate)
+      
+      val actual = filtered.map(_.values.toIndexedSeq)
+      
+      val expected = Seq(
+          Seq("G", "C", "42"),
+          Seq("A", "T", "42"))
+     
+      assert(actual === expected)
+      
+      predicate.close()
+      
+      val logLines = linesFrom(logStore.path)
+      
+      assert(logLines.size === 3)
+      assert(logLines.containsOnce("U,T,42"))
+      assert(logLines.containsOnce("U,V,42"))
+      assert(logLines.containsOnce("A,V,42"))
     }
   }
   
   test("logToFile") {
+    withLogStore { logStore =>
+      val FOO = ColumnName("FOO")
+      
+      val fooIsEven = FOO.asInt.map(_ % 2 == 0)
+      
+      val predicate = CsvRowFilters.logToFile(logStore, append = true)(fooIsEven)
+      
+      val rows = Helpers.csvRows(
+          Seq(FOO.name, "BAR"),
+          Seq("1", "42"),
+          Seq("2", "42"),
+          Seq("3", "42"),
+          Seq("6", "42"),
+          Seq("7", "42"))
+          
+      val filtered = rows.filter(predicate)
+      
+      val actual = filtered.map(_.values.mkString(" ")).toIndexedSeq
+      
+      val expected = Seq(
+          "2 42",
+          "6 42")
+          
+      assert(actual === expected)
+      
+      predicate.close()
+      
+      val logLines = linesFrom(logStore.path)
+      
+      assert(logLines.size === 3)
+      assert(logLines.containsOnce("1,42"))
+      assert(logLines.containsOnce("3,42"))
+      assert(logLines.containsOnce("7,42"))
+    }
+  }
+  
+  test("validEaf") {
+    withLogStore { logStore => 
+      val predicate = DataRowFilters.validEaf(logStore, append = true)
+      
+      val rows = Seq(
+          DataRow(marker = Variant("1_12345_T_C"), pvalue = 0.5, eaf = Some(1.0)),
+          DataRow(marker = Variant("2_12345_T_C"), pvalue = 0.5, eaf = Some(0.99)),
+          DataRow(marker = Variant("3_12345_T_C"), pvalue = 0.5, eaf = Some(0.0)),
+          DataRow(marker = Variant("4_12345_T_C"), pvalue = 0.5, eaf = Some(-1.0)),
+          DataRow(marker = Variant("2_12345_T_C"), pvalue = 0.5, eaf = Some(0.25)),
+          DataRow(marker = Variant("5_12345_T_C"), pvalue = 0.5, eaf = Some(100.0)),
+          DataRow(marker = Variant("6_12345_T_C"), pvalue = 0.5, eaf = Some(-10.0)))
+          
+      val filtered = rows.filter(predicate)
+      
+      val actual = filtered.flatMap(_.eaf)
+      
+      val expected = Seq(0.99, 0.25)
+          
+      assert(actual === expected)
+      
+      predicate.close()
+      
+      val logLines = linesFrom(logStore.path)
+      
+      assert(logLines.size === 5)
+      assert(logLines.containsOnce("(1.0)"))
+      assert(logLines.containsOnce("(0.0)"))
+      assert(logLines.containsOnce("(-1.0)"))
+      assert(logLines.containsOnce("(100.0)"))
+      assert(logLines.containsOnce("(-10.0)"))
+    }
+  }
+  
+  test("validMaf") {
+    withLogStore { logStore => 
+      val predicate = DataRowFilters.validMaf(logStore, append = true)
+      
+      val rows = Seq(
+          DataRow(marker = Variant("1_12345_T_C"), pvalue = 0.5, maf = Some(0.51)),
+          DataRow(marker = Variant("2_12345_T_C"), pvalue = 0.5, maf = Some(0.5)),
+          DataRow(marker = Variant("3_12345_T_C"), pvalue = 0.5, maf = Some(0.0)),
+          DataRow(marker = Variant("4_12345_T_C"), pvalue = 0.5, maf = Some(-1.0)),
+          DataRow(marker = Variant("2_12345_T_C"), pvalue = 0.5, maf = Some(0.25)),
+          DataRow(marker = Variant("5_12345_T_C"), pvalue = 0.5, maf = Some(100.0)),
+          DataRow(marker = Variant("6_12345_T_C"), pvalue = 0.5, maf = Some(-10.0)))
+          
+      val filtered = rows.filter(predicate)
+      
+      val actual = filtered.flatMap(_.maf)
+      
+      val expected = Seq(0.5, 0.25)
+          
+      assert(actual === expected)
+      
+      predicate.close()
+      
+      val logLines = linesFrom(logStore.path)
+      
+      assert(logLines.size === 5)
+      assert(logLines.containsOnce("(0.51)"))
+      assert(logLines.containsOnce("(0.0)"))
+      assert(logLines.containsOnce("(-1.0)"))
+      assert(logLines.containsOnce("(100.0)"))
+      assert(logLines.containsOnce("(-10.0)"))
+    }
+  }
+  
+  test("validPvalue") {
+    withLogStore { logStore => 
+      val predicate = DataRowFilters.validPValue(logStore, append = true)
+      
+      val rows = Seq(
+          DataRow(marker = Variant("1_12345_T_C"), pvalue = 1.0),
+          DataRow(marker = Variant("2_12345_T_C"), pvalue = 0.99),
+          DataRow(marker = Variant("3_12345_T_C"), pvalue = 0.0),
+          DataRow(marker = Variant("4_12345_T_C"), pvalue = -1.0),
+          DataRow(marker = Variant("2_12345_T_C"), pvalue = 0.25),
+          DataRow(marker = Variant("5_12345_T_C"), pvalue = 100.0),
+          DataRow(marker = Variant("6_12345_T_C"), pvalue = -10.0))
+          
+      val filtered = rows.filter(predicate)
+      
+      val actual = filtered.map(_.pvalue)
+      
+      val expected = Seq(0.99, 0.25)
+          
+      assert(actual === expected)
+      
+      predicate.close()
+      
+      val logLines = linesFrom(logStore.path)
+      
+      assert(logLines.size === 5)
+      assert(logLines.containsOnce("(1.0)"))
+      assert(logLines.containsOnce("(0.0)"))
+      assert(logLines.containsOnce("(-1.0)"))
+      assert(logLines.containsOnce("(100.0)"))
+      assert(logLines.containsOnce("(-10.0)"))
+    }
+  }
+  
+  private def withLogStore[A](f: Store => A): A = {
     TestHelpers.withWorkDir(getClass.getSimpleName) { workDir =>
       TestHelpers.withScriptContext { implicit context =>
-        import LoamSyntax._
-        
-        val logStore = logStoreIn(workDir)
-        
-        val FOO = ColumnName("FOO")
-        
-        val fooIsEven = FOO.asInt.map(_ % 2 == 0)
-        
-        val predicate = CsvRowFilters.logToFile(logStore, append = true)(fooIsEven)
-        
-        val rows = Helpers.csvRows(
-            Seq(FOO.name, "BAR"),
-            Seq("1", "42"),
-            Seq("2", "42"),
-            Seq("3", "42"),
-            Seq("6", "42"),
-            Seq("7", "42"))
-            
-        val filtered = rows.filter(predicate)
-        
-        val actual = filtered.map(_.values.mkString(" ")).toIndexedSeq
-        
-        val expected = Seq(
-            "2 42",
-            "6 42")
-            
-        assert(actual === expected)
-        
-        predicate.close()
-        
-        val logLines = linesFrom(logStore.path)
-        
-        assert(logLines.size === 3)
-        assert(logLines.containsOnce("1,42"))
-        assert(logLines.containsOnce("3,42"))
-        assert(logLines.containsOnce("7,42"))
+        f(logStoreIn(workDir))
       }
     }
   }

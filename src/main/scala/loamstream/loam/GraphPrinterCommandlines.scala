e7407d360da801af096d57b67f6dcca23c201800
clint <cgilbert@broadinstitute.org>
2020-11-10 14:10:10 -0500

    Add case for NativeTool to GraphPrinterCommandLines

--- src/main/scala/loamstream/loam/GraphPrinterCommandlines.scala
+++ src/main/scala/loamstream/loam/GraphPrinterCommandlines.scala
@@ -8,12 +8,12 @@ import loamstream.loam.LoamToken.MultiToken
 final case class GraphPrinterCommandlines(lineLength: Int) extends GraphPrinter {
 
   /** Prints a store */
-  def print(store: Store): String = store.pathOpt.map(_.toString).getOrElse("[file]")
+  private def print(store: Store): String = store.pathOpt.map(_.toString).getOrElse("[file]")
 
-  def print(stores: Iterable[Store]): String = stores.map(print).mkString(" ")
+  private def print(stores: Iterable[Store]): String = stores.map(print).mkString(" ")
 
   /** Prints a token */
-  def print(token: LoamToken, graph: LoamGraph): String = token match {
+  private def print(token: LoamToken, graph: LoamGraph): String = token match {
     case StringToken(string) => string
     case StoreToken(store) => print(store)
     case MultiStoreToken(stores) => print(stores)
@@ -21,12 +21,15 @@ final case class GraphPrinterCommandlines(lineLength: Int) extends GraphPrinter 
   }
 
   /** Prints a tool */
-  def print(tool: Tool): String = tool match {
-    case cmdTool: LoamCmdTool => print(cmdTool)
+  private def print(tool: Tool): String = tool match {
+    case cmdTool: LoamCmdTool => printCmd(cmdTool)
+    case nativeTool: NativeTool => printNative(nativeTool)
   }
 
   /** Prints a cmd tool */
-  def print(tool: LoamCmdTool): String = tool.tokens.map(print(_, tool.graph)).mkString
+  private def printCmd(tool: LoamCmdTool): String = tool.tokens.map(print(_, tool.graph)).mkString
+  
+  private def printNative(tool: NativeTool): String = s"${classOf[NativeTool].getSimpleName}(id=${tool.id})"
 
   /** Prints file names and command lines in LoamGraph */
   override def print(graph: LoamGraph): String = {

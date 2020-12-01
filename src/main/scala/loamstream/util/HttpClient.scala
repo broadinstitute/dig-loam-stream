34432788023f0372411adb1b8e150c027653af96
clint <cgilbert@broadinstitute.org>
2020-12-01 15:07:16 -0500

    Add post method to HttpClient

--- src/main/scala/loamstream/util/HttpClient.scala
+++ src/main/scala/loamstream/util/HttpClient.scala
@@ -8,4 +8,6 @@ trait HttpClient {
   def get(url: String): Either[String, String]
   
   def contentLength(url: String): Either[String, Long]
+  
+  def post(url: String, body: Option[String] = None, headers: Map[String, String] = Map.empty): Either[String, String]
 }
--- src/main/scala/loamstream/util/SttpHttpClient.scala
+++ src/main/scala/loamstream/util/SttpHttpClient.scala
@@ -2,6 +2,7 @@ package loamstream.util
 
 import sttp.client.HttpURLConnectionBackend
 import sttp.client.basicRequest
+import sttp.client.asStream
 import sttp.client.UriContext
 
 /**
@@ -25,5 +26,19 @@ final class SttpHttpClient extends HttpClient with Loggable {
     val request = basicRequest.head(uri"${url}")
     
     backend.send(request).contentLength.toRight(s"No content-length returned when invoking HEAD ${url}")
+  }
+  
+  override def post(
+      url: String, 
+      body: Option[String] = None, 
+      headers: Map[String, String] = Map.empty): Either[String, String] = {
+    
+    trace(s"Invoking POST ${url} ; headers = ${headers} ; body size: ${body.map(_.size)}")
+    
+    val request = basicRequest.post(uri"${url}").body(body.getOrElse("")).headers(headers)
+    
+    val response = backend.send(request)
+    
+    response.body
   }
 }

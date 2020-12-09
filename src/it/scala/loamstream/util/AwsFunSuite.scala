f53fa6e6c67ee64fcb261f5d15a469a1cda35d04
clint <cgilbert@broadinstitute.org>
2020-12-08 19:50:37 -0500

    Add integration tests for AwsClient.Default and DGA tissue uploading

---
+++ src/it/scala/loamstream/util/AwsFunSuite.scala
@@ -0,0 +1,64 @@
+package loamstream.util
+
+import org.broadinstitute.dig.aws.AWS
+import org.broadinstitute.dig.aws.config.AWSConfig
+import org.scalatest.FunSuite
+import org.broadinstitute.dig.aws.config.S3Config
+import org.broadinstitute.dig.aws.config.emr.EmrConfig
+import org.broadinstitute.dig.aws.config.emr.SubnetId
+
+/**
+ * @author clint
+ * Jul 27, 2018
+ */
+trait AwsFunSuite extends FunSuite with Loggable {
+  protected val bucketName: String = "dig-integration-tests"
+
+  protected val awsConfig: AWSConfig = {
+    //dummy values, except fot the bucket name
+    AWSConfig(
+        S3Config(bucketName), 
+        EmrConfig("some-ssh-key-name", SubnetId("subnet-foo"))) 
+  }
+  
+  protected val aws: AWS = new AWS(awsConfig)
+
+  def testWithPseudoDir(name: String)(body: String => Any): Unit = {
+    test(name) {
+      val mungedName = name.filter(_ != '/')
+
+      val pseudoDirKey = s"integrationTests/${mungedName}"
+
+      def nukeTestDir(): Unit = aws.rmdir(s"${pseudoDirKey}/").unsafeRunSync()
+
+      nukeTestDir()
+
+      body(pseudoDirKey)
+
+      //Test dir will be deleted after successful runs, but will live until the next run
+      //if there's a failure.
+      nukeTestDir()
+    }
+  }
+
+  /*private def uploadResource(resource: String, dirKey: String = "resources"): URI = {
+    val key = s"""${dirKey}/${resource.stripPrefix("/")}"""
+
+    //Produce the contents of the classpath resource at `resource` as a string,
+    //and will close the InputStream backed by the resource when reading the resource's data is
+    //done, either successfully or due to an error.
+    val contents: String = {
+      using(getClass.getClassLoader.getResourceAsStream(resource)) { stream =>
+        using(Source.fromInputStream(stream)) {
+          _.mkString.replace("\r\n", "\n")
+        }
+      }
+    }
+
+    logger.debug(s"Uploading $resource to S3...")
+
+    aws.put(key, contents).unsafeRunSync()
+
+    aws.uriOf(key)
+  }*/
+}
---
+++ src/it/scala/loamstream/util/DgaTissueUploadTest.scala
@@ -0,0 +1,56 @@
+package loamstream.util
+
+import loamstream.loam.intake.IntakeSyntax
+import loamstream.loam.intake.dga.DgaSyntax
+import loamstream.loam.intake.AwsRowSink
+import loamstream.loam.intake.dga.Tissue
+
+/**
+ * @author clint
+ * Dec 8, 2020
+ */
+final class DgaTissueUploadTest extends AwsFunSuite with IntakeSyntax with DgaSyntax with Loggable {
+  testWithPseudoDir(s"${getClass.getSimpleName}-Read_from_DGA_upload_to_S3") { testDir =>
+    val (versionSource, tissueSource) = Dga.versionAndTissueSource()
+    
+    assert(versionSource.records.next() === "3")
+    
+    val uuid = java.util.UUID.randomUUID.toString
+    
+    val awsClient = new AwsClient.Default(aws)
+    
+    val sink = new AwsRowSink(
+        topic = "some-topic",
+        name = "some-name",
+        batchSize = 10000, //10k
+        awsClient = awsClient,
+        baseDir = Some(testDir),
+        // :(
+        yes = true,
+        uuid = uuid)
+    
+    val count: Fold[Tissue, Int, Int] = Fold.count
+    val upload: Fold[Tissue, Unit, Unit] = Fold.foreach(sink.accept)
+    
+    val m = count |+| upload
+    
+    val filtered = tissueSource.filter(_.isValid)
+    
+    TimeUtils.time("Uploading tissues", info(_)) {
+      info("Uploading tissues...")
+      
+      val (n, _) = try { m.process(filtered.records) } finally { sink.close() }
+      
+      info(s"Uploaded $n valid tissues")
+    }
+    
+    val uploaded = awsClient.list(s"${testDir}")
+    
+    val expectedKeys = Set(
+        s"${testDir}/some-topic/some-name/part-000-${uuid}.json",
+        s"${testDir}/some-topic/some-name/part-001-${uuid}.json",
+        s"${testDir}/some-topic/some-name/part-002-${uuid}.json")
+    
+    assert(uploaded.toSet == expectedKeys)
+  }
+}

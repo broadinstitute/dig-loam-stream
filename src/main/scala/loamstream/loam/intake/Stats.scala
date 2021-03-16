43283b094f826e3d85c5289ad539b9416e576981
clint <cgilbert@broadinstitute.org>
2021-03-16 16:35:17 -0400

    Port some stats functions from dig-aggregator-intake (effective N and qnorm)

---
+++ src/main/scala/loamstream/loam/intake/Stats.scala
@@ -0,0 +1,36 @@
+package loamstream.loam.intake
+
+import breeze.stats.distributions.Gaussian
+
+/**
+ * @author clint
+ * 
+ * 16 Mar, 2021
+ */
+object Stats {
+  /**
+   * Ported from dig-aggregator-intake/processors/variants.py
+   */
+  def effectiveN(cases: Double, controls: Double): Double = 4.0 / ((1.0 / cases) + (1.0 / controls))
+  
+  private val threadLocalGaussian: ThreadLocal[Gaussian] = {
+    ThreadLocal.withInitial(() => Gaussian(mu = 0, sigma = 1)) 
+  }
+  
+  /**
+   * Quantized function.
+   * See: https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/Normal
+   * 
+   * Ported from dig-aggregator-intake/processors/variants.py
+   */
+  def qnorm(beta: Double, p: Double): Double = {
+    //TODO: Is it safe to cache this?
+    val gaussian = threadLocalGaussian.get()
+    
+    import gaussian.inverseCdf
+    import scala.math.abs
+    
+    if(beta == 0.0) { 1.0 }
+    else { -(abs(beta) / inverseCdf(p / 2)) }
+  }
+}
---
+++ src/test/scala/loamstream/loam/intake/StatsTest.scala
@@ -0,0 +1,17 @@
+package loamstream.loam.intake
+
+import org.scalatest.FunSuite
+
+/**
+ * @author clint
+ * 16 Mar, 2021
+ */
+final class StatsTest extends FunSuite {
+  test("effectiveN") {
+    fail("TODO")
+  }
+  
+  test("qnorm") {
+    fail("TODO")
+  }
+}

1459b76a738008cb6ef48a2053e3536cd2a1d519
clint <cgilbert@broadinstitute.org>
2021-03-18 17:28:29 -0400

    Restore ability to write aggregator config file contents, for dry-run/sanity-checking

---
+++ src/test/scala/loamstream/loam/intake/AggregatorMetadataTest.scala
@@ -0,0 +1,78 @@
+package loamstream.loam.intake
+
+import org.scalatest.FunSuite
+import scala.collection.Seq
+
+/**
+ * @author clint
+ * Jun 25, 2020
+ */
+final class AggregatorMetadataTest extends FunSuite {
+  test("escape") {
+    import AggregatorMetadata.escape
+    
+    assert(escape("") === "")
+    assert(escape("foo") === "foo")
+    assert(escape("foo bar") === "\"foo bar\"")
+    assert(escape(" ") === "\" \"")
+    
+    assert(escape("foo_\"lalala\"") === "foo_\"lalala\"")
+    assert(escape("foo \"lalala\"") === "\"foo \\\"lalala\\\"\"")
+  }
+  
+  test("asConfigFileContents") {
+    import System.lineSeparator
+    
+    def doTest(
+        author: Option[String], 
+        expectedAuthorString: String,
+        quantitative: Option[AggregatorMetadata.Quantitative], 
+        expectedQuantString: String): Unit = {
+
+      val dataset = "some-dataset"
+      val phenotype = "some-phenotype"
+      val ancestry = Ancestry.AA
+      val tech = TechType.ExChip
+      val properties = Seq("x" -> "123", "a" -> "456")
+    
+      val m = AggregatorMetadata(
+        dataset = dataset,
+        phenotype = phenotype,
+        ancestry = ancestry,
+        author = author,
+        tech = tech,
+        quantitative = quantitative,
+        properties = properties)
+
+      val expected = s"""
+        |dataset ${dataset} ${phenotype}
+        |ancestry ${ancestry}
+        |tech ${tech}
+        |${expectedQuantString}
+        |${expectedAuthorString}""".stripMargin.trim
+    
+      def removeEmptyLines(s: String): String = {
+        s.split(lineSeparator).iterator.map(_.trim).filter(_.nonEmpty).mkString(lineSeparator)
+      }
+        
+      //NB: Ignore empty lines when considering differences
+      assert(removeEmptyLines(m.asConfigFileContents) === removeEmptyLines(expected))
+    }
+    
+    doTest(None, "", None, "")
+    doTest(Some("some-author"), "author some-author", None, "")
+    doTest(None, "", Some(AggregatorMetadata.Quantitative.Subjects(42)), "subjects 42")
+    doTest(Some("some-author"), 
+        "author some-author", Some(AggregatorMetadata.Quantitative.Subjects(42)), "subjects 42")
+    doTest(
+        None, 
+        "", 
+        Some(AggregatorMetadata.Quantitative.CasesAndControls(42, 99)), 
+        s"cases 42${lineSeparator}controls 99")
+    doTest(
+        Some("some-author"), 
+        "author some-author",
+        Some(AggregatorMetadata.Quantitative.CasesAndControls(42, 99)), 
+        s"cases 42${lineSeparator}controls 99")
+  }
+}

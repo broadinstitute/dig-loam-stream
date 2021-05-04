83f6254403422dd1fb821bab944de197f4e433eb
clint <cgilbert@broadinstitute.org>
2021-05-04 14:21:48 -0400

    Treat pvalues as doubles and not strings in PValueVariantRow when making JSON

--- src/main/scala/loamstream/loam/intake/rows.scala
+++ src/main/scala/loamstream/loam/intake/rows.scala
@@ -273,7 +273,7 @@ object PValueVariantRow {
       
       BaseVariantRow.commonJson(row) ++
       Seq(
-        AggregatorJsonKeys.pValue -> JString(pvalue.toString),
+        AggregatorJsonKeys.pValue -> JDouble(pvalue),
         AggregatorJsonKeys.beta -> toJson(beta),
         AggregatorJsonKeys.oddsRatio -> toJson(oddsRatio),
         AggregatorJsonKeys.eaf -> toJson(eaf),

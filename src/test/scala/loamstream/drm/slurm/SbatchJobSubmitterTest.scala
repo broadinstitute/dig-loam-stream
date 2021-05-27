9aa13afc14275f711fb8b8845973ce47c2ec5421
clint <cgilbert@broadinstitute.org>
2021-05-27 12:40:28 -0400

    Stubbed out some Slurm tests

---
+++ src/test/scala/loamstream/drm/slurm/SbatchJobSubmitterTest.scala
@@ -0,0 +1,13 @@
+package loamstream.drm.slurm
+
+import org.scalatest.FunSuite
+
+/**
+ * @author clint
+ * May 25, 2021
+ */
+final class SbatchJobSubmitterTest extends FunSuite {
+  test("submitJobs") {
+    fail("TODO")
+  }
+}
---
+++ src/test/scala/loamstream/drm/slurm/ScancelJobKillerTest.scala
@@ -0,0 +1,13 @@
+package loamstream.drm.slurm
+
+import org.scalatest.FunSuite
+
+/**
+ * @author clint
+ * May 25, 2021
+ */
+final class ScancelJobKillerTest extends FunSuite {
+  test("killAllJobs") {
+    fail("TODO")
+  }
+}

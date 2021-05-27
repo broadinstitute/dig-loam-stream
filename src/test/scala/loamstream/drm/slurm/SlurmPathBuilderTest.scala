a65556ce5115c56816568209c6148c58a5d9fa79
clint <cgilbert@broadinstitute.org>
2021-05-27 17:42:34 -0400

    Stubbed out some Slurm tests

---
+++ src/test/scala/loamstream/drm/slurm/SlurmPathBuilderTest.scala
@@ -0,0 +1,13 @@
+package loamstream.drm.slurm
+
+import org.scalatest.FunSuite
+
+/**
+ * @author clint
+ * May 27, 2021
+ */
+final class SlurmPathBuilderTest extends FunSuite {
+  test("TODO") {
+    fail("TODO")
+  }
+}
---
+++ src/test/scala/loamstream/drm/slurm/SlurmScriptBuilderParamsTest.scala
@@ -0,0 +1,13 @@
+package loamstream.drm.slurm
+
+import org.scalatest.FunSuite
+
+/**
+ * @author clint
+ * May 27, 2021
+ */
+final class SlurmScriptBuilderParamsTest extends FunSuite {
+  test("TODO") {
+    fail("TODO")
+  }
+}

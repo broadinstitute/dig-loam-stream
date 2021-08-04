fd7c65b7c07fbd8b98fd10ded49431f197cae2c2
clint <cgilbert@broadinstitute.org>
2021-08-03 20:47:44 -0400

    Add test for SlurmDrmTaskId

--- src/main/scala/loamstream/drm/slurm/SlurmDrmTaskId.scala
+++ src/main/scala/loamstream/drm/slurm/SlurmDrmTaskId.scala
@@ -15,20 +15,26 @@ object SlurmDrmTaskId {
     val jobAndTaskIndex = "^(\\d+)_(\\d+)$".r
     val jobId = "^(\\d+)$".r
     val byRange = "^(\\d+)_\\[(\\d+)-(\\d+)\\]$".r
-    val enumerated = "(\\d+)_([.,]+)$".r
   }
   
+  /**
+    * Parse a Slurm task id string into one or more DrmTaskIds.
+    * 
+    * Slurm's text representation of task ids, as returned by `squeue`, allows encoding multiple
+    * task ids, like "foo_[1-3]", corresponding to 
+    * Seq(DrmTaskId("foo", 1), DrmTaskId("foo", 2), DrmTaskId("foo", 3)), as well as single task
+    * ids, like "foo_42" corresponding to DrmTaskId("foo", 42)
+    *
+    * @param s
+    * @return
+    */
   private[slurm] def parseDrmTaskIds(s: String): Try[Iterable[DrmTaskId]] = s.trim match {
     case Regexes.jobAndTaskIndex(jobId, taskIndex) => Success(Seq(DrmTaskId(jobId, taskIndex.toInt)))
     case Regexes.byRange(jobId, start, end) => Try {
-      //TODO: .to vs .until ??
+      //NB: .to() makes for inclusive ranges
       (start.toInt to end.toInt).map(index => DrmTaskId(jobId, index))
     }
-    /*case Regexes.enumerated(jobId, indexList) => Try {
-      indexList.split("\\,").map(_.toInt).map(index => DrmTaskId(jobId, index))
-    }*/
     //TODO: are combinations of by-range and enumerated ids allowed?
-    //case Regexes.jobId(jobId) => Success(DrmTaskId(jobId, 0)) //TODO
     case _ => Tries.failure(s"Couldn't parse DrmTaskId from '$s'")
   }
 }

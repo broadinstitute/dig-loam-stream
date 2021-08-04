1634015b2f6564e776a3691d0feefaf2104c9ec9
clint <cgilbert@broadinstitute.org>
2021-08-04 10:40:38 -0400

    Make sure SLURM DRM scripts loam Singularity before running jobs

--- src/main/scala/loamstream/drm/slurm/SlurmScriptBuilderParams.scala
+++ src/main/scala/loamstream/drm/slurm/SlurmScriptBuilderParams.scala
@@ -7,7 +7,7 @@ import loamstream.drm.ScriptBuilderParams
  * May 18, 2021
  */
 object SlurmScriptBuilderParams extends ScriptBuilderParams {
-  override def preamble: Option[String] = None
+  override def preamble: Option[String] = Some("module load singularity/3.2.1")
   override def indexEnvVarName: String = "SLURM_ARRAY_TASK_ID"
   override def jobIdEnvVarName: String = "SLURM_ARRAY_JOB_ID"
   override def drmIndexVarExpr: String = "%a"
--- src/test/scala/loamstream/drm/slurm/SlurmScriptBuilderParamsTest.scala
+++ src/test/scala/loamstream/drm/slurm/SlurmScriptBuilderParamsTest.scala
@@ -7,7 +7,12 @@ import org.scalatest.FunSuite
  * May 27, 2021
  */
 final class SlurmScriptBuilderParamsTest extends FunSuite {
-  test("TODO") {
-    fail("TODO")
+  test("values") {
+    import SlurmScriptBuilderParams._
+
+    assert(preamble === Some("module load singularity/3.2.1"))
+    assert(indexEnvVarName === "SLURM_ARRAY_TASK_ID")
+    assert(jobIdEnvVarName === "SLURM_ARRAY_JOB_ID")
+    assert(drmIndexVarExpr === "%a")
   }
 }

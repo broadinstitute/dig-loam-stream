38d342d3b9688e2367b523c0c58d8d719e31beb9
clint <cgilbert@broadinstitute.org>
2021-08-05 16:32:08 -0400

    Fill in SlurmPathBuilderTest

--- src/test/scala/loamstream/drm/slurm/SlurmPathBuilderTest.scala
+++ src/test/scala/loamstream/drm/slurm/SlurmPathBuilderTest.scala
@@ -1,13 +1,48 @@
 package loamstream.drm.slurm
 
 import org.scalatest.FunSuite
+import loamstream.conf.SlurmConfig
+import loamstream.conf.DrmConfig
 
 /**
  * @author clint
  * May 27, 2021
  */
 final class SlurmPathBuilderTest extends FunSuite {
-  test("TODO") {
-    fail("TODO")
+  import loamstream.util.BashScript.Implicits._
+  import loamstream.TestHelpers.path
+  
+  private val slurmConfig = SlurmConfig(maxNumJobsPerTaskArray = 42)
+  
+  private val workDir = slurmConfig.workDir
+  
+  test("reifyPathTemplate") {
+    import SlurmScriptBuilderParams.drmIndexVarExpr
+
+    val template = s"/foo/bar/${drmIndexVarExpr}/baz.${drmIndexVarExpr}"
+    
+    assert(SlurmPathBuilder.reifyPathTemplate(template, 42) === path("/foo/bar/42/baz.42").toAbsolutePath)
+  }
+  
+  test("pathTemplatePrefix") {
+    assert(SlurmPathBuilder.pathTemplatePrefix === "")
+  }
+  
+  test("slurmStdOutPathTemplate") {
+    val expected = workDir.resolve("blarg-blahblah.%a.stdout")
+    doPathTemplateTest(SlurmPathBuilder.stdOutPathTemplate, expected.render)
+  }
+
+  test("slurmStdErrPathTemplate") {
+    val expected = workDir.resolve("blarg-blahblah.%a.stderr")
+    doPathTemplateTest(SlurmPathBuilder.stdErrPathTemplate, expected.render)
+  }
+
+  private def doPathTemplateTest(makeTemplate: (DrmConfig, String) => String, expected: String): Unit = {
+    val jobName = "blarg-blahblah"
+
+    val template = makeTemplate(slurmConfig, jobName)
+
+    assert(template === expected)
   }
 }

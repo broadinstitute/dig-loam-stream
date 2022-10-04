58d829322dfc0178f4488db84bae2d108609d325
Ryan Koesterer <rmkoesterer@users.noreply.github.com>
2022-10-04 09:04:25 -0400

    Revert "Cg bch slurm fs woes"

--- src/main/scala/loamstream/drm/slurm/SlurmScriptBuilderParams.scala
+++ src/main/scala/loamstream/drm/slurm/SlurmScriptBuilderParams.scala
@@ -8,7 +8,7 @@ import loamstream.drm.DrmSystem
  * May 18, 2021
  */
 object SlurmScriptBuilderParams extends ScriptBuilderParams {
-  override def preamble: Option[String] = Some("module load singularity/3.6.4")
+  override def preamble: Option[String] = Some("module load singularity/3.2.1")
   override def indexEnvVarName: String = "SLURM_ARRAY_TASK_ID"
   override def jobIdEnvVarName: String = "SLURM_ARRAY_JOB_ID"
   override def drmIndexVarExpr: String = "%a"
